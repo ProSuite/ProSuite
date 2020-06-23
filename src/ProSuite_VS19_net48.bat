43c99d74fcab33cfe56e6bd30cd9d3256fd9a7c6
Emanuel Mahler <e.mahler@esri.ch>
2020-06-23 13:32:59 +0200

    Document csproj environment variables, add example .bat

---
+++ src/ProSuite_VS19_net48.bat
@@ -0,0 +1,6 @@
+@cd /d "%~dp0"
+
+set TargetFrameworkVersion=v4.8
+set OutputDirectory=..\..\bin
+
+"C:\Program Files (x86)\Microsoft Visual Studio\2019\Professional\Common7\IDE\devenv.exe" ProSuite.sln
