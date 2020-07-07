991d9dc7599be9ae28925ce1440d7292e2daafd8
Emanuel Mahler <e.mahler@esri.ch>
2020-07-07 18:23:54 +0200

    Use shared AssemblyInfo, remove from test projects

--- src/ProSuite.Commons/Properties/AssemblyInfo.cs
+++ src/ProSuite.Commons/Properties/AssemblyInfo.cs
@@ -2,36 +2,12 @@ using System;
 using System.Reflection;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-
 [assembly : AssemblyTitle("ProSuite.Commons")]
 [assembly : AssemblyDescription("")]
-[assembly : AssemblyConfiguration("")]
-[assembly : AssemblyCompany("")]
-[assembly : AssemblyProduct("ProSuite.Commons")]
-[assembly : AssemblyCopyright("The ProSuite Authors")]
-[assembly : AssemblyTrademark("")]
 [assembly : AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-
 [assembly : ComVisible(false)]
+[assembly : CLSCompliant(true)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-
 [assembly : Guid("4e655b4c-6283-4fd4-842a-2b642fdffe4a")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-
-[assembly : AssemblyVersion("1.0.0.0")]
-[assembly : AssemblyFileVersion("1.0.0.0")]
-[assembly : CLSCompliant(true)]
