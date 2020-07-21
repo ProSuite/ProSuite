0e7da6572d5d484b396041c62cc81d5cb2d47d94
Urs-Jakob Ruetschi <u.rueetschi@esri.ch>
2020-07-21 18:11:50 +0200

    Setup Commons.AO.Core, Commons.AGP, ProSuite.AGP.WorkLists

---
+++ src/ProSuite.Commons.AGP/Properties/AssemblyInfo.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ProSuite.Commons.AGP")]
+[assembly: AssemblyDescription("Common code with ArcGIS Pro dependencies")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: CLSCompliant(false)]
+
+// This GUID is for the ID of the typelib if this project is exposed to COM:
+[assembly: Guid("89f2c583-3022-43f8-9b69-27123836625f")]
---
+++ src/ProSuite.ProSuite.AGP.WorkLists/Properties/AssemblyInfo.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ProSuite.ProSuite.AGP.WorkLists")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: CLSCompliant(false)]
+
+// This GUID is for the ID of the typelib if this project is exposed to COM:
+[assembly: Guid("bbb8fc6f-6ba6-4e7a-8bcb-c185c75e60a1")]
