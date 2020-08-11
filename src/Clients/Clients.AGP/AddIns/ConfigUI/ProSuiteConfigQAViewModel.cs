878736f8ee73f3ec64dc019b1e6e7261925aecf9
Aldis Grauze <a.grauze@esri-de.com>
2020-08-11 10:22:23 +0200

    One way binding of QA configuration

--- src/Clients/Clients.AGP/AddIns/ConfigUI/ProSuiteConfigQAViewModel.cs
+++ src/Clients/Clients.AGP/AddIns/ConfigUI/ProSuiteConfigQAViewModel.cs
@@ -3,6 +3,7 @@ using ProSuite.Commons.QA.ServiceManager.Interfaces;
 using ProSuite.Commons.QA.ServiceManager.Types;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Linq;
 
 namespace Clients.AGP.ProSuiteSolution.ConfigUI
 {
@@ -13,6 +14,20 @@ namespace Clients.AGP.ProSuiteSolution.ConfigUI
 		public ProSuiteConfigQAViewModel(IEnumerable<ProSuiteQAServerConfiguration> configurations)
 		{
 			ServiceProviderConfigs = new ObservableCollection<ProSuiteQAServerConfiguration>(configurations);
+			if (ServiceProviderConfigs.Count > 0)
+				SelectedConfiguration = ServiceProviderConfigs.FirstOrDefault();
+		}
+
+		private ProSuiteQAServerConfiguration selectedConfiguration = null;
+		public ProSuiteQAServerConfiguration SelectedConfiguration
+		{
+			get {
+				return selectedConfiguration;
+			}
+			set {
+				selectedConfiguration = value;
+				NotifyPropertyChanged("SelectedConfiguration");
+			}
 		}
 
 		public string TabName
