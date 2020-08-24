69933edce40fbdd3dec51515e92c2bed72c769f7
Urs-Jakob Ruetschi <u.rueetschi@esri.ch>
2020-08-24 18:28:17 +0200

    Path to Pro SDK assemblies: configurable by envvar
    ProAssemblyPath, will default to standard install location,
    which is C:\Program Files\ArcGIS\Pro

--- src/ProSuite_VS19_net48.bat
+++ src/ProSuite_VS19_net48.bat
@@ -2,8 +2,13 @@
 
 set TargetFrameworkVersion=v4.8
 set OutputDirectory=..\..\bin
+set ProAssemblyPath=C:\Program Files\ArcGIS\Pro
 
 REM In case encryption is used, provide the encryptor factory from a custom location:
 REM set ProSuiteEncryptorFactoryDir=..\..\..\ProSuite-etc\
 
-"C:\Program Files (x86)\Microsoft Visual Studio\2019\Professional\Common7\IDE\devenv.exe" ProSuite.sln
+set DEVENV="C:\Program Files (x86)\Microsoft Visual Studio\2019\Professional\Common7\IDE\devenv.exe"
+set DEVOPTS=
+@REM set DEVOPTS=/ReSharper.LogFile C:\Temp\ReSharper_Log.txt /ReSharper.LogLevel Verbose
+
+%DEVENV% %DEVOPTS% ProSuite.sln
