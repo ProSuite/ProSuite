47a13430881aa2adb119aaaea06e325ec5b0b88d
Urs-Jakob Ruetschi <u.rueetschi@esri.ch>
2020-08-25 10:22:28 +0200

    Fix project output path / output directory

--- src/ProSuite.Commons.AGP.Hosting.Test/ProHostingTest.cs
+++ src/ProSuite.Commons.AGP.Hosting.Test/ProHostingTest.cs
@@ -1,43 +1,43 @@
 using System.Threading;
+using ArcGIS.Core.Geometry;
 using NUnit.Framework;
 using ProSuite.Commons.AGP.Spatial;
-using ArcGIS.Core.Geometry;
 
 namespace ProSuite.Commons.AGP.Hosting.Test
 {
 	/// <summary>
 	/// The tests here confirm that code from ArcGIS Pro SDK can be used
 	/// directly and via referenced projects that may use the SDK's NuGet.
-	/// <seealso cref="CoreHostProxy"/>
+	/// <seealso cref="CoreHostProxy" />
 	/// </summary>
 	[TestFixture]
 	[Apartment(ApartmentState.STA)]
-    public class ProHostingTest
-    {
+	public class ProHostingTest
+	{
 		[OneTimeSetUp]
-	    public void OneTimeSetup()
-	    {
+		public void OneTimeSetup()
+		{
 			CoreHostProxy.Initialize();
-	    }
+		}
 
 		[Test]
 		public void CanUseCoreDirectly()
 		{
-			var ch1903 = SpatialReferenceBuilder.CreateSpatialReference(21781);
-			var point = MapPointBuilder.CreateMapPoint(600000, 200000, ch1903);
+			SpatialReference ch1903 = SpatialReferenceBuilder.CreateSpatialReference(21781);
+			MapPoint point = MapPointBuilder.CreateMapPoint(600000, 200000, ch1903);
 
 			Assert.AreEqual(600000.0, point.X);
 			Assert.AreEqual(200000.0, point.Y);
 		}
 
-	    [Test]
-	    public void CanUseCoreViaProject()
-	    {
-		    var ch1903 = GeometryUtils.CreateSpatialReference(21781);
-		    var point = GeometryUtils.CreatePoint(600000, 200000, ch1903);
+		[Test]
+		public void CanUseCoreViaProject()
+		{
+			SpatialReference ch1903 = GeometryUtils.CreateSpatialReference(21781);
+			MapPoint point = GeometryUtils.CreatePoint(600000, 200000, ch1903);
 
-		    Assert.AreEqual(600000.0, point.X);
-		    Assert.AreEqual(200000.0, point.Y);
-	    }
-    }
+			Assert.AreEqual(600000.0, point.X);
+			Assert.AreEqual(200000.0, point.Y);
+		}
+	}
 }
