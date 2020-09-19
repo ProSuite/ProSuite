60e89621b01b8569b89404d12bb1cb8db608f19d
Stefan Schlaefli <s.schlaefli@esri.ch>
2020-09-19 23:32:20 +0200

    Refactor WorkItemVm

--- src/ProSuite.AGP.WorkList/WorkItemVm.cs
+++ src/ProSuite.AGP.WorkList/WorkItemVm.cs
@@ -7,54 +7,42 @@ namespace ProSuite.AGP.WorkList
 {
 	public class WorkItemVm: PropertyChangedBase
 	{
-		private IWorkItem _workItem;
-		private string  _description;
 		private WorkItemStatus _status;
 		private bool _visited;
 
 		public WorkItemVm(IWorkItem workItem)
 		{
 			this.WorkItem = workItem;
-			Description = WorkItem.Description;
-			Status = WorkItem.Status;
-			Visited = WorkItem.Visited;
 		}
 
-		public IWorkItem WorkItem
-		{
-			get { return _workItem; }
-			set { _workItem = value; }
-		}
+		private IWorkItem WorkItem { get; set; }
+		
 
 		public String Description
 		{
-			get { return _workItem.Description; }
-			set
-			{
-				_description = value;
-				SetProperty(ref _description, value, () => Description);
-			}
+			get { return WorkItem.Description; }
+			set { }
 		}
 
 		public WorkItemStatus Status
 		{
-			get { return _workItem.Status; }
+			get { return WorkItem.Status; }
 			set
 			{
 				_status = value;
 				SetProperty(ref _status, value, () => Status);
-				_workItem.Status = value;
+				WorkItem.Status = value;
 			}
 		}
 
 		public bool Visited
 		{
-			get { return _workItem.Visited; }
+			get { return WorkItem.Visited; }
 			set
 			{
 				_visited = value;
 				SetProperty(ref _visited, value, () => Visited);
-				_workItem.Visited = value;
+				WorkItem.Visited = value;
 			}
 		}
 
