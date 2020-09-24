966f650519a5af411f19d23d7cec709053332960
Stefan Schlaefli <s.schlaefli@esri.ch>
2020-09-24 14:22:34 +0200

    Handle CurrentWorkItem is null

--- src/ProSuite.AGP.WorkList/WorkItemVm.cs
+++ src/ProSuite.AGP.WorkList/WorkItemVm.cs
@@ -9,6 +9,7 @@ namespace ProSuite.AGP.WorkList
 	{
 		private WorkItemStatus _status;
 		private bool _visited;
+		private const string NoItem = "no current item";
 
 		public WorkItemVm(IWorkItem workItem)
 		{
@@ -20,27 +21,44 @@ namespace ProSuite.AGP.WorkList
 
 		public String Description
 		{
-			get { return WorkItem.Description; }
+			get
+			{
+				return WorkItem == null ? NoItem : WorkItem.Description;
+			}
 			set { }
 		}
 
 		public WorkItemStatus Status
 		{
-			get { return WorkItem.Status; }
+			get
+			{
+				
+				return WorkItem == null ? WorkItemStatus.Todo: WorkItem.Status;
+			}
 			set
 			{
-				WorkItem.Status = value; 
+				if (WorkItem != null)
+				{
+					WorkItem.Status = value;
+				}
+				
 				SetProperty(ref _status, value, () => Status);
 			}
 		}
 
 		public bool Visited
 		{
-			get { return WorkItem.Visited; }
+			get
+			{
+				return WorkItem == null ? false: WorkItem.Visited;
+			}
 			set
 			{
+				if (WorkItem != null)
+				{
+					WorkItem.Visited = value;
+				}
 				SetProperty(ref _visited, value, () => Visited);
-				WorkItem.Visited = value;
 			}
 		}
 
