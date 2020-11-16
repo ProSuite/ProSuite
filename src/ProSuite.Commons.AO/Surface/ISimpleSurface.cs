40cadeb5c733038a864d44885d0f96d59a9a634b
Emanuel Mahler <e.mahler@esri.ch>
2020-11-16 18:59:01 +0100

    Remove 3D Analyst dependency using simple surface abstraction

---
+++ src/ProSuite.Commons.AO/Surface/ISimpleSurface.cs
@@ -0,0 +1,25 @@
+using System;
+using ESRI.ArcGIS.Geodatabase;
+using ESRI.ArcGIS.Geometry;
+using ProSuite.Commons.Essentials.CodeAnnotations;
+
+namespace ProSuite.Commons.AO.Surface
+{
+	[CLSCompliant(false)]
+	public interface ISimpleSurface : IDisposable
+	{
+		[CanBeNull]
+		IPolygon GetDomain();
+
+		double GetZ(double x, double y);
+
+		[CanBeNull]
+		IGeometry Drape([NotNull] IGeometry shape, double densifyDistance = double.NaN);
+
+		[CanBeNull]
+		IGeometry SetShapeVerticesZ([NotNull] IGeometry shape);
+
+		[CanBeNull]
+		ITin AsTin([CanBeNull] IEnvelope extent = null);
+	}
+}
