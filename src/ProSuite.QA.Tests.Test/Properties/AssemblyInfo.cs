da73720a24e38575eae17e7fc7210ed55e007364
Emanuel Mahler <e.mahler@esri.ch>
2020-11-25 18:12:55 +0100

    Prepare new QA.Test projects

---
+++ src/ProSuite.QA.Tests.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,5 @@
+using System.Threading;
+using NUnit.Framework;
+
+// nunit apartment model
+[assembly: Apartment(ApartmentState.STA)]
---
+++ src/ProSuite.QA.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ProSuite.QA.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: CLSCompliant(true)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("BA385DB2-4C31-4AD3-A288-1D965B1456AE")]
