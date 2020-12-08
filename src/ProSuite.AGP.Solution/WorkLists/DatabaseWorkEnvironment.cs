92e4d7b70d6b809d92f3b84081ff35b55fe5a882
Daniel Roth <d.roth@esri.ch>
2020-12-08 14:16:15 +0100

    Don't create domain if already exist
    - Used to log an error message

--- src/ProSuite.AGP.Solution/WorkLists/DatabaseWorkEnvironment.cs
+++ src/ProSuite.AGP.Solution/WorkLists/DatabaseWorkEnvironment.cs
@@ -53,6 +53,16 @@ namespace ProSuite.AGP.Solution.WorkLists
 				return false;
 			}
 
+			using (Geodatabase geodatabase = new Geodatabase(new FileGeodatabaseConnectionPath(new Uri(_path, UriKind.Absolute))))
+			{
+				if (geodatabase.GetDomains().Any(domain => string.Equals(_domainName, domain.GetName())))
+				{
+					_msg.Debug($"Domain {_domainName} already exists in {_path}");
+					return true;
+				}
+			}
+
+			// the GP tool is going to fail on creating a domain with the same name
 			await Task.WhenAll(
 				GeoprocessingUtils.CreateDomainAsync(_path, _domainName, "Correction status for work list"),
 				GeoprocessingUtils.AddCodedValueToDomainAsync(_path, _domainName, 100, "Not Corrected"),
@@ -104,6 +114,7 @@ namespace ProSuite.AGP.Solution.WorkLists
 		{
 			const string fieldName = "STATUS";
 
+			// the GP tool is not going to fail on adding a field with the same name
 			Task<bool> addField =
 				GeoprocessingUtils.AddFieldAsync(featureLayer.Name, fieldName, "Status",
 				                                 esriFieldType.esriFieldTypeInteger, null, null,
