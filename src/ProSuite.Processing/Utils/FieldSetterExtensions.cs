a208cb9a9b18071e4c672734190564b9b404c425
Urs-Jakob Ruetschi <u.rueetschi@esri.ch>
2021-02-05 11:01:41 +0100

    Use ProSuite.Processing.Evaluation in GeniusDB ...
    ... and drop Evaluation code from EsriDE.ProSuite.Processing

--- src/ProSuite.Processing/Utils/FieldSetterExtensions.cs
+++ src/ProSuite.Processing/Utils/FieldSetterExtensions.cs
@@ -1,5 +1,5 @@
+using System;
 using ArcGIS.Core.Data;
-using ProSuite.Commons.Essentials.CodeAnnotations;
 using ProSuite.Processing.Evaluation;
 
 namespace ProSuite.Processing.Utils
@@ -9,39 +9,46 @@ namespace ProSuite.Processing.Utils
 	/// suited for the Pro SDK types RowBuffer and Row, where
 	/// the latter no longer derives from the former.
 	/// </summary>
+	// TODO Must go to ProSuite.Processing.AGP.Core (cf EsriDE.ProSuite.Processing.Utils.FieldSetterExtensions)
 	public static class FieldSetterExtensions
 	{
 		public static IRowValues RowValues(this Row row)
 		{
-			return new RowValues(row);
+			return row == null ? null : new RowValues(row);
 		}
 
 		public static IRowValues RowValues(this RowBuffer row)
 		{
-			return new RowBufferValues(row);
+			return row == null ? null : new RowBufferValues(row);
 		}
 
 		public static FieldSetter DefineFields(
-			this FieldSetter instance, [NotNull] Row row, string qualifier = null)
+			this FieldSetter instance, Row row, string qualifier = null)
 		{
+			if (instance == null) return null;
 			return instance.DefineFields(row.RowValues(), qualifier);
 		}
 
 		public static FieldSetter DefineFields(
-			this FieldSetter instance, [NotNull] RowBuffer row, string qualifier = null)
+			this FieldSetter instance, RowBuffer row, string qualifier = null)
 		{
+			if (instance == null) return null;
 			return instance.DefineFields(row.RowValues(), qualifier);
 		}
 
 		public static void Execute(
 			this FieldSetter instance, Row row, IEvaluationEnvironment env = null)
 		{
+			if (instance == null)
+				throw new ArgumentNullException(nameof(instance));
 			instance.Execute(row.RowValues(), env);
 		}
 
 		public static void Execute(
 			this FieldSetter instance, RowBuffer row, IEvaluationEnvironment env = null)
 		{
+			if (instance == null)
+				throw new ArgumentNullException(nameof(instance));
 			instance.Execute(row.RowValues(), env);
 		}
 	}
