e54cb763fbd21ed6f878d0d1bfc743f202970a45
Emanuel Mahler <e.mahler@esri.ch>
2021-02-10 10:48:36 +0100

    Repro test and work-around for failing cursor on empty multipatches

--- src/ProSuite.QA.Tests.Test/QaNonEmptyGeometryTest.cs
+++ src/ProSuite.QA.Tests.Test/QaNonEmptyGeometryTest.cs
@@ -1,12 +1,18 @@
+using System;
+using ESRI.ArcGIS.esriSystem;
 using ESRI.ArcGIS.Geodatabase;
 using ESRI.ArcGIS.Geometry;
-using ProSuite.QA.Container;
-using ProSuite.QA.Tests.Test.Construction;
-using ProSuite.QA.Tests.Test.TestRunners;
 using NUnit.Framework;
+using ProSuite.Commons;
+using ProSuite.Commons.AO.Geodatabase;
 using ProSuite.Commons.AO.Geometry;
 using ProSuite.Commons.AO.Licensing;
 using ProSuite.Commons.AO.Test.TestSupport;
+using ProSuite.QA.Container;
+using ProSuite.QA.Container.Test;
+using ProSuite.QA.Tests.Test.Construction;
+using ProSuite.QA.Tests.Test.TestRunners;
+using TestUtils = ProSuite.Commons.AO.Test.TestUtils;
 
 namespace ProSuite.QA.Tests.Test
 {
@@ -18,7 +24,16 @@ namespace ProSuite.QA.Tests.Test
 		[OneTimeSetUp]
 		public void SetupFixture()
 		{
-			_lic.Checkout();
+			if (EnvironmentUtils.Is64BitProcess)
+			{
+				// Server
+				_lic.Checkout();
+			}
+			else
+			{
+				// Creating FeataureClasses requires standard
+				_lic.Checkout(EsriProduct.ArcEditor);
+			}
 		}
 
 		[OneTimeTearDown]
@@ -120,6 +135,170 @@ namespace ProSuite.QA.Tests.Test
 
 			QaError error;
 			AssertUtils.OneError(runner, "EmptyGeometry.GeometryNull", out error);
+		}
+
+		[Test]
+		public void CanTestEmptyGeometryFgdbPolyline()
+		{
+			IFeatureWorkspace featureWorkspace =
+				TestWorkspaceUtils.CreateTestFgdbWorkspace("EmptyPolyline");
+
+			IFeature feature =
+				CreateEmptyGeometryFeature(featureWorkspace, esriGeometryType.esriGeometryPolyline);
+
+			AssertOneErrorEmptyGeometry((IFeatureClass) feature.Class);
+		}
+
+		[Test]
+		public void CanTestEmptyGeometryFgdbMultipatch()
+		{
+			IFeatureWorkspace featureWorkspace =
+				TestWorkspaceUtils.CreateTestFgdbWorkspace("EmptyMultipatch");
+
+			IFeature feature =
+				CreateEmptyGeometryFeature(featureWorkspace,
+				                           esriGeometryType.esriGeometryMultiPatch);
+
+			AssertOneErrorEmptyGeometry((IFeatureClass) feature.Class);
+		}
+
+		[Test]
+		public void CanTestEmptyGeometrySdeMultipatch()
+		{
+			IWorkspace workspace = TestUtils.OpenUserWorkspaceOracle();
+			//IWorkspace workspace = ProSuite.Commons.AO.Test.TestUtils.OpenSDEWorkspaceOracle();
+
+			IMultiPatch normalGeometry = GeometryFactory.CreateMultiPatch(
+				GeometryFactory.CreateRing(new[]
+				                           {
+					                           new WKSPointZ {X = 2600000, Y = 1200000, Z = 400},
+					                           new WKSPointZ {X = 2600000, Y = 1200100, Z = 400},
+					                           new WKSPointZ {X = 2600100, Y = 1200000, Z = 400},
+					                           new WKSPointZ {X = 2600000, Y = 1200000, Z = 400},
+				                           }));
+
+			GeometryUtils.MakeZAware(normalGeometry);
+
+			IFeature feature =
+				CreateEmptyGeometryFeature((IFeatureWorkspace) workspace,
+				                           esriGeometryType.esriGeometryMultiPatch, normalGeometry);
+
+			AssertOneErrorEmptyGeometry((IFeatureClass) feature.Class);
+		}
+
+		[Test]
+		public void CanTestEmptyGeometrySdePolyline()
+		{
+			IWorkspace workspace = TestUtils.OpenUserWorkspaceOracle();
+			//IWorkspace workspace = ProSuite.Commons.AO.Test.TestUtils.OpenSDEWorkspaceOracle();
+
+			IPolyline normalGeometry = GeometryFactory.CreatePolyline(
+				GeometryFactory.CreatePath(new PointClass {X = 2600000, Y = 1200000, Z = 400},
+				                           new PointClass {X = 2600000, Y = 1200100, Z = 400},
+				                           new PointClass {X = 2600100, Y = 1200000, Z = 400},
+				                           new PointClass {X = 2600000, Y = 1200000, Z = 400}));
+
+			GeometryUtils.MakeZAware(normalGeometry);
+
+			IFeature feature =
+				CreateEmptyGeometryFeature((IFeatureWorkspace) workspace,
+				                           esriGeometryType.esriGeometryPolyline, normalGeometry);
+
+			AssertOneErrorEmptyGeometry((IFeatureClass) feature.Class);
+		}
+
+		[Test]
+		public void CanTestEmptyGeometrySdePoint()
+		{
+			IWorkspace workspace = TestUtils.OpenUserWorkspaceOracle();
+			//IWorkspace workspace = ProSuite.Commons.AO.Test.TestUtils.OpenSDEWorkspaceOracle();
+
+			IPoint normalGeometry = GeometryFactory.CreatePoint(2600000, 1200000, 400);
+
+			GeometryUtils.MakeZAware(normalGeometry);
+
+			IFeature feature =
+				CreateEmptyGeometryFeature((IFeatureWorkspace) workspace,
+				                           esriGeometryType.esriGeometryPoint, normalGeometry);
+
+			AssertOneErrorEmptyGeometry((IFeatureClass) feature.Class);
+		}
+
+		private static void AssertOneErrorEmptyGeometry(IFeatureClass featureClass)
+		{
+			var test = new QaNonEmptyGeometry(featureClass);
+			var runner = new QaTestRunner(test);
+			runner.Execute();
+
+			QaError error;
+			AssertUtils.OneError(runner, "EmptyGeometry.GeometryEmpty", out error);
+		}
+
+		private IFeature CreateEmptyGeometryFeature(IFeatureWorkspace ws,
+		                                            esriGeometryType geometryType,
+		                                            IGeometry andAlso = null)
+		{
+			ISpatialReference sr = SpatialReferenceUtils.CreateSpatialReference
+			((int) esriSRProjCS2Type.esriSRProjCS_CH1903Plus_LV95,
+			 true);
+
+			IFeatureClass featureClass =
+				EnsureFeatureClass(ws, geometryType, geometryType.ToString(), sr);
+
+			IGdbTransaction transaction = new GdbTransaction();
+
+			IFeature feature = null;
+			bool inserted = transaction.Execute((IWorkspace) ws, () =>
+			{
+				if (andAlso != null)
+				{
+					andAlso.SpatialReference = sr;
+					IFeature nonEmpty = featureClass.CreateFeature();
+					nonEmpty.Shape = andAlso;
+					nonEmpty.Store();
+				}
+
+				feature = featureClass.CreateFeature();
+
+				// NOTE: There is no difference when setting an empty geometry
+				feature.Store();
+			}, "Insert feature");
+
+			Assert.IsTrue(inserted);
+
+			return feature;
+		}
+
+		private static IFeatureClass EnsureFeatureClass(IFeatureWorkspace ws,
+		                                                esriGeometryType geometryType,
+		                                                string name,
+		                                                ISpatialReference sr)
+		{
+			try
+			{
+				IFeatureClass existing = DatasetUtils.OpenFeatureClass(ws, name);
+				DatasetUtils.DeleteFeatureClass(existing);
+			}
+			catch (Exception e)
+			{
+				Console.WriteLine(e);
+			}
+
+			IFieldsEdit fields = new FieldsClass();
+			fields.AddField(FieldUtils.CreateOIDField());
+
+			fields.AddField(FieldUtils.CreateShapeField(
+				                "Shape", geometryType,
+				                sr, 1000, true));
+
+			IFeatureClass fc = DatasetUtils.CreateSimpleFeatureClass(ws, name, fields);
+
+			if (fc is IVersionedTable)
+			{
+				DatasetUtils.RegisterAsVersioned(fc);
+			}
+
+			return fc;
 		}
 	}
 }
