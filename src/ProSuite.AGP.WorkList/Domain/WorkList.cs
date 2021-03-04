87ecf2c0f8d47b9442fc173c12ba19e64764760b
Daniel Roth <d.roth@esri.ch>
2021-03-04 15:37:40 +0100

    IEquatable<WorkList>

--- src/ProSuite.AGP.WorkList/Domain/WorkList.cs
+++ src/ProSuite.AGP.WorkList/Domain/WorkList.cs
@@ -22,7 +22,7 @@ namespace ProSuite.AGP.WorkList.Domain
 	/// </summary>
 	// todo daro: separate geometry processing code
 	// todo daro: separate QueuedTask code
-	public abstract class WorkList : IWorkList
+	public abstract class WorkList : IWorkList, IEquatable<WorkList>
 	{
 		private static readonly IMsg _msg = Msg.ForCurrentClass();
 
@@ -1106,5 +1106,39 @@ namespace ProSuite.AGP.WorkList.Domain
 		{
 			PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
 		}
+
+		#region IEquatable implementation
+
+		public bool Equals(WorkList other)
+		{
+			return string.Equals(Name, other.Name, StringComparison.OrdinalIgnoreCase);
+		}
+
+		public override bool Equals(object obj)
+		{
+			if (ReferenceEquals(null, obj))
+			{
+				return false;
+			}
+
+			if (ReferenceEquals(this, obj))
+			{
+				return true;
+			}
+
+			if (obj.GetType() != GetType())
+			{
+				return false;
+			}
+
+			return Equals((WorkList) obj);
+		}
+
+		public override int GetHashCode()
+		{
+			return StringComparer.OrdinalIgnoreCase.GetHashCode(Name);
+		}
+
+		#endregion
 	}
 }
