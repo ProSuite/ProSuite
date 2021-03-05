76413dfecf69feeb7f6c06a13a6d32a4784d9fa9
Felix Arnet <felix.arnet@esri-de.com>
2021-03-05 07:42:19 +0100

    to synch

--- src/ProSuite.DomainModel.AO/QA/TestParameterTypeUtils.cs
+++ src/ProSuite.DomainModel.AO/QA/TestParameterTypeUtils.cs
@@ -5,6 +5,7 @@ using ESRI.ArcGIS.DataSourcesRaster;
 #endif
 using System;
 using ESRI.ArcGIS.Geodatabase;
+using ProSuite.Commons.AO.Surface;
 using ProSuite.Commons.Essentials.Assertions;
 using ProSuite.Commons.Essentials.CodeAnnotations;
 using ProSuite.DomainModel.Core.DataModel;
@@ -47,6 +48,8 @@ namespace ProSuite.DomainModel.AO.QA
 				return TestParameterType.RasterMosaicDataset;
 			if (typeof(IRasterDataset).IsAssignableFrom(dataType))
 				return TestParameterType.RasterDataset;
+			if (typeof(TerrainReference).IsAssignableFrom(dataType))
+				return TestParameterType.TerrainDataset;
 
 			// The following types cannot be loaded in the Enterprise SDK:
 			if (dataType.Name == "IMosaicLayer")
