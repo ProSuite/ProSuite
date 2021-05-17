7999f8f7731c608be65637e69e9e62530430902c
Emanuel Mahler <e.mahler@esri.ch>
2021-05-17 10:20:40 +0200

    Initial test service implementation

---
+++ src/ProSuite.Microservices.Client/QualityTestService/ExternalTestClient.cs
@@ -0,0 +1,30 @@
+using Grpc.Core;
+using ProSuite.Commons.Essentials.CodeAnnotations;
+using ProSuite.Microservices.Definitions.QA.Test;
+
+namespace ProSuite.Microservices.Client.QualityTestService
+{
+	public class ExternalTestClient : MicroserviceClientBase
+	{
+		private QualityTestGrpc.QualityTestGrpcClient _staticTestClient;
+
+		public ExternalTestClient([NotNull] string url)
+			: base(ClientChannelConfig.Parse(url)) { }
+
+		public ExternalTestClient([NotNull] string host,
+		                          int port = 5151,
+		                          bool useTls = false,
+		                          string clientCertificate = null)
+			: base(host, port, useTls, clientCertificate) { }
+
+		protected override string ServiceName => nameof(QualityTestGrpc);
+
+		protected override void ChannelOpenedCore(Channel channel)
+		{
+			_staticTestClient = new QualityTestGrpc.QualityTestGrpcClient(channel);
+		}
+
+		[CanBeNull]
+		public QualityTestGrpc.QualityTestGrpcClient TestClient => _staticTestClient;
+	}
+}
