a79ab05684258805f3da02a91e1b62339df05f86
Emanuel Mahler <e.mahler@esri.ch>
2021-07-05 17:40:45 +0200

    Add SchemaInfo properties to ProSuite.Commons.AO/Geodatabase

---
+++ src/ProSuite.Commons.AO/Geodatabase/SchemaInfo/ObjectClassProperties.cs
@@ -0,0 +1,62 @@
+﻿using System.ComponentModel;
+using ESRI.ArcGIS.Geodatabase;
+using ProSuite.Commons.Essentials.Assertions;
+using ProSuite.Commons.Essentials.CodeAnnotations;
+
+namespace ProSuite.Commons.AO.Geodatabase.SchemaInfo
+{
+	public abstract class ObjectClassProperties
+	{
+		protected ObjectClassProperties([NotNull] IObjectClass objectClass)
+		{
+			Assert.ArgumentNotNull(objectClass, nameof(objectClass));
+
+			Workspace = new WorkspaceProperties(DatasetUtils.GetWorkspace(objectClass));
+			AliasName = DatasetUtils.GetAliasName(objectClass);
+			Name = DatasetUtils.GetName(objectClass);
+			SubtypeField = GetSubtypeFieldText(objectClass);
+
+			var versionedObject3 = objectClass as IVersionedObject3;
+
+			RegisteredAsVersioned = versionedObject3 != null &&
+			                        versionedObject3.IsRegisteredAsVersioned;
+		}
+
+		[Category(PropertyCategories.General)]
+		[UsedImplicitly]
+		public string Name { get; private set; }
+
+		[Category(PropertyCategories.General)]
+		[DisplayName("Alias Name")]
+		[UsedImplicitly]
+		public string AliasName { get; private set; }
+
+		[Category(PropertyCategories.General)]
+		[DisplayName("Subtype Field")]
+		[UsedImplicitly]
+		public string SubtypeField { get; private set; }
+
+		[Category(PropertyCategories.General)]
+		[TypeConverter(typeof(AllPropertiesConverter))]
+		[UsedImplicitly]
+		public WorkspaceProperties Workspace { get; private set; }
+
+		[Category(PropertyCategories.General)]
+		[DisplayName("Registered as Versioned")]
+		[UsedImplicitly]
+		public bool RegisteredAsVersioned { get; private set; }
+
+		[NotNull]
+		private static string GetSubtypeFieldText([NotNull] IObjectClass objectClass)
+		{
+			int subtypeFieldIndex = DatasetUtils.GetSubtypeFieldIndex(objectClass);
+			if (subtypeFieldIndex < 0)
+			{
+				return "No subtype field";
+			}
+
+			IField field = objectClass.Fields.Field[subtypeFieldIndex];
+			return field.Name; // use alias also?
+		}
+	}
+}
---
+++ src/ProSuite.Commons.AO/Geodatabase/SchemaInfo/XYDomainProperties.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.ComponentModel;
+using ProSuite.Commons.Essentials.CodeAnnotations;
+
+namespace ProSuite.Commons.AO.Geodatabase.SchemaInfo
+{
+	public class XyDomainProperties
+	{
+		public XyDomainProperties(double xMin, double yMin, double xMax, double yMax)
+		{
+			XMin = xMin;
+			YMin = yMin;
+			XMax = xMax;
+			YMax = yMax;
+
+			Width = Math.Abs(XMax - XMin);
+			Height = Math.Abs(YMax - YMin);
+		}
+
+		[DisplayName("X Minimum")]
+		[UsedImplicitly]
+		public double XMin { get; private set; }
+
+		[DisplayName("Y Minimum")]
+		[UsedImplicitly]
+		public double YMin { get; private set; }
+
+		[DisplayName("X Maximum")]
+		[UsedImplicitly]
+		public double XMax { get; private set; }
+
+		[DisplayName("Y Maximum")]
+		[UsedImplicitly]
+		public double YMax { get; private set; }
+
+		private double Width { get; set; }
+
+		private double Height { get; set; }
+
+		public override string ToString()
+		{
+			return string.Format("{0} x {1}", Width, Height);
+		}
+	}
+}
