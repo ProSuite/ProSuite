5753a07187c661105bde01ef500438b49f06f0ff
Emanuel Mahler <e.mahler@esri.ch>
2021-09-27 09:34:03 +0200

    Add support classes for rule-based verification to ProSuite.DomainServices

---
+++ src/ProSuite.DomainServices.AO/QA/Standalone/RuleBased/HtmlReportWriter.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using ProSuite.Commons.DotLiquid;
+using ProSuite.Commons.Essentials.Assertions;
+using ProSuite.Commons.Essentials.CodeAnnotations;
+using ProSuite.Commons.IO;
+using ProSuite.Commons.Logging;
+using ProSuite.DomainServices.AO.QA.DatasetReports.Xml;
+using ProSuite.DomainServices.AO.QA.HtmlReports;
+using ProSuite.DomainServices.AO.QA.Issues;
+using ProSuite.DomainServices.AO.QA.VerificationReports.Xml;
+
+namespace ProSuite.DomainServices.AO.QA.Standalone.RuleBased
+{
+	public class HtmlReportWriter
+	{
+		private readonly string _htmlReportTemplatePath;
+
+		private static readonly IMsg _msg =
+			new Msg(MethodBase.GetCurrentMethod().DeclaringType);
+
+		public HtmlReportWriter([NotNull] string htmlReportTemplatePath)
+		{
+			Assert.ArgumentNotNullOrEmpty(htmlReportTemplatePath,
+			                              nameof(htmlReportTemplatePath));
+			Assert.ArgumentCondition(File.Exists(htmlReportTemplatePath),
+			                         "template file does not exist: {0}",
+			                         htmlReportTemplatePath);
+
+			_htmlReportTemplatePath = htmlReportTemplatePath;
+		}
+
+		public void WriteHtmlReport([NotNull] string htmlReportFilePath,
+		                            [NotNull] IssueStatistics issueStatistics,
+		                            [NotNull] ObjectClassReport objectClassReport,
+		                            [NotNull] XmlVerificationReport verificationReport,
+		                            [NotNull] string outputDirectoryPath,
+		                            [NotNull] string objectClassReportName,
+		                            [NotNull] string verificationReportName,
+		                            [CanBeNull] string issueGeodatabasePath,
+		                            [CanBeNull] string mapDocumentName)
+		{
+			Assert.ArgumentNotNullOrEmpty(htmlReportFilePath, nameof(htmlReportFilePath));
+			Assert.ArgumentCondition(! File.Exists(htmlReportFilePath),
+			                         "output file already exists: {0}", htmlReportFilePath);
+			Assert.ArgumentNotNull(issueStatistics, nameof(issueStatistics));
+			Assert.ArgumentNotNull(objectClassReport, nameof(objectClassReport));
+			Assert.ArgumentNotNull(verificationReport, nameof(verificationReport));
+
+			_msg.DebugFormat("Preparing html report model");
+			var reportModel = new HtmlReportModel(issueStatistics,
+			                                      objectClassReport,
+			                                      verificationReport,
+			                                      outputDirectoryPath,
+			                                      objectClassReportName,
+			                                      verificationReportName,
+			                                      issueGeodatabasePath,
+			                                      mapDocumentName);
+
+			_msg.DebugFormat("Rendering html report based on template {0}",
+			                 _htmlReportTemplatePath);
+
+			LiquidUtils.RegisterSafeType<HtmlReportModel>();
+			LiquidUtils.RegisterSafeType<HtmlTexts>();
+
+			string output = LiquidUtils.Render(
+				_htmlReportTemplatePath,
+				new KeyValuePair<string, object>("report", reportModel),
+				new KeyValuePair<string, object>("text", new HtmlTexts()));
+
+			_msg.DebugFormat("Writing html report to {0}", htmlReportFilePath);
+			FileSystemUtils.WriteTextFile(output, htmlReportFilePath);
+
+			_msg.InfoFormat("Html report written to {0}", htmlReportFilePath);
+		}
+	}
+}
---
+++ src/ProSuite.DomainServices.AO/QA/Standalone/RuleBased/VerifiedModelUtils.cs
@@ -0,0 +1,125 @@
+using ESRI.ArcGIS.Geodatabase;
+using ESRI.ArcGIS.Geometry;
+using ProSuite.Commons.AO.Geodatabase;
+using ProSuite.Commons.Essentials.Assertions;
+using ProSuite.Commons.Essentials.CodeAnnotations;
+using ProSuite.DomainModel.AO.DataModel;
+using ProSuite.DomainModel.Core.DataModel;
+using ProSuite.DomainServices.AO.QA.VerifiedDataModel;
+
+namespace ProSuite.DomainServices.AO.QA.Standalone.RuleBased
+{
+	public static class VerifiedModelUtils
+	{
+		[NotNull]
+		public static Model CreateModel([NotNull] IObjectClass objectClass)
+		{
+			Assert.ArgumentNotNull(objectClass, nameof(objectClass));
+
+			IWorkspace workspace = DatasetUtils.GetWorkspace(objectClass);
+
+			var featureClass = objectClass as IFeatureClass;
+			return featureClass == null
+				       ? CreateModel(workspace)
+				       : CreateModel(workspace, ((IGeoDataset) featureClass).SpatialReference);
+		}
+
+		[NotNull]
+		public static Model CreateModel([NotNull] IWorkspace workspace,
+		                                [CanBeNull] ISpatialReference spatialReference)
+		{
+			Assert.ArgumentNotNull(workspace, nameof(workspace));
+
+			Model model = CreateModel(workspace);
+
+			if (spatialReference != null)
+			{
+				model.SpatialReferenceDescriptor =
+					new SpatialReferenceDescriptor(spatialReference);
+			}
+
+			return model;
+		}
+
+		[NotNull]
+		public static Dataset GetDataset([NotNull] IObjectClass objectClass,
+		                                 [NotNull] Model model)
+		{
+			Assert.ArgumentNotNull(objectClass, nameof(objectClass));
+			Assert.ArgumentNotNull(model, nameof(model));
+
+			Dataset result = model.GetDatasetByModelName(GetDatasetModelName(objectClass));
+
+			return result ?? CreateDataset(objectClass, model);
+		}
+
+		[NotNull]
+		public static Dataset CreateDataset([NotNull] IObjectClass objectClass,
+		                                    [NotNull] Model model)
+		{
+			Assert.ArgumentNotNull(objectClass, nameof(objectClass));
+			Assert.ArgumentNotNull(model, nameof(model));
+
+			var featureClass = objectClass as IFeatureClass;
+			return featureClass != null
+				       ? (Dataset) CreateVectorDataset(featureClass, model)
+				       : CreateTableDataset(objectClass, model);
+		}
+
+		[NotNull]
+		public static TableDataset CreateTableDataset(
+			[NotNull] IObjectClass objectClass,
+			[NotNull] Model model)
+		{
+			Assert.ArgumentNotNull(objectClass, nameof(objectClass));
+			Assert.ArgumentNotNull(model, nameof(model));
+
+			var dataset = new VerifiedTableDataset(GetDatasetModelName(objectClass))
+			              {
+				              AliasName = objectClass.AliasName
+			              };
+
+			model.AddDataset(dataset);
+
+			// no harvesting of attributes/object types
+
+			return dataset;
+		}
+
+		[NotNull]
+		public static VectorDataset CreateVectorDataset(
+			[NotNull] IFeatureClass featureClass,
+			[NotNull] Model model)
+		{
+			Assert.ArgumentNotNull(featureClass, nameof(featureClass));
+			Assert.ArgumentNotNull(model, nameof(model));
+
+			var dataset = new VerifiedVectorDataset(GetDatasetModelName(featureClass))
+			              {
+				              AliasName = featureClass.AliasName
+			              };
+
+			model.AddDataset(dataset);
+
+			// no harvesting of attributes/object types
+
+			return dataset;
+		}
+
+		[NotNull]
+		private static string GetDatasetModelName([NotNull] IObjectClass objectClass)
+		{
+			return DatasetUtils.GetName(objectClass);
+		}
+
+		[NotNull]
+		private static Model CreateModel([NotNull] IWorkspace workspace)
+		{
+			string modelName = WorkspaceUtils.GetWorkspaceDisplayText(workspace);
+
+			// no need to specify databaseName/schemaOwner, as dataset names will be fully qualified
+			return new VerifiedModel(modelName, workspace,
+			                         (m, w) => new MasterDatabaseWorkspaceContext(w, m));
+		}
+	}
+}
