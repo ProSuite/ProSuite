23d80161365aa5d19e1231eef1fbc0e314a29da0
Daniel Roth <d.roth@esri.ch>
2021-11-29 10:03:57 +0100

    refactor

--- src/ProSuite.AGP.Solution/Editing/EraseTool.cs
+++ src/ProSuite.AGP.Solution/Editing/EraseTool.cs
@@ -1,6 +1,8 @@
 using ProSuite.AGP.Editing.Erase;
+using ProSuite.Commons.Essentials.CodeAnnotations;
 
 namespace ProSuite.AGP.Solution.Editing
 {
+	[UsedImplicitly]
 	public class EraseTool : EraseToolBase { }
 }
