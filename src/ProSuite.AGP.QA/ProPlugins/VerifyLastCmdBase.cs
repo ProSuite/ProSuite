8d951506e7312f83cd78a29e37a9863cae28242c
Markus Bedel <m.bedel@esri.ch>
2021-12-07 23:05:56 +0100

    add simple zoom/flash to VerifyLastCmdBase

--- src/ProSuite.AGP.QA/ProPlugins/VerifyLastCmdBase.cs
+++ src/ProSuite.AGP.QA/ProPlugins/VerifyLastCmdBase.cs
@@ -1,22 +1,29 @@
+using System;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Forms;
+using ArcGIS.Core.CIM;
 using ArcGIS.Core.Geometry;
 using ArcGIS.Core.Threading.Tasks;
 using ArcGIS.Desktop.Core;
-using ArcGIS.Desktop.Framework.Contracts;
 using ArcGIS.Desktop.Mapping;
 using ProSuite.AGP.QA.VerificationProgress;
 using ProSuite.Commons.AGP;
+using ProSuite.Commons.AGP.Carto;
+using ProSuite.Commons.AGP.Framework;
 using ProSuite.Commons.Essentials.Assertions;
 using ProSuite.Commons.Essentials.CodeAnnotations;
 using ProSuite.Commons.Logging;
 using ProSuite.Commons.Progress;
+using ProSuite.Commons.UI.Keyboard;
 using ProSuite.DomainModel.AGP.QA;
 using ProSuite.DomainModel.AGP.Workflow;
 using ProSuite.DomainModel.Core.QA;
 using ProSuite.DomainModel.Core.QA.VerificationProgress;
 using ProSuite.UI.QA.VerificationProgress;
+using Button = ArcGIS.Desktop.Framework.Contracts.Button;
+using MessageBox = ArcGIS.Desktop.Framework.Dialogs.MessageBox;
 
 namespace ProSuite.AGP.QA.ProPlugins
 {
@@ -24,8 +31,6 @@ namespace ProSuite.AGP.QA.ProPlugins
 	{
 		private static readonly IMsg _msg = Msg.ForCurrentClass();
 
-		//TODO: display/flash qaEnvironment.LastVerificationPerimeter OnTooltip
-		//TODO: ALT+Click: Zoom to qaEnvironment.LastVerificationPerimeter
 		//TODO: improve enabling: disable if qaEnvironment.LastVerificationPerimeter is null
 		protected VerifyLastCmdBase()
 		{
@@ -70,10 +75,17 @@ namespace ProSuite.AGP.QA.ProPlugins
 
 			Geometry perimeter = qaEnvironment.LastVerificationPerimeter;
 
-			if (perimeter == null)
+			if (perimeter == null || perimeter.IsEmpty)
 			{
 				MessageBox.Show("No last verification perimeter", "Verify Last",
 				                MessageBoxButton.OK, MessageBoxImage.Warning);
+				return;
+			}
+
+			if (KeyboardUtils.IsModifierPressed(Keys.Alt, exclusive: true))
+			{
+				ZoomTo(perimeter);
+
 				return;
 			}
 
@@ -140,6 +152,38 @@ namespace ProSuite.AGP.QA.ProPlugins
 			}
 
 			return result;
+		}
+
+		private static void ZoomTo([NotNull] Geometry perimeter)
+		{
+			QueuedTaskUtils.Run(
+				async delegate
+				{
+					try
+					{
+						Envelope extent = perimeter.Extent.Expand(1.1, 1.1, true);
+
+						await MapView.Active.ZoomToAsync(extent, TimeSpan.FromSeconds(0.3));
+
+						CIMColor darkGreen = ColorFactory.Instance.CreateRGBColor(0, 128, 0);
+
+						CIMStroke outline =
+							SymbolFactory.Instance.ConstructStroke(
+								darkGreen, 3, SimpleLineStyle.Solid);
+
+						CIMPolygonSymbol polygonSymbol =
+							SymbolFactory.Instance.ConstructPolygonSymbol(
+								darkGreen, SimpleFillStyle.Null, outline);
+
+						await MapUtils.FlashGeometryAsync(MapView.Active, perimeter,
+						                                  polygonSymbol.MakeSymbolReference(),
+						                                  milliseconds: 800);
+					}
+					catch (Exception e)
+					{
+						_msg.Warn($"Error zooming to/flashing verified perimeter: {e.Message}", e);
+					}
+				});
 		}
 	}
 }
