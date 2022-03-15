b41c1c2be047af68851a189aa1ba93182938f9d5
Emanuel Mahler <e.mahler@dirageosystems.ch>
2022-03-15 19:36:49 +0100

    Add class documentation.

--- src/ProSuite.Commons/IoC/IComponentInstaller.cs
+++ src/ProSuite.Commons/IoC/IComponentInstaller.cs
@@ -1,5 +1,10 @@
 namespace ProSuite.Commons.IoC
 {
+	/// <summary>
+	/// Before using them, the components must be registered with the <see cref="IoCContainer"/>. 
+	/// Installers encapsulate and partition the registration logic if needed.
+	/// The installer terminology is borrowed from Castle Windsor.
+	/// </summary>
 	public interface IComponentInstaller
 	{
 		void Install(IoCContainer ioCContainer);
