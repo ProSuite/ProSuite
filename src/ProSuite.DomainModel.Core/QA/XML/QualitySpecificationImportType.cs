95173b76348f3e8ba874c4b27fdaa048d7479026
Emanuel Mahler <e.mahler@dirageosystems.ch>
2022-03-26 07:59:20 +0100

    Add import/export interfaces to DomainModel.Core

---
+++ src/ProSuite.DomainModel.Core/QA/XML/QualitySpecificationImportType.cs
@@ -0,0 +1,17 @@
+﻿namespace ProSuite.DomainModel.Core.QA.XML
+{
+	public enum QualitySpecificationImportType
+	{
+		/// <summary>
+		/// Existing specifications are updated and new specifications are added
+		/// </summary>
+		UpdateOrAdd,
+
+		/// <summary>
+		/// Only existing specifications are updated, if there is a specification in 
+		/// the import file that is not in the selected list of target specifications,
+		/// then it is not imported.
+		/// </summary>
+		UpdateOnly
+	}
+}
