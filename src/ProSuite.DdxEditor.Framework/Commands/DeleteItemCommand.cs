2736dd124893a9d07bc96796710fd71a26ffc78c
Emanuel Mahler <e.mahler@dirageosystems.ch>
2022-04-04 14:19:28 +0200

    Delete obsolete types

--- src/ProSuite.DdxEditor.Framework/Commands/DeleteItemCommand.cs
+++ src/ProSuite.DdxEditor.Framework/Commands/DeleteItemCommand.cs
@@ -1,4 +1,3 @@
-using System;
 using System.Drawing;
 using ProSuite.Commons.Essentials.Assertions;
 using ProSuite.Commons.Essentials.CodeAnnotations;
@@ -23,46 +22,6 @@ namespace ProSuite.DdxEditor.Framework.Commands
 		/// <param name="item">The item.</param>
 		/// <param name="applicationController">The application controller.</param>
 		public DeleteItemCommand([NotNull] Item item,
-		                         [NotNull] IApplicationController applicationController)
-			: base(item)
-		{
-			Assert.ArgumentNotNull(applicationController, nameof(applicationController));
-
-			_applicationController = applicationController;
-		}
-
-		public override Image Image => _image;
-
-		public override string Text => "Delete...";
-
-		protected override bool EnabledCore => _applicationController.CanDeleteItem(Item);
-
-		protected override void ExecuteCore()
-		{
-			_applicationController.DeleteItem(Item);
-		}
-	}
-
-	[Obsolete(
-		"override AllowDelete on item instead (or if really needed, use non-generic class)")
-	]
-	public class DeleteItemCommand<T> : ItemCommandBase<T>, IDeleteItemCommand
-		where T : Item
-	{
-		private readonly IApplicationController _applicationController;
-		private static readonly Image _image;
-
-		static DeleteItemCommand()
-		{
-			_image = Resources.Delete;
-		}
-
-		/// <summary>
-		/// Initializes a new instance of the <see cref="DeleteItemCommand&lt;T&gt;"/> class.
-		/// </summary>
-		/// <param name="item">The item.</param>
-		/// <param name="applicationController">The application controller.</param>
-		public DeleteItemCommand([NotNull] T item,
 		                         [NotNull] IApplicationController applicationController)
 			: base(item)
 		{
--- src/ProSuite.DdxEditor.Framework/IHelpProvider.cs
+++
@@ -1,7 +0,0 @@
-using System;
-
-namespace ProSuite.DdxEditor.Framework
-{
-	[Obsolete("Use interface moved to Help namespace")]
-	public interface IHelpProvider : Help.IHelpProvider { }
-}
