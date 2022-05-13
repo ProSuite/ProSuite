312ff17e2e8b4456912c25a4fa47ca7e035335d3
Markus Bedel <m.bedel@esri.ch>
2022-05-13 19:17:05 +0200

    move ObjectCategoryUtils and ObjectDatasetUtils
    from EsriDE.ProSuite.DomainModel.DataModel.AO to ProSuite.DomainModel.AO.DataModel

---
+++ src/ProSuite.DomainModel.AO/DataModel/ObjectCategoryUtils.cs
@@ -0,0 +1,188 @@
+using System;
+using System.Reflection;
+using ESRI.ArcGIS.Geodatabase;
+using ProSuite.Commons.AO.Geodatabase;
+using ProSuite.Commons.Essentials.Assertions;
+using ProSuite.Commons.Essentials.CodeAnnotations;
+using ProSuite.Commons.Logging;
+using ProSuite.DomainModel.Core.DataModel;
+
+namespace ProSuite.DomainModel.AO.DataModel
+{
+	public static class ObjectCategoryUtils
+	{
+		private static readonly IMsg _msg = new Msg(MethodBase.GetCurrentMethod().DeclaringType);
+
+		public static void ApplyObjectCategoryValuesTo(
+			[NotNull] ObjectCategory objectCategory,
+			[NotNull] IObject obj,
+			[CanBeNull] IFieldIndexCache fieldIndexCache = null)
+		{
+			ObjectSubtype objectSubtype = objectCategory as ObjectSubtype;
+
+			if (objectSubtype != null)
+			{
+				ApplyObjectSubtypeValuesTo(objectSubtype, obj, fieldIndexCache);
+			}
+			else
+			{
+				ApplyObjectTypeValueTo((ObjectType) objectCategory, obj, fieldIndexCache);
+			}
+		}
+
+		/// <summary>
+		/// Gets the object subtype that corresponds to a given object.
+		/// </summary>
+		/// <param name="objectType">The (known) object type.</param>
+		/// <param name="fromObject">The object whose attribute values shall be analyzed.</param>
+		/// <param name="fieldIndexCache">The optional field index cache.</param>
+		/// <returns></returns>
+		/// <exception cref="ArgumentException">
+		/// The object type does not apply to the object.
+		///   </exception>
+		[CanBeNull]
+		public static ObjectSubtype TryIdentifyObjectSubtype(
+			[NotNull] ObjectType objectType,
+			[NotNull] IObject fromObject,
+			[CanBeNull] IFieldIndexCache fieldIndexCache = null)
+		{
+			Assert.ArgumentNotNull(fromObject, nameof(fromObject));
+
+			int maxMatchingFieldCount = -1;
+			ObjectSubtype bestMatchingObjectSubtype = null;
+
+			if (! ObjectTypeAppliesTo(objectType, fromObject))
+			{
+				throw new ArgumentException(
+					string.Format("Object type {0} does not apply to object {1}",
+					              objectType, GdbObjectUtils.ToString(fromObject)));
+			}
+
+			foreach (ObjectSubtype objectSubtype in objectType.ObjectSubtypes)
+			{
+				if (! ObjectSubtypeAppliesTo(objectSubtype, fromObject, fieldIndexCache))
+				{
+					continue;
+				}
+
+				int matchingFieldCount = objectSubtype.Criteria.Count;
+
+				if (matchingFieldCount > maxMatchingFieldCount)
+				{
+					maxMatchingFieldCount = matchingFieldCount;
+					bestMatchingObjectSubtype = objectSubtype;
+				}
+			}
+
+			return bestMatchingObjectSubtype;
+		}
+
+		/// <summary>
+		/// Returns a value indicating if this object type applies to a given object.
+		/// </summary>
+		/// <param name="objectType"></param>
+		/// <param name="obj">The obj.</param>
+		/// <returns></returns>
+		private static bool ObjectTypeAppliesTo([NotNull] ObjectType objectType,
+		                                        [NotNull] IObject obj)
+		{
+			Assert.ArgumentNotNull(obj, nameof(obj));
+
+			int subtypeFieldIndex = GdbObjectUtils.GetSubtypeFieldIndex(obj);
+
+			if (subtypeFieldIndex < 0)
+			{
+				return false;
+			}
+
+			object value = obj.Value[subtypeFieldIndex];
+
+			if (value is DBNull || value == null)
+			{
+				return false;
+			}
+
+			return (int) value == objectType.SubtypeCode;
+		}
+
+		private static bool ObjectSubtypeAppliesTo(
+			[NotNull] ObjectSubtype objectSubtype,
+			[NotNull] IObject obj,
+			[CanBeNull] IFieldIndexCache fieldIndexCache = null)
+		{
+			Assert.ArgumentNotNull(obj, nameof(obj));
+
+			foreach (ObjectSubtypeCriterion criterion in objectSubtype.Criteria)
+			{
+				ObjectAttribute attribute = criterion.Attribute;
+
+				int fieldIndex =
+					AttributeUtils.GetFieldIndex(obj.Class, attribute, fieldIndexCache);
+				if (fieldIndex < 0)
+				{
+					// the criterion field is missing, no match
+					return false;
+				}
+
+				object value = obj.Value[fieldIndex];
+				if (! Equals(value, criterion.AttributeValue))
+				{
+					return false;
+				}
+			}
+
+			return true;
+		}
+
+		public static void ApplyObjectTypeValueTo([NotNull] ObjectType objectType,
+		                                          [NotNull] IObject obj,
+		                                          [CanBeNull] IFieldIndexCache fieldIndexCache =
+			                                          null)
+		{
+			// TODO: Make private once obsolete methods are gone
+			Assert.ArgumentNotNull(obj, nameof(obj));
+
+			int subtypeFieldIndex = fieldIndexCache?.GetSubtypeFieldIndex(obj.Class) ??
+			                        GdbObjectUtils.GetSubtypeFieldIndex(obj);
+
+			if (subtypeFieldIndex < 0)
+			{
+				return;
+			}
+
+			object value = obj.Value[subtypeFieldIndex];
+
+			if (value != null && ! (value is DBNull) && (int) value == objectType.SubtypeCode)
+			{
+				return;
+			}
+
+			_msg.DebugFormat("Changing subtype value to {0} (current value: {1})", objectType.SubtypeCode, value);
+
+			obj.set_Value(subtypeFieldIndex, objectType.SubtypeCode);
+		}
+
+		public static void ApplyObjectSubtypeValuesTo([NotNull] ObjectSubtype objectSubtype,
+		                                              [NotNull] IObject obj,
+		                                              [CanBeNull] IFieldIndexCache fieldIndexCache)
+		{
+			// TODO: Make private once obsolete methods are gone
+			Assert.ArgumentNotNull(obj, nameof(obj));
+
+			ApplyObjectTypeValueTo(objectSubtype.ObjectType, obj, fieldIndexCache);
+
+			foreach (ObjectSubtypeCriterion criterion in objectSubtype.Criteria)
+			{
+				ObjectAttribute attribute = criterion.Attribute;
+
+				int fieldIndex =
+					AttributeUtils.GetFieldIndex(obj.Class, attribute, fieldIndexCache);
+				Assert.True(fieldIndex >= 0,
+				            "Object subtype criterion attribute not found in object class {0}: {1}",
+				            DatasetUtils.GetName(obj.Class), attribute.Name);
+
+				obj.set_Value(fieldIndex, criterion.AttributeValue);
+			}
+		}
+	}
+}
---
+++ src/ProSuite.DomainModel.AO/DataModel/ObjectDatasetUtils.cs
@@ -0,0 +1,338 @@
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using ESRI.ArcGIS.esriSystem;
+using ESRI.ArcGIS.Geodatabase;
+using ProSuite.Commons.AO.Geodatabase;
+using ProSuite.Commons.Essentials.Assertions;
+using ProSuite.Commons.Essentials.CodeAnnotations;
+using ProSuite.Commons.Exceptions;
+using ProSuite.Commons.Logging;
+using ProSuite.DomainModel.Core.DataModel;
+using Attribute = ProSuite.DomainModel.Core.DataModel.Attribute;
+
+namespace ProSuite.DomainModel.AO.DataModel
+{
+	/// <summary>
+	/// Provides ArcObjects-specific functionality for domain datasets
+	/// </summary>
+	public static class ObjectDatasetUtils
+	{
+		private static readonly IMsg _msg = new Msg(MethodBase.GetCurrentMethod().DeclaringType);
+
+		private const int _minimumExpectedOperatorFieldLength = 10;
+
+		[NotNull]
+		public static ObjectAttribute GetExistingAttribute([NotNull] IObjectDataset objectDataset,
+		                                                   [NotNull] AttributeRole attributeRole)
+		{
+			Assert.ArgumentNotNull(objectDataset, nameof(objectDataset));
+			Assert.ArgumentNotNull(attributeRole, nameof(attributeRole));
+
+			ObjectAttribute attribute = objectDataset.GetAttribute(attributeRole);
+
+			if (attribute == null)
+			{
+				throw new ArgumentException(
+					string.Format(
+						"Attribute for role {0} not defined in dataset {1}",
+						attributeRole, objectDataset));
+			}
+
+			return attribute;
+		}
+
+		[CanBeNull]
+		public static ObjectCategory GetObjectCategory([NotNull] IObjectDataset objectDataset,
+		                                               [NotNull] IObject ofObject)
+		{
+			int subtypeFieldIndex = GdbObjectUtils.GetSubtypeFieldIndex(ofObject);
+
+			if (subtypeFieldIndex < 0)
+			{
+				if (objectDataset.ObjectTypes.Count == 1)
+				{
+					return objectDataset.ObjectTypes[0];
+				}
+
+				if (objectDataset.ObjectTypes.Count > 1)
+				{
+					_msg.WarnFormat(
+						"More than one object type for dataset without subtypes: {0}",
+						objectDataset.AliasName);
+
+					return null;
+				}
+
+				// no default object type; return null
+				return null;
+			}
+
+			object value = ofObject.Value[subtypeFieldIndex];
+			if (value is DBNull || value == null)
+			{
+				return null;
+			}
+
+			ObjectType objectType = objectDataset.GetObjectType((int) value);
+
+			if (objectType == null)
+			{
+				return null;
+			}
+
+			ObjectSubtype objectSubtype =
+				ObjectCategoryUtils.TryIdentifyObjectSubtype(objectType, ofObject);
+
+			return objectSubtype ?? (ObjectCategory) objectType;
+		}
+
+		[NotNull]
+		public static IEnumerable<int> GetObjectDefininingFieldIndexes(
+			[NotNull] IObjectDataset objectDataset,
+			[NotNull] IDatasetContext datasetContext)
+		{
+			IObjectClass objectClass = datasetContext.OpenObjectClass(objectDataset);
+			Assert.NotNull(objectClass, "dataset does not exist in context: {0}",
+			               objectDataset.Name);
+
+			return GetObjectDefininingFieldIndexes(objectDataset, objectClass);
+		}
+
+		[NotNull]
+		public static IEnumerable<int> GetObjectDefininingFieldIndexes(
+			[NotNull] IObjectDataset objectDataset,
+			[NotNull] IObjectClass objectClass,
+			[CanBeNull] IFieldIndexCache fieldIndexCache = null)
+		{
+			foreach (ObjectAttribute attribute in GetObjectDefininingAttributes(
+				objectDataset, objectClass, fieldIndexCache))
+			{
+				int fieldIndex =
+					AttributeUtils.GetFieldIndex(objectClass, attribute, fieldIndexCache);
+
+				if (fieldIndex >= 0)
+				{
+					yield return fieldIndex;
+				}
+			}
+		}
+
+		[NotNull]
+		public static IEnumerable<ObjectAttribute> GetObjectDefininingAttributes(
+			[NotNull] IObjectDataset objectDataset,
+			[NotNull] IObjectClass objectClass,
+			[CanBeNull] IFieldIndexCache fieldIndexCache)
+		{
+			var subtypeHandled = false;
+
+			int subtypeFieldIndex = GetSubtypeFieldIndex(objectClass, fieldIndexCache);
+
+			foreach (ObjectAttribute attribute in objectDataset.GetAttributes())
+			{
+				if (attribute.IsObjectDefining)
+				{
+					yield return attribute;
+				}
+				else if (! subtypeHandled)
+				{
+					if (subtypeFieldIndex < 0)
+					{
+						// class has no subtype
+						subtypeHandled = true;
+					}
+					else
+					{
+						int fieldIndex =
+							AttributeUtils.GetFieldIndex(objectClass, attribute,
+							                             fieldIndexCache);
+
+						if (fieldIndex == subtypeFieldIndex)
+						{
+							// attribute is subtype field; these are always object defining
+							yield return attribute;
+
+							subtypeHandled = true;
+						}
+					}
+				}
+			}
+		}
+
+		public static int GetSubtypeFieldIndex([NotNull] IObjectDataset objectDataset,
+		                                       [NotNull] IDatasetContext datasetContext,
+		                                       [CanBeNull] IFieldIndexCache fieldIndexCache = null)
+		{
+			IObjectClass objectClass = datasetContext.OpenObjectClass(objectDataset);
+			return objectClass == null
+				       ? -1
+				       : GetSubtypeFieldIndex(objectClass, fieldIndexCache);
+		}
+
+		private static int GetSubtypeFieldIndex([NotNull] IObjectClass objectClass,
+		                                        [CanBeNull] IFieldIndexCache fieldIndexCache)
+		{
+			return fieldIndexCache?.GetSubtypeFieldIndex(objectClass) ??
+			       DatasetUtils.GetSubtypeFieldIndex(objectClass);
+		}
+
+		#region Object attribute values
+
+		public static object GetValue([NotNull] IObject obj,
+		                              [NotNull] AttributeRole attributeRole,
+		                              [NotNull] IObjectDataset objectDataset,
+		                              [CanBeNull] IFieldIndexCache fieldIndexCache = null)
+		{
+			Assert.ArgumentNotNull(obj, nameof(obj));
+			
+			ObjectAttribute attribute = GetExistingAttribute(objectDataset, attributeRole);
+
+			return AttributeUtils.GetValueFor(obj, attribute, fieldIndexCache);
+		}
+
+		public static bool SetValue([NotNull] IObject obj,
+		                            [NotNull] AttributeRole attributeRole,
+		                            [CanBeNull] object value,
+		                            [NotNull] IObjectDataset objectDataset,
+		                            [CanBeNull] IFieldIndexCache fieldIndexCache = null,
+		                            bool writeOnlyIfNull = false,
+		                            bool allowMissingRole = false)
+		{
+			ObjectAttribute attribute = objectDataset.GetAttribute(attributeRole);
+
+			if (attribute == null)
+			{
+				if (! allowMissingRole)
+				{
+					throw new ArgumentException(
+						string.Format(
+							"Attribute for role {0} not defined in dataset {1}",
+							attributeRole, objectDataset));
+				}
+
+				if (_msg.IsVerboseDebugEnabled)
+				{
+					_msg.DebugFormat(
+						"Attribute for role {0} not defined in dataset {1}. Value not written",
+						attributeRole, objectDataset);
+				}
+
+				return false;
+			}
+
+			int fieldIndex = AttributeUtils.GetFieldIndex(obj.Class, attribute, fieldIndexCache);
+
+			if (fieldIndex >= 0)
+			{
+				if (! writeOnlyIfNull || GdbObjectUtils.IsNullOrEmpty(obj, fieldIndex))
+				{
+					SetValueCore(obj, attribute, fieldIndex, value);
+				}
+			}
+			else
+			{
+				throw new InvalidConfigurationException(
+					string.Format("Field not found for attribute {0}",
+					              attribute));
+			}
+
+			return true;
+		}
+
+		public static void SetOperator([NotNull] IObject obj, string operatorName,
+		                               [NotNull] IObjectDataset objectDataset,
+		                               [CanBeNull] IFieldIndexCache fieldIndexCache)
+		{
+			AttributeRole role = AttributeRole.Operator;
+
+			// special treatment for long operator names (assume that up to 10 chars are guaranteed to fit into field)
+			ObjectAttribute attribute = objectDataset.GetAttribute(role);
+			Assert.NotNull(attribute, "No operator attribute defined for {0}", objectDataset.Name);
+
+			int fieldIndex = AttributeUtils.GetFieldIndex(obj.Class, attribute, fieldIndexCache);
+			if (fieldIndex < 0)
+			{
+				throw new InvalidOperationException(
+					string.Format("Operator field {0} not found in current context for dataset {1}",
+					              attribute.Name,
+					              DatasetUtils.GetName(obj.Class)));
+			}
+
+			if (operatorName.Length > _minimumExpectedOperatorFieldLength)
+			{
+				int fieldLength = obj.Fields.Field[fieldIndex].Length;
+
+				if (operatorName.Length > fieldLength)
+				{
+					string truncated = operatorName.Substring(0, fieldLength);
+
+					_msg.WarnFormat("Truncating long operator name: {0} -> {1}",
+					                operatorName, truncated);
+
+					operatorName = truncated;
+				}
+			}
+
+			SetValueCore(obj, attribute, fieldIndex, operatorName);
+		}
+
+		/// <summary>
+		/// Gets the UUID for an object
+		/// </summary>
+		/// <param name="obj">The obj.</param>
+		/// <param name="objectDataset"></param>
+		/// <param name="fieldIndexCache">The optional field index cache.</param>
+		/// <returns>
+		/// a guid string, or an empty string if the object does not have a uuid attribute or value.
+		/// </returns>
+		public static string GetUuid([NotNull] IObject obj,
+		                             [NotNull] IObjectDataset objectDataset,
+		                             [CanBeNull] IFieldIndexCache fieldIndexCache = null)
+		{
+			ObjectAttribute uuidAttribute = objectDataset.GetAttribute(AttributeRole.UUID);
+
+			if (uuidAttribute == null)
+			{
+				return string.Empty;
+			}
+
+			int fieldIndex =
+				AttributeUtils.GetFieldIndex(obj.Class, uuidAttribute, fieldIndexCache);
+			if (fieldIndex < 0)
+			{
+				return string.Empty;
+			}
+
+			// write a UID, read a string...
+			var guidString = obj.Value[fieldIndex] as string;
+
+			// TODO revise...
+			return guidString ?? string.Empty;
+		}
+
+		private static void SetValueCore([NotNull] IObject obj,
+		                                 [NotNull] Attribute attribute,
+		                                 int fieldIndex,
+		                                 [CanBeNull] object value)
+		{
+			Assert.ArgumentNotNull(obj, nameof(obj));
+			Assert.ArgumentNotNull(attribute, nameof(attribute));
+
+			if (_msg.IsDebugEnabled)
+			{
+				var uid = value as IUID;
+				_msg.DebugFormat("Set {0}={1}", attribute.Name, uid != null
+					                                                ? uid.Value
+					                                                : value);
+			}
+
+			// If writing null, the value 0 is actually written in case of numeric fields.
+			// needs to be DBNull.Value.
+			object valueToWrite = value ?? DBNull.Value;
+
+			obj.set_Value(fieldIndex, valueToWrite);
+		}
+
+		#endregion
+	}
+}
