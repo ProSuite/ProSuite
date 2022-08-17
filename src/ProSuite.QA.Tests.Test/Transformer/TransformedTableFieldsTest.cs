2bee2b35c1fbf794eece04523b05a4862ad06e50
Emanuel Mahler <e.mahler@dirageosystems.ch>
2022-08-17 11:11:25 +0200

    Add initial TransformedTableFields implementation

---
+++ src/ProSuite.QA.Tests.Test/Transformer/TransformedTableFieldsTest.cs
@@ -0,0 +1,147 @@
+using System;
+using System.Collections.Generic;
+using ESRI.ArcGIS.Geodatabase;
+using ESRI.ArcGIS.Geometry;
+using NUnit.Framework;
+using ProSuite.Commons.AO.Geodatabase;
+using ProSuite.Commons.AO.Geodatabase.GdbSchema;
+using ProSuite.Commons.AO.Geometry;
+using ProSuite.Commons.AO.Licensing;
+using ProSuite.QA.Container;
+using ProSuite.QA.Container.Test;
+using ProSuite.QA.Tests.Transformers;
+
+namespace ProSuite.QA.Tests.Test.Transformer
+{
+	[TestFixture]
+	public class TransformedTableFieldsTest
+	{
+		private readonly ArcGISLicenses _lic = new ArcGISLicenses();
+
+		[OneTimeSetUp]
+		public void SetupFixture()
+		{
+			_lic.Checkout(EsriProduct.ArcEditor);
+		}
+
+		[OneTimeTearDown]
+		public void TearDownFixture()
+		{
+			_lic.Release();
+		}
+
+		[Test]
+		public void CanValidateFieldNames()
+		{
+			GdbTable sourceTable = CreateSampleTable();
+
+			var fields = new List<string>
+			             {
+				             "OBJEKTART AS OBJ_ART",
+				             "TABLE_NAME.FIELD_NAME as NAME",
+				             "MIN(field_value) AS minVal ",
+				             "NOALIAS_field ",
+				             "	TABLE_NAME.NO_ALIAS_EITHER"
+			             };
+
+			TransformedTableFields tableFields = new TransformedTableFields(sourceTable);
+
+			Assert.IsTrue(tableFields.ValidateFieldNames(fields, true, out string message));
+			Assert.IsFalse(tableFields.ValidateFieldNames(fields, false, out message));
+		}
+
+		[Test]
+		public void CanAddAllFields()
+		{
+			GdbTable sourceTable = CreateSampleTable();
+
+			GdbTable targetTable = new GdbTable(-1, "OUTPUT_TABLE");
+
+			TransformedTableFields tableFields = new TransformedTableFields(sourceTable);
+
+			tableFields.AddAllFields(targetTable);
+
+			Assert.AreEqual("OBJECTID", targetTable.Fields.Field[0].Name);
+			Assert.AreEqual("OBJEKTART", targetTable.Fields.Field[1].Name);
+			Assert.AreEqual("FIELD_NAME", targetTable.Fields.Field[2].Name);
+			Assert.AreEqual("FIELD_VALUE", targetTable.Fields.Field[3].Name);
+			Assert.AreEqual("NOALIAS_FIELD", targetTable.Fields.Field[4].Name);
+			Assert.AreEqual("NO_ALIAS_EITHER", targetTable.Fields.Field[5].Name);
+			Assert.AreEqual(InvolvedRowUtils.BaseRowField, targetTable.Fields.Field[6].Name);
+
+			WriteFieldNames(targetTable);
+		}
+
+		private static void WriteFieldNames(GdbTable targetTable)
+		{
+			for (int i = 0; i < targetTable.Fields.FieldCount; i++)
+			{
+				IField field = targetTable.Fields.Field[i];
+
+				Console.WriteLine(field.Name);
+			}
+		}
+
+		[Test]
+		public void CanAddUserFields()
+		{
+			IField shapeField = FieldUtils.CreateShapeField(
+				"Shape", esriGeometryType.esriGeometryPolygon,
+				SpatialReferenceUtils.CreateSpatialReference
+				((int) esriSRProjCS2Type.esriSRProjCS_CH1903Plus_LV95,
+				 true), 1000);
+
+			GdbTable sourceTable = CreateSampleTable();
+
+			//IField shapeField = FieldUtils.CreateShapeField(
+			//	esriGeometryType.esriGeometryPolygon,
+			//	SpatialReferenceUtils.CreateSpatialReference(WellKnownHorizontalCS.LV95));
+
+
+//IField shapeField = FieldUtils.CreateShapeField(
+//				esriGeometryType.esriGeometryPolygon,
+//				SpatialReferenceUtils.CreateSpatialReference(WellKnownHorizontalCS.LV95));
+
+			GdbFeatureClass targetTable = new GdbFeatureClass(-1, "OUTPUT_TABLE", esriGeometryType.esriGeometryPoint);
+
+			TransformedTableFields tableFields = new TransformedTableFields(sourceTable);
+
+			tableFields.AddOIDField(targetTable);
+			tableFields.AddShapeField(targetTable, esriGeometryType.esriGeometryPolygon, shapeField.GeometryDef);
+
+			tableFields.AddUserDefinedFields(new List<string>
+			                                 {
+												 "TABLE_NAME.OBJEKTART",
+												 "field_value as bla",
+												 "NOALIAS_FIELD"
+											 }, targetTable);
+
+			Assert.AreEqual("OBJECTID", targetTable.Fields.Field[0].Name);
+			Assert.AreEqual("SHAPE", targetTable.Fields.Field[1].Name);
+			Assert.AreEqual("OBJEKTART", targetTable.Fields.Field[2].Name);
+			Assert.AreEqual("bla", targetTable.Fields.Field[3].Name);
+			Assert.AreEqual("NOALIAS_FIELD", targetTable.Fields.Field[4].Name);
+			Assert.AreEqual(InvolvedRowUtils.BaseRowField, targetTable.Fields.Field[5].Name);
+
+			for (int i = 0; i < targetTable.Fields.FieldCount; i++)
+			{
+				IField field = targetTable.Fields.Field[i];
+
+				Console.WriteLine(field.Name);
+			}
+		}
+
+		private static GdbTable CreateSampleTable()
+		{
+			GdbTable table = new GdbTable(-1, "TABLE_NAME", "TableName");
+			table.AddFieldT(FieldUtils.CreateOIDField());
+			table.AddFieldT(
+				FieldUtils.CreateIntegerField("OBJEKTART"));
+			table.AddFieldT(FieldUtils.CreateTextField("FIELD_NAME", 12));
+			table.AddFieldT(FieldUtils.CreateTextField("FIELD_VALUE", 12));
+			table.AddFieldT(FieldUtils.CreateTextField("NOALIAS_FIELD", 12));
+			table.AddFieldT(FieldUtils.CreateTextField("NO_ALIAS_EITHER", 12));
+			return table;
+		}
+	}
+}
---
+++ src/ProSuite.QA.Tests/Transformers/TransformedTableFields.cs
@@ -0,0 +1,291 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using ESRI.ArcGIS.esriSystem;
+using ESRI.ArcGIS.Geodatabase;
+using ESRI.ArcGIS.Geometry;
+using ProSuite.Commons.AO.Geodatabase;
+using ProSuite.Commons.AO.Geodatabase.GdbSchema;
+using ProSuite.Commons.Essentials.Assertions;
+using ProSuite.Commons.Essentials.CodeAnnotations;
+using ProSuite.Commons.Text;
+using ProSuite.QA.Container;
+
+namespace ProSuite.QA.Tests.Transformers
+{
+	public class TransformedTableFields
+	{
+		private readonly IReadOnlyTable _sourceTable;
+		//private IList<string> _userDefinedAttributes;
+
+		public TransformedTableFields([NotNull] IReadOnlyTable sourceTable)
+		{
+			_sourceTable = sourceTable;
+		}
+
+		public string OutputFieldPrefix { get; set; }
+
+		public bool ExcludeBaseRowsField { get; set; }
+
+		/// <summary>
+		/// The target-source index mapping.
+		/// </summary>
+		public IDictionary<int, int> CopyIndexMatrix { get; set; } = new Dictionary<int, int>();
+
+		//public IList<string> UserDefinedAttributes
+		//{
+		//	get => _userDefinedAttributes;
+		//	set => _userDefinedAttributes = value;
+		//}
+
+		public void AddAllFields(GdbTable toOutputClass,
+		                         bool skipIfAlreadyExist = false)
+		{
+			IFields sourceFields = _sourceTable.Fields;
+			for (int sourceIdx = 0; sourceIdx < sourceFields.FieldCount; sourceIdx++)
+			{
+				if (skipIfAlreadyExist)
+				{
+					IField sourceField = _sourceTable.Fields.Field[sourceIdx];
+
+					if (toOutputClass.FindField(sourceField.Name) >= 0)
+					{
+						continue;
+					}
+				}
+
+				CopyField(sourceIdx, toOutputClass, sourceFields);
+			}
+
+			if (!ExcludeBaseRowsField)
+				EnsureBaseRowField(toOutputClass);
+		}
+
+		private static void EnsureBaseRowField(GdbTable inTable)
+		{
+
+			if (inTable.FindField(InvolvedRowUtils.BaseRowField) < 0)
+			{
+				inTable.AddFieldT(FieldUtils.CreateBlobField(InvolvedRowUtils.BaseRowField));
+			}
+		}
+
+		public void AddUserDefinedFields(IList<string> userAttributes,
+		                                 GdbTable toOutputClass)
+		{
+			const bool allowExpressions = true;
+			if (! ValidateFieldNames(userAttributes, allowExpressions, out string message))
+			{
+				throw new InvalidOperationException(
+					$"Error adding fields to {toOutputClass.Name}: {message}");
+			}
+
+			foreach (string userAttribute in userAttributes)
+			{
+				string inputField =
+					ExpressionUtils.GetExpression(userAttribute, out string resultField);
+
+				string unqualifiedInputField =
+					GetUnqualifiedFieldName(_sourceTable, inputField);
+
+				int sourceIndex = _sourceTable.FindField(unqualifiedInputField);
+
+				if (resultField.Equals(inputField, StringComparison.InvariantCultureIgnoreCase))
+				{
+					resultField = unqualifiedInputField;
+				}
+
+				//if (resultField.Contains('.'))
+				//{
+				//	throw new InvalidOperationException(
+				//		$"Error adding fields to {toOutputClass.Name}: Result field {resultField} should be unqualified name");
+				//}
+
+				CopyField(sourceIndex, toOutputClass, _sourceTable.Fields, resultField);
+			}
+
+			if (!ExcludeBaseRowsField)
+				EnsureBaseRowField(toOutputClass);
+		}
+
+		public void AddOIDField([NotNull] GdbTable toOutputClass)
+		{
+			toOutputClass.AddFieldT(FieldUtils.CreateOIDField());
+		}
+
+		public void AddShapeField([NotNull] GdbFeatureClass toOutputClass,
+		                          esriGeometryType newGeometryType,
+		                          [NotNull] IGeometryDef geometryDef)
+		{
+			IField shapeField = FieldUtils.CreateShapeField(
+				newGeometryType,
+				geometryDef.SpatialReference, geometryDef.GridSize[0], geometryDef.HasZ,
+				geometryDef.HasM);
+
+			toOutputClass.AddFieldT(shapeField);
+		}
+		//public void AddMinimumFields([NotNull] GdbTable toOutputClass,
+		//                             [CanBeNull] IGeometryDef geometryDef = null)
+		//{
+		//	AddOIDField(toOutputClass);
+
+		//	if (geometryDef != null && toOutputClass is GdbFeatureClass featureClass)
+		//	{
+		//		AddShapeField(featureClass, );
+		//	}
+
+		//}
+
+		public bool ValidateFieldNames([NotNull] IList<string> userAttributes,
+		                               bool allowExpressions,
+		                               out string message)
+		{
+			message = null;
+
+			HashSet<string> resultFieldNames = new HashSet<string>();
+
+			foreach (string userAttribute in userAttributes)
+			{
+				string sourceField =
+					ExpressionUtils.GetExpression(userAttribute, out string resultField);
+
+				if (resultFieldNames.Contains(resultField))
+				{
+					message = $"The field name '{resultField}' is defined multiple times.";
+					return false;
+				}
+
+				resultFieldNames.Add(resultField);
+
+				var expressionTokens = ExpressionUtils.GetExpressionTokens(sourceField).ToList();
+
+				if (expressionTokens.Count > 1)
+				{
+					if (! ValidateExpression(allowExpressions, expressionTokens, sourceField,
+					                         out message))
+					{
+						return false;
+					}
+				}
+				else
+				{
+					if (! ValidateField(_sourceTable, sourceField, out message))
+					{
+						return false;
+					}
+				}
+			}
+
+			return true;
+		}
+
+		private bool ValidateExpression(bool allowExpressions,
+		                                [NotNull] List<string> expressionTokens,
+		                                string inputField,
+		                                out string message)
+		{
+			if (! allowExpressions)
+			{
+				message =
+					$"Field {inputField} is an expression. This is not yet supported.";
+				return false;
+			}
+
+			bool any = false;
+			foreach (string candidate in expressionTokens)
+			{
+				if (ValidateField(_sourceTable, candidate, out message))
+				{
+					any = true;
+				}
+			}
+
+			if (! any)
+			{
+				message =
+					$"The expression {inputField} does not contain a valid field name.";
+				return false;
+			}
+
+			message = null;
+			return true;
+		}
+
+		private void CopyField(int sourceIdx,
+		                       [NotNull] GdbTable toOutputClass,
+		                       [NotNull] IFields sourceFields,
+		                       [CanBeNull] string resultFieldName = null)
+		{
+			Assert.True(sourceIdx >= 0 && sourceIdx < sourceFields.FieldCount, $"Invalid source index: {sourceIdx}");
+			IField field = sourceFields.Field[sourceIdx];
+
+			string targetName = resultFieldName ?? field.Name;
+
+			if (OutputFieldPrefix != null)
+			{
+				targetName = OutputFieldPrefix + targetName;
+			}
+
+			if (field.Name != targetName)
+			{
+				IField clone = (IField) ((IClone) field).Clone();
+				((IFieldEdit) clone).Name_2 = targetName;
+				field = clone;
+			}
+
+			int targetIdx = toOutputClass.AddFieldT(field);
+
+			CopyIndexMatrix.Add(targetIdx, sourceIdx);
+		}
+
+		private static bool ValidateField(IReadOnlyTable sourceTable, string fieldName,
+		                                  out string message)
+		{
+			if (string.IsNullOrEmpty(fieldName))
+			{
+				message = $"Null or empty field name defined for {sourceTable.Name}";
+				return false;
+			}
+
+			string unqualifiedField = GetUnqualifiedFieldName(sourceTable, fieldName);
+
+			if (sourceTable.FindField(unqualifiedField) < 0)
+			{
+				message =
+					$"Field name {fieldName} does not exist in {sourceTable.Name}. Valid fields are: {GetFieldList(sourceTable)}";
+				return false;
+			}
+
+			// TODO: Check for illegal characters, reserved names etc.
+
+			message = null;
+			return true;
+		}
+
+		private static string GetUnqualifiedFieldName(IReadOnlyTable sourceTable, string fieldName)
+		{
+			// Repeating the name of the current table is not necessary for single tables but might be done for clarity
+
+			if (fieldName.StartsWith(sourceTable.Name, StringComparison.InvariantCultureIgnoreCase))
+			{
+				string remainder = fieldName.Substring(sourceTable.Name.Length);
+
+				return remainder.Length > 1 ? remainder.Substring(1) : null;
+			}
+
+			return fieldName;
+		}
+
+		private static string GetFieldList(IReadOnlyTable table)
+		{
+			var fieldList = DatasetUtils.GetFields(table.Fields)
+			                            .Where(f => f.Name != InvolvedRowUtils.BaseRowField)
+			                            .Select(f => f.Name).ToList();
+
+			string fieldDisplayList =
+				$"{Environment.NewLine}{StringUtils.Concatenate(fieldList, Environment.NewLine)}";
+
+			return fieldDisplayList;
+		}
+	}
+}
