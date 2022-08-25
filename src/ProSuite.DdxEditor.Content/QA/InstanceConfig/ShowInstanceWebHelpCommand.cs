400b4bfedb89380e914174eecd8cc82710958a8a
Emanuel Mahler <e.mahler@dirageosystems.ch>
2022-08-25 16:16:58 +0200

    Fix namespace declaration

--- src/ProSuite.DdxEditor.Content/QA/InstanceConfig/ShowInstanceWebHelpCommand.cs
+++ src/ProSuite.DdxEditor.Content/QA/InstanceConfig/ShowInstanceWebHelpCommand.cs
@@ -10,79 +10,80 @@ using ProSuite.DdxEditor.Framework.Properties;
 using ProSuite.DomainModel.AO.QA.TestReport;
 using ProSuite.DomainModel.Core.QA;
 
-namespace ProSuite.DdxEditor.Content.QA.InstanceConfig;
-
-public class ShowInstanceWebHelpCommand<T> : ItemCommandBase<T> where T : Item
+namespace ProSuite.DdxEditor.Content.QA.InstanceConfig
 {
-	private static readonly Image _image;
-
-	/// <summary>
-	/// Initializes the <see cref="CopyQualityConditionCommand"/> class.
-	/// </summary>
-	static ShowInstanceWebHelpCommand()
+	public class ShowInstanceWebHelpCommand<T> : ItemCommandBase<T> where T : Item
 	{
-		_image = Resources.ShowOnlineHelpCmd;
+		private static readonly Image _image;
+
+		/// <summary>
+		/// Initializes the <see cref="CopyQualityConditionCommand"/> class.
+		/// </summary>
+		static ShowInstanceWebHelpCommand()
+		{
+			_image = Resources.ShowOnlineHelpCmd;
+		}
+
+		public IApplicationController ApplicationController { get; }
+
+		public ShowInstanceWebHelpCommand([NotNull] T item,
+		                                  IApplicationController applicationController)
+			: base(item)
+		{
+			ApplicationController = applicationController;
+
+			// This could be made more generic to support Html help of other entities.
+			InstanceDescriptor descriptor = GetInstanceDescriptor(Item);
+			if (descriptor is TransformerDescriptor)
+			{
+				Text = "Transformer Documentation";
+			}
+			else if (descriptor is IssueFilterDescriptor)
+			{
+				Text = "Issue Filter Documentation";
+			}
+			else
+			{
+				Text = "Test Documentation";
+			}
+		}
+
+		#region Overrides of CommandBase
+
+		public override Image Image => _image;
+
+		public override string Text { get; }
+
+		protected override void ExecuteCore()
+		{
+			InstanceDescriptor descriptor = GetInstanceDescriptor(Item);
+
+			if (descriptor == null)
+			{
+				throw new InvalidOperationException("No instance descriptor available.");
+			}
+
+			StringWriter stringWriter = new StringWriter();
+			TestReportUtils.WriteDescriptorDoc(descriptor, stringWriter);
+
+			ApplicationController.ShowHelpForm(descriptor.TypeDisplayName,
+			                                   stringWriter.ToString());
+		}
+
+		private static InstanceDescriptor GetInstanceDescriptor(Item item)
+		{
+			var instanceConfigItem = item as InstanceConfigurationItem;
+
+			if (instanceConfigItem == null)
+			{
+				return null;
+			}
+
+			InstanceConfiguration instanceConfiguration = instanceConfigItem.GetEntity();
+			InstanceDescriptor descriptor = instanceConfiguration?.InstanceDescriptor;
+			return descriptor;
+		}
+
+		#endregion
 	}
-
-	public IApplicationController ApplicationController { get; }
-
-	public ShowInstanceWebHelpCommand([NotNull] T item,
-	                                  IApplicationController applicationController)
-		: base(item)
-	{
-		ApplicationController = applicationController;
-
-		// This could be made more generic to support Html help of other entities.
-		InstanceDescriptor descriptor = GetInstanceDescriptor(Item);
-		if (descriptor is TransformerDescriptor)
-		{
-			Text = "Transformer Documentation";
-		}
-		else if (descriptor is IssueFilterDescriptor)
-		{
-			Text = "Issue Filter Documentation";
-		}
-		else
-		{
-			Text = "Test Documentation";
-		}
-	}
-
-	#region Overrides of CommandBase
-
-	public override Image Image => _image;
-
-	public override string Text { get; }
-
-	protected override void ExecuteCore()
-	{
-		InstanceDescriptor descriptor = GetInstanceDescriptor(Item);
-
-		if (descriptor == null)
-		{
-			throw new InvalidOperationException("No instance descriptor available.");
-		}
-
-		StringWriter stringWriter = new StringWriter();
-		TestReportUtils.WriteDescriptorDoc(descriptor, stringWriter);
-
-		ApplicationController.ShowHelpForm(descriptor.TypeDisplayName,
-		                                   stringWriter.ToString());
-	}
-
-	private static InstanceDescriptor GetInstanceDescriptor(Item item)
-	{
-		var instanceConfigItem = item as InstanceConfigurationItem;
-
-		if (instanceConfigItem == null)
-		{
-			return null;
-		}
-
-		InstanceConfiguration instanceConfiguration = instanceConfigItem.GetEntity();
-		InstanceDescriptor descriptor = instanceConfiguration?.InstanceDescriptor;
-		return descriptor;
-	}
-
-	#endregion
 }
