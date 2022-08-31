8a195fdae9cf8492032a15dbf6d18389fca30019
Emanuel Mahler <e.mahler@dirageosystems.ch>
2022-08-31 21:16:36 +0200

    Convert to auto-property, add setters

--- src/ProSuite.Commons.AO/Geodatabase/AssociationDescription.cs
+++ src/ProSuite.Commons.AO/Geodatabase/AssociationDescription.cs
@@ -4,26 +4,23 @@ namespace ProSuite.Commons.AO.Geodatabase
 {
 	public abstract class AssociationDescription
 	{
-		private readonly IReadOnlyTable _table1;
-		private readonly IReadOnlyTable _table2;
-
 		protected AssociationDescription([NotNull] IReadOnlyTable table1,
 		                                 [NotNull] IReadOnlyTable table2)
 		{
-			_table1 = table1;
-			_table2 = table2;
+			Table1 = table1;
+			Table2 = table2;
 		}
 
 		/// <summary>
 		/// The 'left' table in the join.
 		/// </summary>
 		[NotNull]
-		public IReadOnlyTable Table1 => _table1;
+		public IReadOnlyTable Table1 { get; set; }
 
 		/// <summary>
 		/// The 'right' table in the join.
 		/// </summary>
 		[NotNull]
-		public IReadOnlyTable Table2 => _table2;
+		public IReadOnlyTable Table2 { get; set; }
 	}
 }
