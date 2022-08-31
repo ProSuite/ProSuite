6815b76d6cd6ea290c63d8e05ba17315f6fd2c37
Daniel Roth <d.roth@esri.ch>
2022-08-31 15:55:00 +0200

    Don't set dirty on insert row
    Helps to avoid inserting row without a value.
    Although it doesn't completely prevent from inserting
    null value.

--- src/ProSuite.DdxEditor.Content.Blazor/ViewModel/TestParameterValueCollectionViewModel.cs
+++ src/ProSuite.DdxEditor.Content.Blazor/ViewModel/TestParameterValueCollectionViewModel.cs
@@ -178,8 +178,6 @@ public class TestParameterValueCollectionViewModel : ViewModelBase, IDataGridVie
 		int i = index is > -1 ? index.Value : Values.Count;
 
 		InsertCore(row, i);
-
-		OnPropertyChanged(nameof(Values));
 	}
 
 	private void InsertDummyRow()
