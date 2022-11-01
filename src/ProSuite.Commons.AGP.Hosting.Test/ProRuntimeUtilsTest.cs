04859722b1c70f53e4009754580e490889254985
Emanuel Mahler <e.mahler@dirageosystems.ch>
2022-11-01 08:51:49 +0100

    Add version detection methods

---
+++ src/ProSuite.Commons.AGP.Hosting.Test/ProRuntimeUtilsTest.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Threading;
+using Microsoft.Win32;
+using NUnit.Framework;
+
+namespace ProSuite.Commons.AGP.Hosting.Test
+{
+	[TestFixture]
+	[Apartment(ApartmentState.STA)]
+	public class ProRuntimeUtilsTest
+	{
+		[Test]
+		public void CanCheckIsProInstalled()
+		{
+			bool isProInstalled = ProRuntimeUtils.IsProInstalled(out Version proVersion);
+
+			RegistryKey versionSubkey =
+				Registry.LocalMachine.OpenSubKey(@"SOFTWARE\ESRI\ArcGISPro", false);
+			bool versionSubkeyExists = versionSubkey != null;
+
+			Assert.AreEqual(versionSubkeyExists, isProInstalled);
+
+			bool isServerInstalled = ProRuntimeUtils.IsServerInstalled(out Version serverVersion);
+
+			Assert.IsTrue(isProInstalled || isServerInstalled);
+
+			Console.WriteLine($"ArcGIS Pro Version: {proVersion}");
+			Console.WriteLine($"ArcGIS Server Version: {serverVersion}");
+		}
+
+		[Test]
+		public void CanGetInstallDirs()
+		{
+			string installDirPro = ProRuntimeUtils.GetProInstallDir();
+			string installDirServer = ProRuntimeUtils.GetServerInstallDir();
+
+			Assert.IsTrue(installDirPro != null || installDirServer != null);
+
+			Console.WriteLine($"ArcGIS Pro InstallDir: {installDirPro}");
+			Console.WriteLine($"ArcGIS Server InstallDir: {installDirServer}");
+		}
+	}
+}
