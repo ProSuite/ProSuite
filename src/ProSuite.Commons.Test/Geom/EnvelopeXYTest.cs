24b5f3e736f8187940efc7cc2bbdf726d04e31b2
Emanuel Mahler <e.mahler@dirageosystems.ch>
2022-12-09 22:40:42 +0100

    Fix Envelope ToString again

--- src/ProSuite.Commons.Test/Geom/EnvelopeXYTest.cs
+++ src/ProSuite.Commons.Test/Geom/EnvelopeXYTest.cs
@@ -15,35 +15,48 @@ namespace ProSuite.Commons.Test.Geom
 			EnvelopeXY envelope = new EnvelopeXY(2600000.1234, 1200000.987654, 2601000.12,
 			                                     1201000.98);
 
-			// This produces the closest to double.ToString()
-			string invariantString = envelope.ToString();
+			// ToString() produces the closest to double.ToString()
+			// ToString() with no formatInfo in de-GB:
+			string toStringInDeGb = ToStringInCulture("de-GB", envelope);
+
+			Assert.AreEqual(
+				"XMin: 2600000,1234 YMin: 1200000,987654 XMax: 2601000,12 YMax: 1201000,98",
+				toStringInDeGb);
+
+			string toStringNullCulture = ToStringInCulture("de-CH", envelope);
 
 			Assert.AreEqual(
 				"XMin: 2600000.1234 YMin: 1200000.987654 XMax: 2601000.12 YMax: 1201000.98",
-				invariantString);
-
-			// Use null culture to force the F:1 formatting:
-			invariantString = envelope.ToString(null, 1);
-
-			Assert.AreEqual(
-				"XMin: 2600000.1 YMin: 1200001.0 XMax: 2601000.1 YMax: 1201001.0",
-				invariantString);
-
-			// This estimates the decimal digits to 3:
-			invariantString = envelope.ToString(null);
-
-			Assert.AreEqual(
-				"XMin: 2600000.123 YMin: 1200000.988 XMax: 2601000.120 YMax: 1201000.980",
-				invariantString);
+				toStringNullCulture);
 
 			// Invariant Culture:
 			// NOTE: The default double.ToString() produces no thousand separators,
-			// but using the invariant culture does:
-			invariantString = envelope.ToString(CultureInfo.InvariantCulture, 2);
+			// but using the invariant culture does (invariant is probably equal to en-US):
+			toStringNullCulture = envelope.ToString(CultureInfo.InvariantCulture, 2);
 
 			Assert.AreEqual(
 				"XMin: 2,600,000.12 YMin: 1,200,000.99 XMax: 2,601,000.12 YMax: 1,201,000.98",
-				invariantString);
+				toStringNullCulture);
+
+			// Fix the culture to de-CH to compare the decimal character:
+			CultureInfoUtils.ExecuteUsing(
+				CultureInfo.GetCultureInfo("de-CH"),
+				() =>
+				{
+					// Use null culture to force the F:1 formatting:
+					toStringNullCulture = envelope.ToString(null, 1);
+
+					Assert.AreEqual(
+						"XMin: 2600000.1 YMin: 1200001.0 XMax: 2601000.1 YMax: 1201001.0",
+						toStringNullCulture);
+
+					// This estimates the decimal digits to 3:
+					toStringNullCulture = envelope.ToString(null);
+
+					Assert.AreEqual(
+						"XMin: 2600000.123 YMin: 1200000.988 XMax: 2601000.120 YMax: 1201000.980",
+						toStringNullCulture);
+				});
 		}
 
 		[Test]
@@ -54,20 +67,23 @@ namespace ProSuite.Commons.Test.Geom
 
 			const string deCh = "de-CH";
 
+			CultureInfo deChCulture = CultureInfo.GetCultureInfo(deCh);
+
 			// setting current culture temporarily to de-CH:
+
+			// ToString():
 			// The default decimal digits for cartesian coordinates is 3:
 			string stringInDeCh = ToStringInCulture(deCh, envelope);
-			Assert.AreEqual(
-				"XMin: 2’600’000.123 YMin: 1’200’000.988 XMax: 2’601’000.120 YMax: 1’201’000.980",
-				stringInDeCh);
 
-			// Using culture as parameter instead current culture:
-			CultureInfo deChCulture = CultureInfo.GetCultureInfo(deCh);
+			Assert.AreEqual(
+				"XMin: 2600000.1234 YMin: 1200000.987654 XMax: 2601000.12 YMax: 1201000.98",
+				stringInDeCh);
 
 			string deChString = envelope.ToString(deChCulture);
 			Console.WriteLine(deChString);
-
-			Assert.AreEqual(stringInDeCh, deChString);
+			Assert.AreEqual(
+				"XMin: 2’600’000.123 YMin: 1’200’000.988 XMax: 2’601’000.120 YMax: 1’201’000.980",
+				deChString);
 
 			// Now with significant digits:
 			stringInDeCh = envelope.ToString(deChCulture, 2);
@@ -87,8 +103,9 @@ namespace ProSuite.Commons.Test.Geom
 			// setting current culture temporarily to de-CH:
 			// The default decimal digits for cartesian coordinates is 3:
 			string stringInDeDe = ToStringInCulture(deDe, envelope);
+
 			Assert.AreEqual(
-				"XMin: 2.600.000,123 YMin: 1.200.000,988 XMax: 2.601.000,120 YMax: 1.201.000,980",
+				"XMin: 2600000,1234 YMin: 1200000,987654 XMax: 2601000,12 YMax: 1201000,98",
 				stringInDeDe);
 
 			// Using culture as parameter instead current culture:
@@ -97,7 +114,9 @@ namespace ProSuite.Commons.Test.Geom
 			string deDeString = envelope.ToString(deDeCulture);
 			Console.WriteLine(deDeString);
 
-			Assert.AreEqual(stringInDeDe, deDeString);
+			Assert.AreEqual(
+				"XMin: 2.600.000,123 YMin: 1.200.000,988 XMax: 2.601.000,120 YMax: 1.201.000,980",
+				deDeString);
 
 			// Now with significant digits:
 			stringInDeDe = envelope.ToString(deDeCulture, 2);
@@ -145,17 +164,17 @@ namespace ProSuite.Commons.Test.Geom
 			Assert.IsTrue(envelope1.Equals(envelope2, 0.0001));
 		}
 
-		private static string ToStringInCulture(string deCh, EnvelopeXY envelope)
+		private static string ToStringInCulture(string cultureName, EnvelopeXY envelope)
 		{
 			string formatted = null;
 			CultureInfoUtils.ExecuteUsing(
-				CultureInfo.GetCultureInfo(deCh),
+				CultureInfo.GetCultureInfo(cultureName),
 				() =>
 				{
 					string cultureInfo =
 						CultureInfoUtils.GetCultureInfoDescription(CultureInfo.CurrentCulture);
 
-					formatted = envelope.ToString(CultureInfo.CurrentCulture);
+					formatted = envelope.ToString();
 
 					Console.WriteLine(formatted);
 					Console.WriteLine("... formatted using {0}", cultureInfo);
