376c9d20d3651dcd8aee7c4ba50fa8a44b838221
Emanuel Mahler <e.mahler@dirageosystems.ch>
2022-12-09 14:59:45 +0100

    Fix EnvelopeXY.ToString(), unit tests

--- src/ProSuite.Commons.Test/Geom/EnvelopeXYTest.cs
+++ src/ProSuite.Commons.Test/Geom/EnvelopeXYTest.cs
@@ -10,28 +10,100 @@ namespace ProSuite.Commons.Test.Geom
 	public class EnvelopeXYTest
 	{
 		[Test]
-		public void CanCreateString()
+		public void CanCreateStringInvariant()
 		{
 			EnvelopeXY envelope = new EnvelopeXY(2600000.1234, 1200000.987654, 2601000.12,
 			                                     1201000.98);
-			string text = envelope.ToString();
 
-			Console.WriteLine(text);
+			// This produces the closest to double.ToString()
+			string invariantString = envelope.ToString();
 
 			Assert.AreEqual(
-				"XMin: 2600000.1234 YMin: 1200000.987654 XMax: 2601000.12 YMax: 1201000.98", text);
+				"XMin: 2600000.1234 YMin: 1200000.987654 XMax: 2601000.12 YMax: 1201000.98",
+				invariantString);
 
-			Console.WriteLine(
-				CultureInfoUtils.GetCultureInfoDescription(CultureInfo.CurrentCulture));
+			// Use null culture to force the F:1 formatting:
+			invariantString = envelope.ToString(null, 1);
 
-			string formatted = null;
-			CultureInfoUtils.ExecuteUsing(CultureInfo.GetCultureInfo("de-CH"),
-			                              () => { formatted = envelope.Format(envelope, 2); });
+			Assert.AreEqual(
+				"XMin: 2600000.1 YMin: 1200001.0 XMax: 2601000.1 YMax: 1201001.0",
+				invariantString);
 
-			Console.WriteLine(formatted);
+			// This estimates the decimal digits to 3:
+			invariantString = envelope.ToString(null);
+
+			Assert.AreEqual(
+				"XMin: 2600000.123 YMin: 1200000.988 XMax: 2601000.120 YMax: 1201000.980",
+				invariantString);
+
+			// Invariant Culture:
+			// NOTE: The default double.ToString() produces no thousand separators,
+			// but using the invariant culture does:
+			invariantString = envelope.ToString(CultureInfo.InvariantCulture, 2);
+
+			Assert.AreEqual(
+				"XMin: 2,600,000.12 YMin: 1,200,000.99 XMax: 2,601,000.12 YMax: 1,201,000.98",
+				invariantString);
+		}
+
+		[Test]
+		public void CanCreateStringDeCh()
+		{
+			EnvelopeXY envelope = new EnvelopeXY(2600000.1234, 1200000.987654, 2601000.12,
+			                                     1201000.98);
+
+			const string deCh = "de-CH";
+
+			// setting current culture temporarily to de-CH:
+			// The default decimal digits for cartesian coordinates is 3:
+			string stringInDeCh = ToStringInCulture(deCh, envelope);
+			Assert.AreEqual(
+				"XMin: 2’600’000.123 YMin: 1’200’000.988 XMax: 2’601’000.120 YMax: 1’201’000.980",
+				stringInDeCh);
+
+			// Using culture as parameter instead current culture:
+			CultureInfo deChCulture = CultureInfo.GetCultureInfo(deCh);
+
+			string deChString = envelope.ToString(deChCulture);
+			Console.WriteLine(deChString);
+
+			Assert.AreEqual(stringInDeCh, deChString);
+
+			// Now with significant digits:
+			stringInDeCh = envelope.ToString(deChCulture, 2);
 			Assert.AreEqual(
 				"XMin: 2’600’000.12 YMin: 1’200’000.99 XMax: 2’601’000.12 YMax: 1’201’000.98",
-				formatted);
+				stringInDeCh);
+		}
+
+		[Test]
+		public void CanCreateStringDeDe()
+		{
+			EnvelopeXY envelope = new EnvelopeXY(2600000.1234, 1200000.987654, 2601000.12,
+			                                     1201000.98);
+
+			const string deDe = "de-DE";
+
+			// setting current culture temporarily to de-CH:
+			// The default decimal digits for cartesian coordinates is 3:
+			string stringInDeDe = ToStringInCulture(deDe, envelope);
+			Assert.AreEqual(
+				"XMin: 2.600.000,123 YMin: 1.200.000,988 XMax: 2.601.000,120 YMax: 1.201.000,980",
+				stringInDeDe);
+
+			// Using culture as parameter instead current culture:
+			CultureInfo deDeCulture = CultureInfo.GetCultureInfo(deDe);
+
+			string deDeString = envelope.ToString(deDeCulture);
+			Console.WriteLine(deDeString);
+
+			Assert.AreEqual(stringInDeDe, deDeString);
+
+			// Now with significant digits:
+			stringInDeDe = envelope.ToString(deDeCulture, 2);
+			Assert.AreEqual(
+				"XMin: 2.600.000,12 YMin: 1.200.000,99 XMax: 2.601.000,12 YMax: 1.201.000,98",
+				stringInDeDe);
 		}
 
 		[Test]
@@ -71,6 +143,24 @@ namespace ProSuite.Commons.Test.Geom
 
 			Assert.IsFalse(envelope1.Equals(envelope2));
 			Assert.IsTrue(envelope1.Equals(envelope2, 0.0001));
+		}
+
+		private static string ToStringInCulture(string deCh, EnvelopeXY envelope)
+		{
+			string formatted = null;
+			CultureInfoUtils.ExecuteUsing(
+				CultureInfo.GetCultureInfo(deCh),
+				() =>
+				{
+					string cultureInfo =
+						CultureInfoUtils.GetCultureInfoDescription(CultureInfo.CurrentCulture);
+
+					formatted = envelope.ToString(CultureInfo.CurrentCulture);
+
+					Console.WriteLine(formatted);
+					Console.WriteLine("... formatted using {0}", cultureInfo);
+				});
+			return formatted;
 		}
 	}
 }
