faacd45f092bbb897f9e5a01c2a125ce69e2c4fb
Emanuel Mahler <e.mahler@dirageosystems.ch>
2022-12-29 10:59:54 +0100

    Reduce the sphynx warnings by better indenting

--- src/ProSuite.QA.Core/Reports/PythonClassBuilderBase.cs
+++ src/ProSuite.QA.Core/Reports/PythonClassBuilderBase.cs
@@ -122,11 +122,17 @@ namespace ProSuite.QA.Core.Reports
 		                                 string conditionConstructorSignature,
 		                                 StringBuilder sb)
 		{
+			string description = factory.TestDescription ?? string.Empty;
+			description  = description.Replace(Environment.NewLine, $"{Environment.NewLine}        ")
+			                          .Replace("/", @"\/");
+
 			sb.AppendLine();
 			sb.AppendLine($"    @classmethod");
 			sb.AppendLine($"    def {methodName}({methodSignature}) -> {resultClassName}:");
 			sb.AppendLine($"        \"\"\"");
-			sb.AppendLine($"        {factory.TestDescription ?? string.Empty}        \"\"\"");
+			sb.AppendLine($"        {description}");
+			sb.AppendLine($"        \"\"\"");
+			sb.AppendLine($"        ");
 			sb.AppendLine($"        result = {resultClassName}({conditionConstructorSignature})");
 
 			foreach (TestParameter testParameter in factory.Parameters)
