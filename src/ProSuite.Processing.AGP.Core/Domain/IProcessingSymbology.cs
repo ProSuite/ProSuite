7ffcef026bac1f28cd8d33dab7a708aceb690e19
Urs-Jakob Ruetschi <u.rueetschi@esri.ch>
2023-01-24 20:10:48 +0100

    Drawing Outline CP: new UseProApiFunction option
     - default is to use our homegrown drawing outline code

--- src/ProSuite.Processing.AGP.Core/Domain/IProcessingSymbology.cs
+++ src/ProSuite.Processing.AGP.Core/Domain/IProcessingSymbology.cs
@@ -1,5 +1,6 @@
 using ArcGIS.Core.Data;
 using ArcGIS.Core.Geometry;
+using ProSuite.Commons.Essentials.CodeAnnotations;
 using ProSuite.Processing.AGP.Core.Utils;
 using ProSuite.Processing.Domain;
 
@@ -8,8 +9,13 @@ namespace ProSuite.Processing.AGP.Core.Domain
 	public interface IProcessingSymbology
 	{
 		// Abstraction over ArcObjects and ArcGIS Pro symbology -- if possible...
+		// Can be null if a feature does not draw
+		[CanBeNull]
 		Geometry QueryDrawingOutline(Feature feature, OutlineType outlineType, IMapContext mapContext);
 
+		// Get outline of a non-real feature, implemented using our own code
+		// Can be null if a feature does not draw
+		[CanBeNull]
 		Geometry QueryDrawingOutline(PseudoFeature feature, OutlineType outlineType, IMapContext mapContext);
 	}
 
