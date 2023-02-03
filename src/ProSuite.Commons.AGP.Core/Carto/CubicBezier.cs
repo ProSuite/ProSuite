868da04c4ad687b60e549b2cce4ea9cc768b5705
Urs-Jakob Ruetschi <u.rueetschi@esri.ch>
2023-02-03 13:02:44 +0100

    Support for qudratic Beziers in glyph outlines
     - convert them to cubic Beziers (degree elevation)
     - still not supported: ArcSegment
     - while at it: pass along scaleFactor instead of IMapContext

---
+++ src/ProSuite.Commons.AGP.Core/Carto/CubicBezier.cs
@@ -0,0 +1,22 @@
+namespace ProSuite.Commons.AGP.Core.Carto;
+
+public static class CubicBezier
+{
+	/// <summary>
+	/// Degree elevation from quadratic to cubic: given a quadratic
+	/// (2nd order) Bezier curve, find the cubic (3rd order) Bezier
+	/// curve that has the same shape. Let the quadratic be defined
+	/// by points Q0 Q1 Q2, and the cubic by points P0 P1 P2 P3.
+	/// Since the first and last points must be the same, we have
+	/// P0 = Q0 and P3 = Q2. This function computes the intermediate
+	/// points P1 and P2, given the three points of the quadratic curve.
+	/// See https://en.wikipedia.org/wiki/B%C3%A9zier_curve#Degree_elevation
+	/// </summary>
+	public static void FromQuadratic(Pair q0, Pair q1, Pair q2, out Pair p1, out Pair p2)
+	{
+		const double c1 = 1.0 / 3.0;
+		const double c2 = 2.0 / 3.0;
+		p1 = c1 * q0 + c2 * q1;
+		p2 = c2 * q1 + c1 * q2;
+	}
+}
--- src/ProSuite.Commons.AGP.Core/Carto/Extensions.cs
+++ src/ProSuite.Commons.AGP.Core/Carto/Extensions.cs
@@ -10,4 +10,14 @@ public static class Extensions
 		if (point is null) throw new ArgumentNullException(nameof(point));
 		return new Pair(point.X, point.Y);
 	}
+
+	public static Pair ToPair(this Coordinate2D coord)
+	{
+		return new Pair(coord.X, coord.Y);
+	}
+
+	public static Coordinate2D ToCoordinate2D(this Pair pair)
+	{
+		return new Coordinate2D(pair.X, pair.Y);
+	}
 }
