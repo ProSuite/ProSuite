56d9cbee79c08df1c896119713491b133b1ecb2c
Urs-Jakob Ruetschi <u.rueetschi@esri.ch>
2023-02-13 14:10:53 +0100

    Unit tests for (most) remaining geometric effects

--- src/ProSuite.Commons.AGP.Core.Test/GeometricEffectsTest.cs
+++ src/ProSuite.Commons.AGP.Core.Test/GeometricEffectsTest.cs
@@ -20,6 +20,105 @@ public class GeometricEffectsTest
 	}
 
 	[Test]
+	public void CanAddControlPoints()
+	{
+		// null geometry
+		Assert.IsNull(GeometricEffects.AddControlPoints(null, 120));
+
+		// non-polycurve geometry
+		var pt = AssertType<MapPoint>(GeometricEffects.AddControlPoints(Pt(100, 200), 120));
+		Assert.AreEqual(100, pt.X);
+		Assert.AreEqual(200, pt.Y);
+		Assert.AreEqual(0, pt.ID);
+
+		// right(1)up(2)downright, angles at (1) 90°, (2) 45°
+		var line = PolylineBuilderEx.CreatePolyline(
+			new[] { Pt(0, 0), Pt(10, 0), Pt(10, 10), Pt(20, 0) });
+
+		var line1 = AssertType<Polyline>(GeometricEffects.AddControlPoints(line, 30));
+		Assert.AreEqual(4, line1.PointCount);
+		Assert.AreEqual(0, line1.Points[0].ID);
+		Assert.AreEqual(0, line1.Points[1].ID);
+		Assert.AreEqual(0, line1.Points[2].ID);
+		Assert.AreEqual(0, line1.Points[3].ID);
+
+		var line2 = AssertType<Polyline>(GeometricEffects.AddControlPoints(line, 45));
+		Assert.IsTrue(line2.HasID);
+		Assert.AreEqual(4, line2.PointCount);
+		Assert.AreEqual(0, line2.Points[0].ID);
+		Assert.AreEqual(0, line2.Points[1].ID);
+		Assert.AreEqual(1, line2.Points[2].ID);
+		Assert.AreEqual(0, line2.Points[3].ID);
+
+		var line3 = AssertType<Polyline>(GeometricEffects.AddControlPoints(line, 89.99));
+		Assert.IsTrue(line3.HasID);
+		Assert.AreEqual(4, line3.PointCount);
+		Assert.AreEqual(0, line3.Points[0].ID);
+		Assert.AreEqual(0, line3.Points[1].ID);
+		Assert.AreEqual(1, line3.Points[2].ID);
+		Assert.AreEqual(0, line3.Points[3].ID);
+
+		var line4 = AssertType<Polyline>(GeometricEffects.AddControlPoints(line, 90));
+		Assert.IsTrue(line4.HasID);
+		Assert.AreEqual(4, line4.PointCount);
+		Assert.AreEqual(0, line4.Points[0].ID);
+		Assert.AreEqual(1, line4.Points[1].ID);
+		Assert.AreEqual(1, line4.Points[2].ID);
+		Assert.AreEqual(0, line4.Points[3].ID);
+
+		var line5 = AssertType<Polyline>(GeometricEffects.AddControlPoints(line, 120, 2));
+		Assert.IsTrue(line5.HasID);
+		Assert.AreEqual(4, line5.PointCount);
+		Assert.AreEqual(0, line5.Points[0].ID);
+		Assert.AreEqual(2, line5.Points[1].ID);
+		Assert.AreEqual(2, line5.Points[2].ID);
+		Assert.AreEqual(0, line5.Points[3].ID);
+	}
+
+	[Test]
+	public void CanCut()
+	{
+		// null geometry
+		Assert.IsNull(GeometricEffects.Cut(null, 3.0, 4.0));
+
+		// non-polyline geometry remains unchanged
+		var poly = PolygonBuilderEx.CreatePolygon(Env(10, 10, 20, 20));
+		var poly2 = AssertType<Polygon>(GeometricEffects.Cut(poly, 3.0, 4.0));
+		Assert.AreEqual(poly.Length, poly2.Length, 0.00001);
+
+		// two-part polyline (expect cut on each part)
+		var builder = new PolylineBuilderEx();
+		builder.AddPart(new[] { Pt(0, 0), Pt(10, 0), Pt(20, 0) });
+		builder.AddPart(new[] { Pt(10, 0), Pt(10, 10) });
+		var polyline = builder.ToGeometry();
+
+		var cut1 = AssertType<Polyline>(GeometricEffects.Cut(polyline, 2.0, 3.0));
+		Assert.AreEqual(2, cut1.PartCount);
+		Assert.AreEqual(15.0, cut1.Parts[0].Sum(s => s.Length), 0.00001);
+		Assert.AreEqual(5.0, cut1.Parts[1].Sum(s => s.Length));
+
+		// inverted cut (keep cuttings, drop inner part)
+		var cut2 = AssertType<Polyline>(GeometricEffects.Cut(polyline, 2.0, 3.0, true));
+		Assert.AreEqual(4, cut2.PartCount);
+		Assert.AreEqual(10.0, cut2.Length, 0.00001);
+
+		// cuts longer than part
+		var cut3 = AssertType<Polyline>(GeometricEffects.Cut(polyline, 5.5, 5.5));
+		Assert.AreEqual(1, cut3.PartCount);
+		Assert.AreEqual(9.0, cut3.Parts.Single().Sum(s => s.Length));
+
+		// inverted cut of more than part length
+		var cut4 = AssertType<Polyline>(GeometricEffects.Cut(polyline, 6.0, 5.0, true));
+		Assert.AreEqual(3, cut4.PartCount);
+		var lens4 = cut4.Parts.Select(p => p.Sum(s => s.Length)).OrderBy(l => l).ToArray();
+		Assert.AreEqual(5.0, lens4[0], 0.00001);
+		Assert.AreEqual(6.0, lens4[1], 0.00001);
+		Assert.AreEqual(10.0, lens4[2], 0.00001);
+
+		// NB. middleCut untested here
+	}
+
+	[Test]
 	public void CanDashesUnconstrained()
 	{
 		// line of length 80 (like the line symbol preview in Pro's Symbology pane)
@@ -83,7 +182,7 @@ public class GeometricEffectsTest
 	}
 
 	[Test]
-	public void CanDashesCustomEndOffset1()
+	public void CanDashesCustomEndOffset()
 	{
 		// line of length 80 (like the line symbol preview in Pro's Symbology pane)
 		var line = PolylineBuilderEx.CreatePolyline(new[] { Pt(0, 0), Pt(80, 0) });
@@ -269,6 +368,116 @@ public class GeometricEffectsTest
 		var ofs4 = AssertType<Polygon>(GeometricEffects.Offset(polygon, -2.0, OffsetType.Miter));
 		Assert.AreEqual(24.0, ofs4.Length, 0.00001); // negative is right (in)
 	}
+
+	[Test]
+	public void CanReverse()
+	{
+		Assert.IsNull(GeometricEffects.Reverse(null));
+
+		// neither polyline nor polygon: remains unchanged
+		var pt = AssertType<MapPoint>(GeometricEffects.Reverse(Pt(10, 20)));
+		Assert.AreEqual(10.0, pt.X, 0.0000001);
+		Assert.AreEqual(20.0, pt.Y, 0.0000001);
+
+		// Implementation is just a call to Pro SDK's ReverseOrientation(),
+		// so here we just check if it is having any effect:
+
+		var poly = PolygonBuilderEx.CreatePolygon(Env(10, 10, 20, 20));
+		Assert.AreEqual(100.0, poly.Area, 0.00001);
+
+		var rev1 = AssertType<Polygon>(GeometricEffects.Reverse(poly));
+		Assert.AreEqual(-100.0, rev1.Area, 0.00001);
+	}
+
+	[Test]
+	public void CanSuppress()
+	{
+		Assert.IsNull(GeometricEffects.Suppress(null));
+
+		// neither polyline nor polygon: remains unchanged
+		var pt = AssertType<MapPoint>(GeometricEffects.Suppress(Pt(10, 20)));
+		Assert.AreEqual(10.0, pt.X, 0.0000001);
+		Assert.AreEqual(20.0, pt.Y, 0.0000001);
+		var env = AssertType<Envelope>(GeometricEffects.Suppress(Env(10, 10, 20, 20)));
+		Assert.AreEqual(100.0, env.Area);
+		Assert.AreEqual(40.0, env.Length);
+
+		// 0  10 20 30 40 50 60 70 80
+		// o~~*--o--*~~o~~*--*~~*--o   (o Vertex, * CP)
+		var line = PolylineBuilderEx.CreatePolyline(
+			new[]
+			{
+				Pt(0, 0), CP(10, 0), Pt(20, 0), CP(30, 0), Pt(40, 0),
+				CP(50, 0), CP(60, 0), CP(70, 0), Pt(80, 0)
+			});
+
+		var sup1 = AssertType<Polyline>(GeometricEffects.Suppress(line));
+		Assert.AreEqual(3, sup1.PartCount);
+		var len1 = sup1.Parts.Select(p => p.Sum(s => s.Length)).ToArray();
+		Assert.AreEqual(10.0, len1[0], 0.00001);
+		Assert.AreEqual(20.0, len1[1], 0.00001);
+		Assert.AreEqual(10.0, len1[2], 0.00001);
+
+		var sup2 = AssertType<Polyline>(GeometricEffects.Suppress(line, true));
+		Assert.AreEqual(3, sup2.PartCount);
+		var len2 = sup2.Parts.Select(p => p.Sum(s => s.Length)).ToArray();
+		Assert.AreEqual(20.0, len2[0], 0.00001);
+		Assert.AreEqual(10.0, len2[1], 0.00001);
+		Assert.AreEqual(10.0, len2[2], 0.00001);
+
+		// 20 o---*---o
+		//    |       |      o Vertex
+		// 10 *       *      * CP
+		//    |       |      start at (0,0)
+		//  0 o---*---o
+		//    0   10  20
+		var square = PolygonBuilderEx.CreatePolygon(
+			new[]
+			{
+				Pt(0, 0), CP(0, 10), Pt(0, 20), CP(10, 20),
+				Pt(20, 20), CP(20, 10), Pt(20, 0), CP(10, 0)
+			});
+
+		var sup3 = AssertType<Polyline>(GeometricEffects.Suppress(square));
+		Assert.AreEqual(2, sup3.PartCount);
+		Assert.AreEqual(40.0, sup3.Length, 0.00001);
+		Assert.AreEqual(20.0, sup3.Parts[0].Sum(s => s.Length));
+		Assert.AreEqual(20.0, sup3.Parts[1].Sum(s => s.Length));
+		var pt31 = sup3.Parts[0][0].StartPoint;
+		Assert.AreEqual(10.0, pt31.X, 0.00001);
+		Assert.AreEqual(0.0, pt31.Y, 0.00001);
+		var pt32 = sup3.Parts[1][0].StartPoint;
+		Assert.AreEqual(10.0, pt32.X, 0.00001);
+		Assert.AreEqual(20.0, pt32.Y, 0.00001);
+
+		var sup4 = AssertType<Polyline>(GeometricEffects.Suppress(square, true));
+		Assert.AreEqual(2, sup4.PartCount);
+		Assert.AreEqual(40.0, sup4.Length, 0.00001);
+		Assert.AreEqual(20.0, sup4.Parts[0].Sum(s => s.Length));
+		Assert.AreEqual(20.0, sup4.Parts[1].Sum(s => s.Length));
+		var pt41 = sup4.Parts[0][0].StartPoint;
+		Assert.AreEqual(0.0, pt41.X, 0.00001);
+		Assert.AreEqual(10.0, pt41.Y, 0.00001);
+		var pt42 = sup4.Parts[1][0].StartPoint;
+		Assert.AreEqual(20.0, pt42.X, 0.00001);
+		Assert.AreEqual(10.0, pt42.Y, 0.00001);
+
+		// two-part polygon (30x30 square with a 10x10 hole)
+		var builder = new PolygonBuilderEx { HasID = true };
+		builder.AddPart(new[] { Pt(0, 0), Pt(0, 30), Pt(30, 30), CP(30, 0), Pt(0, 0) });
+		builder.AddPart(new[] { Pt(10, 10), Pt(20, 10), CP(20, 20), CP(10, 20), Pt(10, 10) });
+		var polygon = builder.ToGeometry();
+		Assert.AreEqual(800.0, polygon.Area, 0.00001);
+
+		var sup5 = AssertType<Polyline>(GeometricEffects.Suppress(polygon));
+		Assert.AreEqual(2, sup5.PartCount);
+		Assert.AreEqual(120.0, sup5.Length, 0.00001);
+
+		var sup6 = AssertType<Polyline>(GeometricEffects.Suppress(polygon, true));
+		Assert.AreEqual(2, sup6.PartCount);
+		Assert.AreEqual(40, sup6.Length);
+	}
+
 	#region Test utilities
 
 	private static Polyline Dashes(
@@ -356,6 +565,17 @@ public class GeometricEffectsTest
 	private static MapPoint Pt(double x, double y)
 	{
 		return MapPointBuilderEx.CreateMapPoint(x, y);
+	}
+
+	private static MapPoint CP(double x, double y, int id = 1)
+	{
+		var builder = new MapPointBuilderEx();
+		builder.HasZ = builder.HasM = false;
+		builder.X = x;
+		builder.Y = y;
+		builder.HasID = true;
+		builder.ID = id;
+		return builder.ToGeometry();
 	}
 
 	private static Envelope Env(double xMin, double yMin, double xMax, double yMax)
