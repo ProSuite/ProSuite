ee0cd45f59522d69759733429fb26215e638482d
Urs-Jakob Ruetschi <u.rueetschi@esri.ch>
2023-03-16 16:11:06 +0100

    ResetControlPoint[Pair]s (and unit test)

---
+++ src/ProSuite.Commons.AGP.Core.Test/ControlPointUtilsTest.cs
@@ -0,0 +1,229 @@
+using NUnit.Framework;
+using ProSuite.Commons.AGP.Hosting;
+using System.Linq;
+using System.Threading;
+using ArcGIS.Core.Geometry;
+using ProSuite.Commons.AGP.Core.Spatial;
+
+namespace ProSuite.Commons.AGP.Core.Test;
+
+[TestFixture]
+[Apartment(ApartmentState.STA)]
+public class ControlPointUtilsTest
+{
+	[OneTimeSetUp]
+	public void OneTimeSetUp()
+	{
+		CoreHostProxy.Initialize();
+	}
+
+	[Test]
+	public void CanSetPointID()
+	{
+		var point = MapPointBuilderEx.CreateMapPoint(1.2, 3.4);
+		Assert.IsFalse(point.HasID);
+		Assert.AreEqual(0, point.ID);
+
+		var point0 = ControlPointUtils.SetPointID(null, 42);
+		Assert.IsNull(point0);
+
+		var point1 = ControlPointUtils.SetPointID(point, null);
+		Assert.AreSame(point, point1);
+
+		var point2 = ControlPointUtils.SetPointID(point, 1);
+		Assert.IsTrue(point2.HasID);
+		Assert.AreEqual(1, point2.ID);
+
+		var point3 = ControlPointUtils.SetPointID(point, 123);
+		Assert.IsTrue(point3.HasID);
+		Assert.AreEqual(123, point3.ID);
+
+		var point4 = ControlPointUtils.SetPointID(point, 0);
+		Assert.IsTrue(point4.HasID);
+		Assert.AreEqual(0, point4.ID);
+	}
+
+	[Test]
+	public void CanSegmentSetPointID()
+	{
+		var line = LineBuilderEx.CreateLineSegment(Pt(0, 0), Pt(4, 2));
+		Assert.False(line.StartPoint.HasID);
+		Assert.AreEqual(0, line.StartPoint.ID);
+		Assert.False(line.EndPoint.HasID);
+		Assert.AreEqual(0, line.EndPoint.ID);
+		
+		var line0 = ControlPointUtils.SetPointID((Segment) null, null, null);
+		Assert.IsNull(line0); // convenience: null is passed through
+
+		var line1 = ControlPointUtils.SetPointID(line, 1, null);
+		Assert.True(line1.StartPoint.HasID);
+		Assert.AreEqual(1, line1.StartPoint.ID);
+		Assert.False(line1.EndPoint.HasID);
+		Assert.AreEqual(0, line1.EndPoint.ID);
+
+		var line2 = ControlPointUtils.SetPointID(line, null, 2);
+		Assert.False(line2.StartPoint.HasID);
+		Assert.AreEqual(0, line2.StartPoint.ID);
+		Assert.True(line2.EndPoint.HasID);
+		Assert.AreEqual(2, line2.EndPoint.ID);
+
+		var line3 = ControlPointUtils.SetPointID(line, 3, 4);
+		Assert.True(line3.StartPoint.HasID);
+		Assert.AreEqual(3, line3.StartPoint.ID);
+		Assert.True(line3.EndPoint.HasID);
+		Assert.AreEqual(4, line3.EndPoint.ID);
+
+		var line4 = ControlPointUtils.SetPointID(line, null, null);
+		Assert.AreSame(line, line4); // nothing updated: return same instance
+	}
+
+	[Test]
+	public void CanBuilderSetPointID()
+	{
+		var env = GeometryFactory.CreateEnvelope(1, 2, 3, 4);
+		var polygon = GeometryFactory.CreatePolygon(env);
+		var builder = new PolygonBuilderEx(polygon);
+
+		const int partIndex = 0;
+		int pointCount = builder.GetPointCount(partIndex);
+		for (int j = 0; j < pointCount; j++)
+		{
+			int id = j + 1;
+			builder.SetPointID(partIndex, j, id);
+		}
+
+		var polygon1 = builder.ToGeometry();
+		Assert.True(polygon1.HasID); // enabled by builder.SetPointID()
+
+		var part = polygon1.Parts.Single();
+		for (int j = 0; j < part.Count; j++)
+		{
+			var segment = part[j];
+			if (j == 0)
+			{
+				// Beware: the ring's start point was overwritten
+				// when we set the point ID on the ring's end point!
+				Assert.IsTrue(segment.StartPoint.HasID);
+				Assert.AreEqual(part.Count + 1, segment.StartPoint.ID);
+			}
+			else
+			{
+				Assert.IsTrue(segment.StartPoint.HasID);
+				Assert.AreEqual(j + 1, segment.StartPoint.ID);
+				Assert.IsTrue(segment.EndPoint.HasID);
+				Assert.AreEqual(j + 2, segment.EndPoint.ID);
+			}
+		}
+	}
+
+	[Test]
+	public void CanResetControlPoints()
+	{
+		var polygon = PolygonBuilderEx.CreatePolygon(
+			new[] { Pt(0, 0, 1), Pt(0, 3, 2), Pt(5, 5), Pt(5, 0, 1), Pt(0, 0, 1) });
+
+		var p1 = ControlPointUtils.ResetControlPoints(polygon, out int c1);
+		Assert.NotNull(p1);
+		Assert.AreEqual(3, c1); // first/last point count as one for this change
+		Assert.IsTrue(p1.Points.All(p => p.ID == 0));
+
+		var p2 = ControlPointUtils.ResetControlPoints(polygon, out int c2, 2);
+		Assert.NotNull(p2);
+		Assert.AreEqual(1, c2);
+		Assert.AreEqual(2, p2.Points.Count(p => p.ID == 0));
+		Assert.AreEqual(3, p2.Points.Count(p => p.ID == 1));
+
+		var p3 = ControlPointUtils.ResetControlPoints(polygon, out int c3, 1);
+		Assert.NotNull(p3);
+		Assert.AreEqual(2, c3); // first/last point count as one for this change
+		Assert.AreEqual(4, p3.Points.Count(p => p.ID == 0));
+		Assert.AreEqual(1, p3.Points.Count(p => p.ID == 2));
+
+		var polyline = PolylineBuilderEx.CreatePolyline(polygon);
+
+		var p4 = ControlPointUtils.ResetControlPoints(polyline, out int c4, 1);
+		Assert.NotNull(p4);
+		Assert.AreEqual(3, c4); // now first/last count as two because path (not ring)
+		Assert.AreEqual(4, p4.Points.Count(p => p.ID == 0));
+		Assert.AreEqual(1, p4.Points.Count(p => p.ID == 2));
+
+		Assert.IsNull(ControlPointUtils.ResetControlPoints((Polygon) null, out _));
+
+		// It also works straight on a MultipartBuilderEx instance:
+		var builder = new PolylineBuilderEx(polyline);
+		var c5 = ControlPointUtils.ResetControlPoints(builder);
+		Assert.AreEqual(4, c5); // polyline builder: start/end counted separately
+	}
+
+	[Test]
+	public void CanResetControlPointPairs()
+	{
+		var polygon1 = PolygonBuilderEx.CreatePolygon(
+			new[]
+			{
+				Pt(0, 0), Pt(0, 3, 1), Pt(0, 6, 0), Pt(3, 6, 1),
+				Pt(6, 6), Pt(6, 4, 2), Pt(6, 2, 2), Pt(6, 0), Pt(0, 0)
+			});
+
+		var p1 = ControlPointUtils.ResetControlPointPairs(polygon1, out int c1);
+		Assert.NotNull(p1);
+		Assert.AreEqual(4, c1);
+		Assert.AreEqual(9, p1.Points.Count(p => p.ID == 0));
+
+		var p2 = ControlPointUtils.ResetControlPointPairs(polygon1, out int c2, 1);
+		Assert.NotNull(p2);
+		Assert.AreEqual(2, c2);
+		Assert.AreEqual(7, p2.Points.Count(p => p.ID == 0));
+		Assert.AreEqual(2, p2.Points.Count(p => p.ID == 2));
+
+		var peri1 = PolygonBuilderEx.CreatePolygon(
+			new[] { Pt(5, 1), Pt(5, 5), Pt(7, 5), Pt(7, 1) });
+		var p3 = ControlPointUtils.ResetControlPointPairs(polygon1, out int c3, -1, peri1);
+		Assert.NotNull(p3);
+		Assert.AreEqual(2, c3);
+		Assert.AreEqual(7, p3.Points.Count(p => p.ID == 0));
+		Assert.AreEqual(2, p3.Points.Count(p => p.ID == 1));
+
+		// pairs are reset if at least one endpoint is in perimeter:
+
+		var peri2 = PolygonBuilderEx.CreatePolygon(
+			new[] { Pt(2, 3), Pt(2, 7), Pt(7, 7), Pt(7, 3) });
+		var p4 = ControlPointUtils.ResetControlPointPairs(polygon1, out int c4, -1, peri2);
+		Assert.NotNull(p4);
+		Assert.AreEqual(4, c4);
+		Assert.AreEqual(9, p4.Points.Count(p => p.ID == 0));
+
+		// Special case: gap across Start/End, which really two gaps
+
+		var polygon2 = PolygonBuilderEx.CreatePolygon(
+			new[]
+			{
+				Pt(0, 0, 1), Pt(0, 3, 1), Pt(0, 6), Pt(6, 6),
+				Pt(6, 0), Pt(3, 0, 1), Pt(0, 0)
+			});
+
+		var p5 = ControlPointUtils.ResetControlPointPairs(polygon2, out int c5);
+		Assert.NotNull(p5);
+		Assert.AreEqual(3, c5); // 2nd (last) gap was not closed!
+		Assert.AreEqual(7, p5.Points.Count(p => p.ID == 0));
+
+		// It also works straight on a MultipartBuilderEx instance:
+		var builder = new PolylineBuilderEx(polygon2);
+		var c6 = ControlPointUtils.ResetControlPointPairs(builder);
+		Assert.AreEqual(3, c6);
+	}
+
+	#region Test utils
+
+	private static MapPoint Pt(double x, double y)
+	{
+		return MapPointBuilderEx.CreateMapPoint(x, y);
+	}
+
+	private static MapPoint Pt(double x, double y, int id)
+	{
+		return MapPointBuilderEx.CreateMapPoint(x, y, false, 0, false, 0, true, id);
+	}
+
+	#endregion
+}
--- src/ProSuite.Commons.AGP.Core/Spatial/ControlPointUtils.cs
+++ src/ProSuite.Commons.AGP.Core/Spatial/ControlPointUtils.cs
@@ -1,122 +1,259 @@
 using System;
 using ArcGIS.Core.Geometry;
-using ArcGIS.Core.Internal.Geometry;
 using ProSuite.Commons.Essentials.Assertions;
 
-namespace ProSuite.Commons.AGP.Core.Spatial
+namespace ProSuite.Commons.AGP.Core.Spatial;
+
+/// <summary>
+/// A control point is a vertex with a non-zero ID value.
+/// They may be used with certain geometric effects to
+/// create nice cartography. Due to the immutable geometries
+/// in the Pro SDK, the code here is very different from our
+/// ArcObjects-based ControlPointUtils.
+/// </summary>
+public static class ControlPointUtils
 {
-	public static class ControlPointUtils
+	public static void SetPointID(this MultipartBuilderEx builder,
+	                              int partIndex, int pointIndex, int pointID)
 	{
-		// Note: very different from ArcObjects-based ControlPointUtils (immutable geoms)
+		if (builder is null)
+			throw new ArgumentNullException(nameof(builder));
+		if (!(0 <= partIndex && partIndex < builder.PartCount))
+			throw new ArgumentOutOfRangeException(nameof(partIndex));
 
-		public static void SetPointID(this MultipartBuilderEx builder,
-		                              int partIndex, int pointIndex, int pointID)
+		// assume point i is between segments i-1 and i
+		var segmentCount = builder.GetSegmentCount(partIndex);
+		if (!(0 <= pointIndex && pointIndex <= segmentCount))
+			throw new ArgumentOutOfRangeException(nameof(pointIndex));
+
+		if (pointID != 0)
 		{
-			if (builder is null)
-				throw new ArgumentNullException(nameof(builder));
-			if (!(0 <= partIndex && partIndex < builder.PartCount))
-				throw new ArgumentOutOfRangeException(nameof(partIndex));
+			builder.HasID = true;
+		}
 
-			// assume point i is between segments i-1 and i
-			var segmentCount = builder.GetSegmentCount(partIndex);
-			if (!(0 <= pointIndex && pointIndex <= segmentCount))
-				throw new ArgumentOutOfRangeException(nameof(pointIndex));
-
-			if (pointID != 0)
-			{
-				builder.HasID = true;
-			}
-
-			switch (builder)
-			{
-				case PolylineBuilderEx:
-					// update StartPoint on segment i (if exists)
-					// update EndPoint on segment i-1 (if exists)
-					if (pointIndex < segmentCount)
-						UpdateStartPoint(builder, partIndex, pointIndex, pointID);
-					pointIndex -= 1;
-					if (pointIndex >= 0)
-						UpdateEndPoint(builder, partIndex, pointIndex, pointID);
-					break;
-
-				case PolygonBuilderEx:
-					// update StartPoint of segment i (mod N)
-					// update EndPoint of segment (i-1) (mod N)
-					pointIndex %= segmentCount;
+		switch (builder)
+		{
+			case PolylineBuilderEx:
+				// update StartPoint on segment i (if exists)
+				// update EndPoint on segment i-1 (if exists)
+				if (pointIndex < segmentCount)
 					UpdateStartPoint(builder, partIndex, pointIndex, pointID);
-					pointIndex -= 1;
-					if (pointIndex < 0) pointIndex += segmentCount;
+				pointIndex -= 1;
+				if (pointIndex >= 0)
 					UpdateEndPoint(builder, partIndex, pointIndex, pointID);
-					break;
-				default:
-					throw new AssertionException(
-						"multipart builder is neither polygon nor polyline builder");
-			}
+				break;
+
+			case PolygonBuilderEx:
+				// update StartPoint of segment i (mod N)
+				// update EndPoint of segment (i-1) (mod N)
+				pointIndex %= segmentCount;
+				UpdateStartPoint(builder, partIndex, pointIndex, pointID);
+				pointIndex -= 1;
+				if (pointIndex < 0) pointIndex += segmentCount;
+				UpdateEndPoint(builder, partIndex, pointIndex, pointID);
+				break;
+			default:
+				throw new AssertionException(
+					"multipart builder is neither polygon nor polyline builder");
+		}
+	}
+
+	private static void UpdateStartPoint(MultipartBuilderEx builder, int k, int i, int value)
+	{
+		var segment = builder.GetSegment(k, i);
+		var updated = SetPointID(segment, value, null);
+		builder.ReplaceSegment(k, i, updated);
+	}
+
+	private static void UpdateEndPoint(MultipartBuilderEx builder, int k, int i, int value)
+	{
+		var segment = builder.GetSegment(k, i);
+		var updated = SetPointID(segment, null, value);
+		builder.ReplaceSegment(k, i, updated);
+	}
+
+	public static T SetPointID<T>(
+		T segment, int? startPointID, int? endPointID) where T : Segment
+	{
+		if (segment is null) return null;
+
+		if (! startPointID.HasValue && ! endPointID.HasValue)
+		{
+			return segment; // nothing to update
 		}
 
-		private static void UpdateStartPoint(MultipartBuilderEx builder, int k, int i, int value)
+		SegmentBuilderEx builder = segment switch
 		{
-			var segment = builder.GetSegment(k, i);
-			var updated = SetPointID(segment, value, null);
-			builder.ReplaceSegment(k, i, updated);
+			LineSegment line => new LineBuilderEx(line),
+			EllipticArcSegment arc => new EllipticArcBuilderEx(arc),
+			CubicBezierSegment cubic => new CubicBezierBuilderEx(cubic),
+			_ => throw new ArgumentException(@"Unknown segment type", nameof(segment))
+		};
+
+		builder.StartPoint = SetPointID(builder.StartPoint, startPointID);
+		builder.EndPoint = SetPointID(builder.EndPoint, endPointID);
+
+		return (T) builder.ToSegment();
+	}
+
+	public static MapPoint SetPointID(MapPoint point, int? pointID)
+	{
+		if (point is null) return null;
+
+		if (! pointID.HasValue)
+		{
+			return point; // nothing to update
 		}
 
-		private static void UpdateEndPoint(MultipartBuilderEx builder, int k, int i, int value)
-		{
-			var segment = builder.GetSegment(k, i);
-			var updated = SetPointID(segment, null, value);
-			builder.ReplaceSegment(k, i, updated);
-		}
+		var builder = new MapPointBuilderEx(point)
+		              { HasID = true, ID = pointID.Value };
 
-		public static Segment SetPointID(Segment template, int? startPointID, int? endPointID)
+		return builder.ToGeometry();
+	}
+
+	/// <summary>
+	/// Reset control points by setting the vertex ID to zero.
+	/// If the given <paramref name="value"/> is non-negative,
+	/// reset only control points with this ID value. If the
+	/// given <paramref name="perimeter"/> is non-null, reset
+	/// only control points within this perimeter. Return the
+	/// updated geometry and the number of modified vertices.
+	/// </summary>
+	public static Polygon ResetControlPoints(
+		Polygon shape, out int count, int value = -1, Polygon perimeter = null)
+	{
+		count = 0;
+		if (shape is null) return null;
+		if (!shape.HasID) return shape;
+
+		var builder = new PolygonBuilderEx(shape);
+		count = ResetControlPoints(builder, value, perimeter);
+		return builder.ToGeometry();
+	}
+
+	/// <summary>
+	/// See <see cref="ResetControlPoints(Polygon,out int,int,Polygon)"/>
+	/// </summary>
+	public static Polyline ResetControlPoints(
+		Polyline shape, out int count, int value = -1, Polygon perimeter = null)
+	{
+		count = 0;
+		if (shape is null) return null;
+		if (! shape.HasID) return shape;
+
+		var builder = new PolylineBuilderEx(shape);
+		count = ResetControlPoints(builder, value, perimeter);
+		return builder.ToGeometry();
+	}
+
+	public static int ResetControlPoints(
+		MultipartBuilderEx builder, int value = -1, Polygon perimeter = null)
+	{
+		if (builder is null)
+			throw new ArgumentNullException(nameof(builder));
+
+		int count = 0;
+
+		int partCount = builder.PartCount;
+		for (int k = 0; k < partCount; k++)
 		{
-			if (! startPointID.HasValue && ! endPointID.HasValue)
+			int pointCount = builder.GetPointCount(k);
+			for (int j = 0; j < pointCount; j++)
 			{
-				return template; // nothing to update
-			}
-
-			switch (template)
-			{
-				case LineSegment lineSegment:
-					using (var builder = new LineBuilder(lineSegment))
-					{
-						builder.StartPoint = SetPointID(builder.StartPoint, startPointID);
-						builder.EndPoint = SetPointID(builder.EndPoint, endPointID);
-						return builder.ToSegment();
-					}
-
-				case EllipticArcSegment ellipticSegment:
-					using (var builder = new EllipticArcBuilder(ellipticSegment))
-					{
-						builder.StartPoint = SetPointID(builder.StartPoint, startPointID);
-						builder.EndPoint = SetPointID(builder.EndPoint, endPointID);
-						return builder.ToSegment();
-					}
-
-				case CubicBezierSegment bezierSegment:
-					using (var builder = new CubicBezierBuilder(bezierSegment))
-					{
-						builder.StartPoint = SetPointID(builder.StartPoint, startPointID);
-						builder.EndPoint = SetPointID(builder.EndPoint, endPointID);
-						return builder.ToSegment();
-					}
-
-				default:
-					throw new ArgumentException(@"Unknown segment type", nameof(template));
+				var point = builder.GetPoint(k, j);
+				if ((value < 0 || point.ID == value) && Within(point, perimeter))
+				{
+					if (point.ID != 0) count += 1;
+					point = SetPointID(point, 0);
+					builder.SetPoint(k, j, point);
+				}
 			}
 		}
 
-		public static MapPoint SetPointID(MapPoint template, int? pointID)
+		return count;
+	}
+
+	/// <summary>
+	/// Reset control point pairs of the given <paramref name="value"/>
+	/// to zero. If a <paramref name="perimeter"/> is given, only reset
+	/// pairs if either (or both) endpoints are within the perimeter.
+	/// If <paramref name="value"/> is negative, all values match.
+	/// </summary>
+	public static Polygon ResetControlPointPairs(
+		Polygon shape, out int count, int value = -1, Polygon perimeter = null)
+	{
+		count = 0;
+		if (shape is null) return null;
+		if (! shape.HasID) return shape;
+
+		var builder = new PolygonBuilderEx(shape);
+		count = ResetControlPointPairs(builder, value, perimeter);
+		return builder.ToGeometry();
+	}
+
+	public static int ResetControlPointPairs(
+		MultipartBuilderEx builder, int value = -1, Polygon perimeter = null)
+	{
+		int count = 0;
+
+		int partCount = builder.PartCount;
+		for (int k = 0; k < partCount; k++)
 		{
-			if (! pointID.HasValue)
+			int gapStartIndex = -1;
+			var gapStartInPerimeter = false;
+
+			int pointCount = builder.GetPointCount(k);
+			for (int j = 0; j < pointCount; j++)
 			{
-				return template; // nothing to update
+				var point = builder.GetPoint(k, j);
+
+				bool isMyCP = value < 0 && point.ID != 0 ||
+				              value >= 0 && point.ID == value;
+				bool inPerimeter = Within(point, perimeter);
+
+				if (isMyCP)
+				{
+					if (gapStartIndex >= 0)
+					{
+						if (gapStartInPerimeter || inPerimeter)
+						{
+							point = SetPointID(point, 0);
+							builder.SetPoint(k, j, point);
+							point = builder.GetPoint(k, gapStartIndex);
+							point = SetPointID(point, 0);
+							builder.SetPoint(k, gapStartIndex, point);
+							count += 2;
+						}
+
+						gapStartIndex = -1;
+						gapStartInPerimeter = false;
+					}
+					else
+					{
+						gapStartIndex = j;
+						gapStartInPerimeter = inPerimeter;
+					}
+				}
+
+				bool isLastInPart = j == pointCount - 1;
+				if (isLastInPart && gapStartIndex >= 0)
+				{
+					// An unpaired control point? Remove it:
+					point = builder.GetPoint(k, gapStartIndex);
+					point = SetPointID(point, 0);
+					builder.SetPoint(k, gapStartIndex, point);
+					count += 1;
+				}
 			}
-
-			var builder = new MapPointBuilderEx(template)
-			              { HasID = true, ID = pointID.Value };
-
-			return builder.ToGeometry();
 		}
+
+		return count;
+	}
+
+	private static bool Within(MapPoint point, Polygon perimeter)
+	{
+		if (point is null) return false;
+		if (perimeter is null) return true;
+		return GeometryUtils.Contains(perimeter, point);
 	}
 }
