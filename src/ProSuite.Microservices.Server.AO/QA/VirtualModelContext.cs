a653d47efc652ba442b299dcffbf0b87b3b05a97
Emanuel Mahler <e.mahler@dirageosystems.ch>
2023-04-14 13:56:30 +0200

    Simplify interface IQueryTableContext

--- src/ProSuite.Microservices.Server.AO/QA/VirtualModelContext.cs
+++ src/ProSuite.Microservices.Server.AO/QA/VirtualModelContext.cs
@@ -195,7 +195,7 @@ namespace ProSuite.Microservices.Server.AO.QA
 
 		#region IQueryTableContext members
 
-		public string GetRelationshipClassName(string associationName, Model model)
+		public string GetRelationshipClassName(string associationName, DdxModel model)
 		{
 			GdbWorkspace workspace =
 				_virtualWorkspaces.FirstOrDefault(w => w.WorkspaceHandle == model.Id);
@@ -210,11 +210,11 @@ namespace ProSuite.Microservices.Server.AO.QA
 			return _dataRequestFunc != null || _virtualWorkspaces != null;
 		}
 
-		public ITable OpenQueryTable(string relationshipClassName,
-		                             DdxModel model,
-		                             IList<IReadOnlyTable> tables,
-		                             JoinType joinType,
-		                             string whereClause)
+		public IReadOnlyTable OpenQueryTable(string relationshipClassName,
+		                                     DdxModel model,
+		                                     IList<IReadOnlyTable> tables,
+		                                     JoinType joinType,
+		                                     string whereClause)
 		{
 			if (_dataRequestFunc != null)
 			{
@@ -233,7 +233,9 @@ namespace ProSuite.Microservices.Server.AO.QA
 					throw new InvalidOperationException($"Workspace for model {model} not found");
 				}
 
-				return workspace.OpenQueryTable(relationshipClassName);
+				ITable queryTable = workspace.OpenQueryTable(relationshipClassName);
+
+				return ReadOnlyTableFactory.Create(queryTable);
 			}
 
 			throw new NotImplementedException();
@@ -251,11 +253,11 @@ namespace ProSuite.Microservices.Server.AO.QA
 			return workspace;
 		}
 
-		private ITable GetRemoteQueryTable([NotNull] string relationshipClassName,
-		                                   [NotNull] DdxModel model,
-		                                   [NotNull] IList<IReadOnlyTable> tables,
-		                                   JoinType joinType,
-		                                   [CanBeNull] string whereClause)
+		private IReadOnlyTable GetRemoteQueryTable([NotNull] string relationshipClassName,
+		                                           [NotNull] DdxModel model,
+		                                           [NotNull] IList<IReadOnlyTable> tables,
+		                                           JoinType joinType,
+		                                           [CanBeNull] string whereClause)
 		{
 			var dataRequest = new DataVerificationResponse
 			                  {
