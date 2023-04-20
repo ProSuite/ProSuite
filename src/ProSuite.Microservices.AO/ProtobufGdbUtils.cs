c28db9dbd9f7ae1ce3ec35419172236a0e5c6999
Emanuel Mahler <e.mahler@dirageosystems.ch>
2023-04-20 12:06:00 +0200

    Correct subtype magic string

--- src/ProSuite.Microservices.AO/ProtobufGdbUtils.cs
+++ src/ProSuite.Microservices.AO/ProtobufGdbUtils.cs
@@ -18,9 +18,10 @@ namespace ProSuite.Microservices.AO
 	{
 		/// <summary>
 		/// The name of the domain property of the FieldMsg that notifies the client that the
-		/// respective field is the subtype field.
+		/// respective field is the subtype field. This could be removed if the proto model is
+		/// extended.
 		/// </summary>
-		public const string SubtypeDomainName = "__BaseRows__";
+		public const string SubtypeDomainName = "__SubType__";
 
 		public static GdbObjRefMsg ToGdbObjRefMsg(IFeature feature)
 		{
