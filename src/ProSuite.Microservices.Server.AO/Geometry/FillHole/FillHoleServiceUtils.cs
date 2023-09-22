6c6b16093e9b23762babcb4c3664451bad89a41e
Emanuel Mahler <e.mahler@dirageosystems.ch>
2023-09-22 13:42:09 +0200

    Implement hole calculation service

---
+++ src/ProSuite.Microservices.Server.AO/Geometry/FillHole/FillHoleServiceUtils.cs
@@ -0,0 +1,91 @@
+using System.Collections.Generic;
+using System.Diagnostics;
+using ESRI.ArcGIS.esriSystem;
+using ESRI.ArcGIS.Geodatabase;
+using ESRI.ArcGIS.Geometry;
+using Google.Protobuf.Collections;
+using ProSuite.Commons.Essentials.CodeAnnotations;
+using ProSuite.Commons.Logging;
+using ProSuite.Microservices.AO;
+using ProSuite.Microservices.Definitions.Geometry;
+using ProSuite.Microservices.Definitions.Shared;
+
+namespace ProSuite.Microservices.Server.AO.Geometry.FillHole
+{
+	public static class FillHoleServiceUtils
+	{
+		private static readonly IMsg _msg = Msg.ForCurrentClass();
+
+		[NotNull]
+		public static CalculateHolesResponse CalculateHoles(
+			[NotNull] CalculateHolesRequest request,
+			[CanBeNull] ITrackCancel trackCancel)
+		{
+			var watch = Stopwatch.StartNew();
+
+			GetFeatures(request.SourceFeatures, request.ClassDefinitions,
+			            out IList<IFeature> sourceFeatures);
+
+			_msg.DebugStopTiming(watch, "Unpacked feature lists from request params");
+
+			var result = new CalculateHolesResponse();
+
+			if (request.UnionFeatures)
+			{
+				HolesMsg holeMsg = CalculateHoleMsg(sourceFeatures, trackCancel);
+				result.Holes.Add(holeMsg);
+			}
+			else
+			{
+				// Feature-by-feature:
+				foreach (IFeature feature in sourceFeatures)
+				{
+					HolesMsg holeMsg =
+						CalculateHoleMsg(new List<IFeature> { feature }, trackCancel);
+					holeMsg.OriginalFeatureRef = ProtobufGdbUtils.ToGdbObjRefMsg(feature);
+					result.Holes.Add(holeMsg);
+				}
+			}
+
+			return result;
+		}
+
+		private static HolesMsg CalculateHoleMsg(IList<IFeature> sourceFeatures,
+		                                         ITrackCancel trackCancel)
+		{
+			IList<IPolygon> holes = FillHoleUtils.CalculateHoles(
+				sourceFeatures, null, trackCancel);
+
+			var watch = Stopwatch.StartNew();
+
+			var holeMsg = new HolesMsg();
+
+			var shapeFormat = ShapeMsg.FormatOneofCase.EsriShape;
+			var srFormat = SpatialReferenceMsg.FormatOneofCase.SpatialReferenceEsriXml;
+
+			foreach (IPolygon holePolygon in holes)
+			{
+				holeMsg.HoleGeometries.Add(ProtobufGeometryUtils.ToShapeMsg(
+					                           holePolygon, shapeFormat, srFormat));
+			}
+
+			_msg.DebugStopTiming(watch, "Packed holes into response");
+			return holeMsg;
+		}
+
+		private static void GetFeatures([NotNull] RepeatedField<GdbObjectMsg> requestSourceFeatures,
+		                                [NotNull] RepeatedField<ObjectClassMsg> classDefinitions,
+		                                [NotNull] out IList<IFeature> sourceFeatures)
+		{
+			Stopwatch watch = Stopwatch.StartNew();
+
+			sourceFeatures = ProtobufConversionUtils.FromGdbObjectMsgList(requestSourceFeatures,
+				classDefinitions);
+
+			_msg.DebugStopTiming(
+				watch,
+				"CalculateReshapeLinesImpl: Unpacked {0} source features from request params",
+				sourceFeatures.Count);
+		}
+	}
+}
