794a698fe23f1f70e4a898e8e6bfe43e1c732545
Felix Arnet <Felix.Arnet@ebp.ch>
2024-01-26 14:51:41 +0100

    Bugfix

--- src/ProSuite.QA.Tests.Test/Transformer/TrZAssignTest.cs
+++ src/ProSuite.QA.Tests.Test/Transformer/TrZAssignTest.cs
@@ -9,60 +9,61 @@ using ProSuite.QA.Tests.Test.Construction;
 using ProSuite.QA.Tests.Test.TestRunners;
 using ProSuite.QA.Tests.Transformers;
 
-namespace ProSuite.QA.Tests.Test.Transformer;
-
-public class TrZAssignTest
+namespace ProSuite.QA.Tests.Test.Transformer
 {
-	private string _simpleGdbPath;
-
-	[OneTimeSetUp]
-	public void SetupFixture()
+	public class TrZAssignTest
 	{
-		TestUtils.InitializeLicense(activateAdvancedLicense: true);
-		_simpleGdbPath = Commons.AO.Test.TestData.GetGdb1Path();
-	}
+		private string _simpleGdbPath;
 
-	[OneTimeTearDown]
-	public void TearDownFixture()
-	{
-		TestUtils.ReleaseLicense();
-	}
-
-	[Test]
-	public void CanUseTrZAssign()
-	{
-		int idLv95 = (int)esriSRProjCS2Type.esriSRProjCS_CH1903Plus_LV95;
-		ISpatialReference srLv95 = SpatialReferenceUtils.CreateSpatialReference(idLv95, true);
-
-		IFeatureWorkspace ws = TestWorkspaceUtils.CreateInMemoryWorkspace("ws");
-
-		IFeatureClass fc = DatasetUtils.CreateSimpleFeatureClass(ws, "lv95",
-			FieldUtils.CreateFields(
-				FieldUtils.CreateOIDField(),
-				FieldUtils.CreateShapeField(
-					"Shape", esriGeometryType.esriGeometryPolyline, srLv95, 1000)));
-
+		[OneTimeSetUp]
+		public void SetupFixture()
 		{
-			IFeature f = fc.CreateFeature();
-			f.Shape = CurveConstruction.StartLine(2600000, 1200000)
-									   .LineTo(2601000, 1201000).Curve;
-			f.Store();
+			TestUtils.InitializeLicense(activateAdvancedLicense: true);
+			_simpleGdbPath = Commons.AO.Test.TestData.GetGdb1Path();
 		}
 
-		IFeatureWorkspace rws = WorkspaceUtils.OpenFeatureWorkspace(_simpleGdbPath);
-		IRasterDataset rds = ((IRasterWorkspace2) rws).OpenRasterDataset("DHM200_Bern");
-
-		RasterDatasetReference rasterRef = new RasterDatasetReference((IRasterDataset2) rds);
-
-		IReadOnlyFeatureClass roFc = ReadOnlyTableFactory.Create(fc);
-		TrZAssign tr = new TrZAssign(roFc, rasterRef);
-		Qa3dConstantZ test =
-			new Qa3dConstantZ(tr.GetTransformed(), 0);
-
+		[OneTimeTearDown]
+		public void TearDownFixture()
 		{
-			var runner = new QaContainerTestRunner(10000, test);
-			runner.Execute();
-			Assert.AreEqual(1, runner.Errors.Count);
+			TestUtils.ReleaseLicense();
+		}
+
+		[Test]
+		public void CanUseTrZAssign()
+		{
+			int idLv95 = (int) esriSRProjCS2Type.esriSRProjCS_CH1903Plus_LV95;
+			ISpatialReference srLv95 = SpatialReferenceUtils.CreateSpatialReference(idLv95, true);
+
+			IFeatureWorkspace ws = TestWorkspaceUtils.CreateInMemoryWorkspace("ws");
+
+			IFeatureClass fc = DatasetUtils.CreateSimpleFeatureClass(ws, "lv95",
+				FieldUtils.CreateFields(
+					FieldUtils.CreateOIDField(),
+					FieldUtils.CreateShapeField(
+						"Shape", esriGeometryType.esriGeometryPolyline, srLv95, 1000)));
+
+			{
+				IFeature f = fc.CreateFeature();
+				f.Shape = CurveConstruction.StartLine(2600000, 1200000)
+				                           .LineTo(2601000, 1201000).Curve;
+				f.Store();
+			}
+
+			IFeatureWorkspace rws = WorkspaceUtils.OpenFeatureWorkspace(_simpleGdbPath);
+			IRasterDataset rds = ((IRasterWorkspace2) rws).OpenRasterDataset("DHM200_Bern");
+
+			RasterDatasetReference rasterRef = new RasterDatasetReference((IRasterDataset2) rds);
+
+			IReadOnlyFeatureClass roFc = ReadOnlyTableFactory.Create(fc);
+			TrZAssign tr = new TrZAssign(roFc, rasterRef);
+			Qa3dConstantZ test =
+				new Qa3dConstantZ(tr.GetTransformed(), 0);
+
+			{
+				var runner = new QaContainerTestRunner(10000, test);
+				runner.Execute();
+				Assert.AreEqual(1, runner.Errors.Count);
+			}
 		}
 	}
 }
