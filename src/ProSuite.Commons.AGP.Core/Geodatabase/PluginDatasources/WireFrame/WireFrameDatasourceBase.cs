939b2bfdfdfcb50a1a932feed5c8b344236a1803
Urs-Jakob Ruetschi <u.rueetschi@esri.ch>
2024-03-04 15:41:34 +0100

    Throw GeodatabaseException from Plugin Datasource
    - Pro handles this case and shows red exclamation mark
    - not explicitly documented, but mentioned in a "Guide"
      document and done so in a sample; tested with Pro 3.2

--- src/ProSuite.Commons.AGP.Core/Geodatabase/PluginDatasources/WireFrame/WireFrameDatasourceBase.cs
+++ src/ProSuite.Commons.AGP.Core/Geodatabase/PluginDatasources/WireFrame/WireFrameDatasourceBase.cs
@@ -1,9 +1,8 @@
 using System;
 using System.Collections.Generic;
-using System.Runtime.CompilerServices;
 using System.Web;
+using ArcGIS.Core.Data.Exceptions;
 using ArcGIS.Core.Data.PluginDatastore;
-using ProSuite.Commons.Essentials.Assertions;
 using ProSuite.Commons.Essentials.CodeAnnotations;
 using ProSuite.Commons.Logging;
 
@@ -29,58 +28,62 @@ namespace ProSuite.Commons.AGP.Core.Geodatabase.PluginDatasources.WireFrame
 
 		public override void Open([NotNull] Uri connectionPath) // "open workspace"
 		{
-			Try(() =>
+			// Note: throw a GeodatabaseException if there's a problem opening
+
+			if (connectionPath is null)
+				throw new ArgumentNullException(nameof(connectionPath));
+
+			if (_msg.IsVerboseDebugEnabled)
 			{
-				Assert.ArgumentNotNull(connectionPath, nameof(connectionPath));
+				_msg.VerboseDebug($"WireFrame plugin: opening datasource {connectionPath}");
+			}
 
-				_msg.Debug($"WireFrame Datasource: Trying to open {connectionPath}");
+			// Empirical: when opening a project (.aprx) with a saved layer using our Plugin
+			// Datasource, the connectionPath will be prepended with the project file's
+			// directory path and two times URL encoded (e.g., ' ' => %20 => %2520)!
 
-				// Empirical: when opening a project (.aprx) with a saved layer using our Plugin
-				// Datasource, the connectionPath will be prepended with the project file's
-				// directory path and two times URL encoded (e.g., ' ' => %20 => %2520)!
+			// TODO: Consider just using the map name and ignore the prepended path to adapt to moved project folder?
+			// TODO: Or, ignore the connectionPath altogether -- we're on the active map, always!
 
-				// TODO: Consider just using the map name and ignore the prepended path to adapt to moved project folder?
+			var path = connectionPath.IsAbsoluteUri
+				           ? connectionPath.LocalPath
+				           : connectionPath.ToString();
 
-				var path = connectionPath.IsAbsoluteUri
-					           ? connectionPath.LocalPath
-					           : connectionPath.ToString();
+			path = HttpUtility.UrlDecode(path);
+			path = HttpUtility.UrlDecode(path);
 
-				path = HttpUtility.UrlDecode(path);
-				path = HttpUtility.UrlDecode(path);
-
-				_mapId = path;
-			}, "Error opening wire frame data source");
+			_mapId = path;
 		}
 
 		public override void Close()
 		{
 			if (_msg.IsVerboseDebugEnabled)
 			{
-				_msg.VerboseDebug($"{nameof(WireFrameDatasourceBase)}.{nameof(Close)}");
+				_msg.VerboseDebug("WireFrame plugin: closing datasource");
 			}
 		}
 
 		public override PluginTableTemplate OpenTable([NotNull] string name)
 		{
-			WireFrameTable result = null;
-			Try(() =>
-			    {
-				    Assert.ArgumentNotNull(name, nameof(name));
+			// Note: the name given is one of those returned by GetTableNames()
+			// Note: throw a GeodatabaseException if there's a problem opening
+			//       (Pro should show the red exclamation mark next to the layer)
 
-				    // The given name is one of those returned by GetTableNames()
-				    _msg.Debug($"Open wire frame table '{name}'");
+			if (name is null)
+				throw new ArgumentNullException(nameof(name));
 
-				    if (name == _tableNames[0])
-				    {
-					    result = new WireFrameTable(_mapId, name);
-				    }
-				    else
-				    {
-					    _msg.Warn($"Cannot find data source of wire frame table: {name}");
-				    }
-			    }, $"Error opening wire frame table {name}");
+			if (_msg.IsVerboseDebugEnabled)
+			{
+				_msg.VerboseDebug($"WireFrame plugin: opening table {name}");
+			}
 
-			return result; // TODO is null ok? shouldn't we throw a GeodatabaseException or similar?
+			if (string.Equals(name, _tableNames[0]))
+			{
+				return new WireFrameTable(_mapId, name);
+			}
+
+			_msg.Warn($"Wire frame data source has no such table: {name}");
+			throw new GeodatabaseException($"No such table in wire frame datasource: {name}");
 		}
 
 		public override IReadOnlyList<string> GetTableNames()
@@ -93,22 +96,9 @@ namespace ProSuite.Commons.AGP.Core.Geodatabase.PluginDatasources.WireFrame
 			return false;
 		}
 
-		private static void Try([NotNull] Action action,
-		                        [NotNull] string message,
-		                        [CallerMemberName] string caller = null)
+		public override string GetDatasourceDescription(bool inPluralForm)
 		{
-			Assert.ArgumentNotNull(action, nameof(action));
-
-			try
-			{
-				_msg.VerboseDebug(() => $"WireFrameDataSource.{caller}");
-
-				action();
-			}
-			catch (Exception e)
-			{
-				_msg.Warn(message, e);
-			}
+			return inPluralForm ? "WireFrame Datasources" : "WireFrame Datasource";
 		}
 	}
 }
