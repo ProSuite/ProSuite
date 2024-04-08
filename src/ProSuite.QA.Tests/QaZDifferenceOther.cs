2ee696f72c18c0b024c27b3fe6901889566d97a9
Paola Dürlewanger <p.duerlewanger@dirageosystems.ch>
2024-04-08 15:34:15 +0200

    Move ZComparisonMethod to Tests.Definitions/ParameterTypes

--- src/ProSuite.QA.Tests/QaZDifferenceOther.cs
+++ src/ProSuite.QA.Tests/QaZDifferenceOther.cs
@@ -8,6 +8,7 @@ using ProSuite.QA.Core;
 using ProSuite.QA.Core.IssueCodes;
 using ProSuite.QA.Core.TestCategories;
 using ProSuite.QA.Tests.Documentation;
+using ProSuite.QA.Tests.ParameterTypes;
 using ProSuite.QA.Tests.SpatialRelations;
 
 namespace ProSuite.QA.Tests
