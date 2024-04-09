6d0669dbc1cc82fdad8896a4ba117c372334533e
Daniel Roth <d.roth@esri.ch>
2024-04-09 12:02:56 +0200

    Work in progress...

--- src/ProSuite.AGP.Editing/Annotation/ToolBase.cs
+++ src/ProSuite.AGP.Editing/Annotation/ToolBase.cs
@@ -1,8 +1,10 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Input;
 using ArcGIS.Core.CIM;
 using ArcGIS.Core.Data;
 using ArcGIS.Core.Geometry;
@@ -10,6 +12,7 @@ using ArcGIS.Desktop.Framework.Threading.Tasks;
 using ArcGIS.Desktop.Mapping;
 using ArcGIS.Desktop.Mapping.Events;
 using ProSuite.AGP.Editing.Picker;
+using ProSuite.AGP.Editing.Properties;
 using ProSuite.Commons.AGP.Carto;
 using ProSuite.Commons.AGP.Core.Spatial;
 using ProSuite.Commons.AGP.Selection;
@@ -23,6 +26,7 @@ namespace ProSuite.AGP.Editing.Annotation;
 public abstract class ToolBase : MapTool
 {
 	private static readonly IMsg _msg = Msg.ForCurrentClass();
+
 	private readonly SketchGeometryType _defaultSketchGeometryType;
 
 	protected ToolBase(SketchGeometryType sketchGeometryType)
@@ -36,11 +40,29 @@ public abstract class ToolBase : MapTool
 		SketchType = sketchGeometryType;
 
 		_defaultSketchGeometryType = sketchGeometryType;
+
+		//_defaultSelectionCursor = GetCursor(Resources.SelectionToolNormal);
+		SelectionCursorCore = GetCursor(Resources.CreateFeatureInPickedClassCursor);
+		ConstructionCursorCore = GetCursor(Resources.EditSketchCrosshair);
 	}
 
-	protected bool AllowNoSelection => false;
+	private List<Key> HandledKeys { get; } = new() { Key.Escape, Key.F2 };
+
+	protected virtual Cursor SelectionCursorCore { get; }
+
+	protected virtual Cursor ConstructionCursorCore { get; }
+
+	protected virtual bool AllowNoSelection => false;
+
+	#region abstract
 
 	protected abstract bool AllowMultiSelection { get; }
+
+	protected abstract Task HandleEscapeAsync();
+
+	protected abstract bool CanSelectFromLayerCore([NotNull] BasicFeatureLayer layer);
+
+	#endregion
 
 	#region overrides
 
@@ -58,16 +80,6 @@ public abstract class ToolBase : MapTool
 		await ViewUtils.TryAsync(OnToolDeactivateCoreAsync(hasMapViewChanged), _msg);
 	}
 
-	private Task OnToolDeactivateCoreAsync(bool hasMapViewChanged)
-	{
-		return Task.FromResult(0);
-	}
-
-	protected virtual Task OnToolActivateCoreAsync(bool hasMapViewChanged)
-	{
-		return Task.FromResult(0);
-	}
-
 	protected sealed override async Task OnSelectionChangedAsync(MapSelectionChangedEventArgs args)
 	{
 		await ViewUtils.TryAsync(OnSelectionChangedCoreAsync(args), _msg);
@@ -76,13 +88,70 @@ public abstract class ToolBase : MapTool
 	/// <summary>
 	/// Is on the GUI thread.
 	/// </summary>
-	/// <returns></returns>
 	protected sealed override async Task<bool> OnSketchModifiedAsync()
 	{
 		return await ViewUtils.TryAsync(OnSketchModifiedCoreAsync(), _msg);
 	}
 
 	protected sealed override async Task<bool> OnSketchCompleteAsync(Geometry geometry)
+	{
+		return await ViewUtils.TryAsync(OnSketchCompleteCoreAsync(geometry), _msg);
+	}
+
+	protected override void OnToolKeyDown(MapViewKeyEventArgs args)
+	{
+		if (HandledKeys.Contains(args.Key))
+		{
+			args.Handled = true;
+		}
+	}
+
+	protected sealed override async Task HandleKeyDownAsync(MapViewKeyEventArgs args)
+	{
+		if (args.Key == Key.F2)
+		{
+			await FinishSketchAsync();
+		}
+
+		if (args.Key == Key.Escape)
+		{
+			await ViewUtils.TryAsync(HandleEscapeAsync, _msg);
+
+			SetSelectionCursor();
+		}
+
+		await ViewUtils.TryAsync(HandleKeyDownCoreAsync(args), _msg);
+	}
+
+	#endregion
+
+	#region tool
+
+	protected virtual Task OnToolActivateCoreAsync(bool hasMapViewChanged)
+	{
+		return Task.CompletedTask;
+	}
+
+	protected virtual Task OnToolDeactivateCoreAsync(bool hasMapViewChanged)
+	{
+		return Task.CompletedTask;
+	}
+
+	protected virtual Task HandleKeyDownCoreAsync(MapViewKeyEventArgs args)
+	{
+		return Task.CompletedTask;
+	}
+
+	#endregion
+
+	#region sketch
+
+	protected virtual Task<bool> OnSketchModifiedCoreAsync()
+	{
+		return Task.FromResult(true);
+	}
+
+	private async Task<bool> OnSketchCompleteCoreAsync(Geometry geometry)
 	{
 		if (geometry == null || geometry.IsEmpty)
 		{
@@ -99,71 +168,16 @@ public abstract class ToolBase : MapTool
 
 			if (CanUseSelection(selection, notifications))
 			{
-				return await ViewUtils.TryAsync(
-					       OnConstructionSketchCompleteAsync(geometry, selection), _msg);
+				return await OnConstructionSketchCompleteAsync(geometry, selection);
 			}
 		}
 
-		return await ViewUtils.TryAsync(OnSelectionSketchCompleteAsync(geometry), _msg);
-	}
-
-	protected virtual async Task OnSelectionChangedCoreAsync(
-		[NotNull] MapSelectionChangedEventArgs args)
-	{
-		if (! MapUtils.HasSelection(ActiveMapView.Map))
-		{
-			ResetSketchAppearance();
-			await ClearSketchAsync();
-			return;
-		}
-
-		Dictionary<BasicFeatureLayer, List<long>> selectionByLayer =
-			SelectionUtils.GetSelection<BasicFeatureLayer>(args.Selection);
-
-		var notifications = new NotificationCollection();
-
-		if (! CanUseSelection(selectionByLayer, notifications))
-		{
-			ResetSketchAppearance();
-			await ClearSketchAsync();
-			return;
-		}
-
-		IDictionary<BasicFeatureLayer, List<Feature>> selection =
-			GetApplicableSelectedFeatures(selectionByLayer, notifications);
-
-		await ProcessSelectionAsync(selection);
-	}
-
-	protected virtual Task<bool> OnSketchModifiedCoreAsync()
-	{
-		return Task.FromResult(true);
+		return await OnSelectionSketchCompleteAsync(geometry);
 	}
 
 	/// <summary>
 	/// Is on the GUI thread.
 	/// </summary>
-	/// <param name="geometry"></param>
-	/// <returns></returns>
-	protected virtual Task<bool> OnSelectionSketchCompleteAsync2([NotNull] Geometry geometry)
-	{
-		// Return true or false does not seem to have an effect.
-		// OnSelectionChangedAsync gets called anyway.
-
-		return QueuedTask.Run(() =>
-		{
-			// todo daro ToList() needed?
-			IEnumerable<FeatureSelectionBase> featureSelection =
-				FindFeatureSelection(geometry).ToList();
-
-			long selectFeatures = SelectionUtils.SelectFeatures(featureSelection,
-			                                                    SelectionCombinationMethod.New,
-			                                                    clearExistingSelection: true);
-
-			return selectFeatures != 0;
-		});
-	}
-
 	protected virtual async Task<bool> OnSelectionSketchCompleteAsync([NotNull] Geometry geometry)
 	{
 		List<IPickableItem> items =
@@ -222,113 +236,100 @@ public abstract class ToolBase : MapTool
 		return Task.FromResult(true);
 	}
 
-	#endregion
-
-	#region base
-
-	private IEnumerable<FeatureSelectionBase> FindFeatureSelection(
-		[NotNull] Geometry geometry,
-		SpatialRelationship spatialRelationship = SpatialRelationship.Intersects)
+	private void ResetSketchAppearance()
 	{
-		var featureFinder = new FeatureFinder(ActiveMapView)
-		                    {
-			                    SpatialRelationship = spatialRelationship,
-			                    DelayFeatureFetching = true
-		                    };
-
-		return featureFinder.FindFeaturesByLayer(
-			geometry,
-			fl => CanSelectFromLayer(fl));
+		SketchType = _defaultSketchGeometryType;
+		SketchSymbol = null;
 	}
 
-	/// <summary>
-	/// Whether the required selection can only contain editable features.
-	/// </summary>
-	protected virtual bool CanSelectOnlyEditFeatures()
+	private void UpdateSketchAppearance([NotNull] FeatureLayer layer, [CanBeNull] Feature feature)
 	{
-		return true;
+		if (feature == null)
+		{
+			ResetSketchAppearance();
+			return;
+		}
+
+		GeometryType geometryType = feature.GetShape().GeometryType;
+		long oid = feature.GetObjectID();
+
+		switch (geometryType)
+		{
+			case GeometryType.Point:
+				SketchType = SketchGeometryType.Point;
+				break;
+			case GeometryType.Polyline:
+				SketchType = SketchGeometryType.Line;
+				break;
+			case GeometryType.Polygon:
+				SketchType = SketchGeometryType.Polygon;
+				break;
+			case GeometryType.Unknown:
+			case GeometryType.Envelope:
+			case GeometryType.Multipoint:
+			case GeometryType.Multipatch:
+			case GeometryType.GeometryBag:
+				break;
+			default:
+				throw new ArgumentOutOfRangeException(nameof(geometryType), geometryType, null);
+		}
+
+		CIMSymbol symbol = layer.LookupSymbol(oid, ActiveMapView);
+
+		if (symbol == null)
+		{
+			_msg.Debug(
+				$"Cannot set sketch symbol: no symbol found in layer {layer.Name} for oid {oid}.");
+		}
+		else
+		{
+			SketchSymbol = symbol.MakeSymbolReference();
+		}
+
+		UpdateSketchAppearanceCore(layer, feature);
 	}
 
-	protected virtual bool CanSelectGeometryType(GeometryType geometryType)
-	{
-		return true;
-	}
-
-	protected abstract bool CanSelectFromLayerCore([NotNull] BasicFeatureLayer layer);
-
-	private bool CanSelectFromLayer([CanBeNull] BasicFeatureLayer layer,
-	                                NotificationCollection notifications = null)
-	{
-		if (layer == null)
-		{
-			NotificationUtils.Add(notifications, "No feature layer");
-			return false;
-		}
-
-		string layerName = layer.Name;
-
-		if (! LayerUtils.IsVisible(layer))
-		{
-			NotificationUtils.Add(notifications, $"Layer {layerName} not visible");
-			return false;
-		}
-
-		if (! layer.IsSelectable)
-		{
-			NotificationUtils.Add(notifications, $"Layer {layerName} not selectable");
-			return false;
-		}
-
-		if (CanSelectOnlyEditFeatures() &&
-		    ! layer.IsEditable)
-		{
-			NotificationUtils.Add(notifications, $"Layer {layerName} not editable");
-			return false;
-		}
-
-		if (! CanSelectGeometryType(
-			    GeometryUtils.TranslateEsriGeometryType(layer.ShapeType)))
-		{
-			NotificationUtils.Add(notifications,
-			                      $"Layer {layerName}: Cannot use geometry type {layer.ShapeType}");
-			return false;
-		}
-
-		return CanSelectFromLayerCore(layer);
-	}
+	protected virtual void UpdateSketchAppearanceCore([NotNull] FeatureLayer layer,
+	                                                  [NotNull] Feature feature) { }
 
 	#endregion
 
-	#region process selection
+	#region selection
 
-	protected bool CanUseSelection(
-		[NotNull] IDictionary<BasicFeatureLayer, List<long>> selectionByLayer,
-		[NotNull] NotificationCollection notifications)
+	private async Task OnSelectionChangedCoreAsync(
+		[NotNull] MapSelectionChangedEventArgs args)
 	{
-		int count = selectionByLayer.Values.Sum(features => features.Count);
-
-		if (count == 0 && ! AllowNoSelection)
+		if (! MapUtils.HasSelection(ActiveMapView.Map))
 		{
-			_msg.Debug(
-				$"Cannot use selection: tool has to have a selection and selection count is {count}");
-			return false;
+			SetSelectionCursor();
+
+			ResetSketchAppearance();
+			await ClearSketchAsync();
+
+			return;
 		}
 
-		if (count > 1 && ! AllowMultiSelection)
+		Dictionary<BasicFeatureLayer, List<long>> selectionByLayer =
+			SelectionUtils.GetSelection<BasicFeatureLayer>(args.Selection);
+
+		var notifications = new NotificationCollection();
+
+		if (! CanUseSelection(selectionByLayer, notifications))
 		{
-			_msg.Debug(
-				$"Cannot use selection: multi selection is not allowed and selection count is {count}");
-			return false;
+			SetSelectionCursor();
+
+			ResetSketchAppearance();
+			await ClearSketchAsync();
+
+			return;
 		}
 
-		return CanUseSelectionCore(selectionByLayer, notifications);
-	}
+		SetConstructionCursor();
 
-	protected virtual bool CanUseSelectionCore(
-		IDictionary<BasicFeatureLayer, List<long>> selectionByLayer,
-		NotificationCollection notifications)
-	{
-		return true;
+		IDictionary<BasicFeatureLayer, List<Feature>> selection =
+			GetApplicableSelectedFeatures(selectionByLayer, notifications);
+
+		await ProcessSelectionAsync(selection);
 	}
 
 	private Task ProcessSelectionAsync(
@@ -352,13 +353,29 @@ public abstract class ToolBase : MapTool
 		[NotNull] IDictionary<BasicFeatureLayer, List<Feature>> featuresByLayer,
 		[CanBeNull] CancelableProgressor progressor = null)
 	{
-		return Task.FromResult(0);
+		return Task.CompletedTask;
 	}
 
-	// todo daro rename
-	protected async Task<IDictionary<T, List<long>>> GetApplicableSelection<T>()
+	private IEnumerable<FeatureSelectionBase> FindFeatureSelection(
+		[NotNull] Geometry geometry,
+		SpatialRelationship spatialRelationship = SpatialRelationship.Intersects)
+	{
+		var featureFinder = new FeatureFinder(ActiveMapView)
+		                    {
+			                    SpatialRelationship = spatialRelationship,
+			                    DelayFeatureFetching = true
+		                    };
+
+		return featureFinder.FindFeaturesByLayer(
+			geometry,
+			fl => CanSelectFromLayer(fl));
+	}
+
+	private async Task<IDictionary<T, List<long>>> GetApplicableSelection<T>()
 		where T : BasicFeatureLayer
 	{
+		// todo daro rename, revise method
+
 		Dictionary<T, List<long>> selectionByLayer =
 			await QueuedTask.Run(() => SelectionUtils.GetSelection<T>(ActiveMapView.Map));
 
@@ -410,61 +427,103 @@ public abstract class ToolBase : MapTool
 		return result;
 	}
 
-	#endregion
-
-	protected void ResetSketchAppearance()
+	private bool CanSelectFromLayer([CanBeNull] BasicFeatureLayer layer,
+	                                NotificationCollection notifications = null)
 	{
-		SketchType = _defaultSketchGeometryType;
-		SketchSymbol = null;
+		if (layer == null)
+		{
+			NotificationUtils.Add(notifications, "No feature layer");
+			return false;
+		}
+
+		string layerName = layer.Name;
+
+		if (! LayerUtils.IsVisible(layer))
+		{
+			NotificationUtils.Add(notifications, $"Layer {layerName} not visible");
+			return false;
+		}
+
+		if (! layer.IsSelectable)
+		{
+			NotificationUtils.Add(notifications, $"Layer {layerName} not selectable");
+			return false;
+		}
+
+		if (CanSelectOnlyEditFeatures() &&
+		    ! layer.IsEditable)
+		{
+			NotificationUtils.Add(notifications, $"Layer {layerName} not editable");
+			return false;
+		}
+
+		if (! CanSelectGeometryType(
+			    GeometryUtils.TranslateEsriGeometryType(layer.ShapeType)))
+		{
+			NotificationUtils.Add(notifications,
+			                      $"Layer {layerName}: Cannot use geometry type {layer.ShapeType}");
+			return false;
+		}
+
+		return CanSelectFromLayerCore(layer);
 	}
 
-	protected void UpdateSketchAppearance([NotNull] FeatureLayer layer, [CanBeNull] Feature feature)
+	private bool CanUseSelection(
+		[NotNull] IDictionary<BasicFeatureLayer, List<long>> selectionByLayer,
+		[NotNull] NotificationCollection notifications)
 	{
-		if (feature == null)
-		{
-			ResetSketchAppearance();
-			return;
-		}
+		int count = selectionByLayer.Values.Sum(features => features.Count);
 
-		GeometryType geometryType = feature.GetShape().GeometryType;
-		long oid = feature.GetObjectID();
-
-		switch (geometryType)
-		{
-			case GeometryType.Point:
-				SketchType = SketchGeometryType.Point;
-				break;
-			case GeometryType.Polyline:
-				SketchType = SketchGeometryType.Line;
-				break;
-			case GeometryType.Polygon:
-				SketchType = SketchGeometryType.Polygon;
-				break;
-			case GeometryType.Unknown:
-			case GeometryType.Envelope:
-			case GeometryType.Multipoint:
-			case GeometryType.Multipatch:
-			case GeometryType.GeometryBag:
-				break;
-			default:
-				throw new ArgumentOutOfRangeException(nameof(geometryType), geometryType, null);
-		}
-
-		CIMSymbol symbol = layer.LookupSymbol(oid, ActiveMapView);
-
-		if (symbol == null)
+		if (count == 0 && ! AllowNoSelection)
 		{
 			_msg.Debug(
-				$"Cannot set sketch symbol: no symbol found in layer {layer.Name} for oid {oid}.");
-		}
-		else
-		{
-			SketchSymbol = symbol.MakeSymbolReference();
+				$"Cannot use selection: tool has to have a selection and selection count is {count}");
+			return false;
 		}
 
-		UpdateSketchAppearanceCore(layer, feature);
+		if (count > 1 && ! AllowMultiSelection)
+		{
+			_msg.Debug(
+				$"Cannot use selection: multi selection is not allowed and selection count is {count}");
+			return false;
+		}
+
+		return CanUseSelectionCore(selectionByLayer, notifications);
 	}
 
-	protected virtual void UpdateSketchAppearanceCore([NotNull] FeatureLayer layer,
-	                                                  [NotNull] Feature feature) { }
+	protected virtual bool CanUseSelectionCore(
+		IDictionary<BasicFeatureLayer, List<long>> selectionByLayer,
+		NotificationCollection notifications)
+	{
+		return true;
+	}
+
+	protected virtual bool CanSelectOnlyEditFeatures()
+	{
+		return true;
+	}
+
+	protected virtual bool CanSelectGeometryType(GeometryType geometryType)
+	{
+		return true;
+	}
+
+	#endregion
+
+	private void SetSelectionCursor()
+	{
+		//new MemoryStream()
+		Cursor = SelectionCursorCore;
+	}
+
+	private void SetConstructionCursor()
+	{
+		Cursor = ConstructionCursorCore;
+	}
+
+	[NotNull]
+	private static Cursor GetCursor([NotNull] byte[] bytes)
+	{
+		return new Cursor(new MemoryStream(bytes));
+	}
 }
