6be5d3b1291e7ead4e43afb76fa5ca1eb2577718
Emanuel Mahler <e.mahler@dirageosystems.ch>
2024-07-02 22:39:18 +0200

    Remove duplicate

--- src/ProSuite.ArcGIS.Geodatabase/IDataset.cs
+++ src/ProSuite.ArcGIS.Geodatabase/IDataset.cs
@@ -34,34 +34,3 @@ namespace ESRI.ArcGIS.Geodatabase
 		void Rename(string Name);
 	}
 }
-
-public interface IDataset
-{
-	string Name { get; }
-
-	IName FullName { get; }
-
-	string BrowseName { get; set; }
-
-	esriDatasetType Type { get; }
-
-	string Category { get; }
-
-	IEnumerable<IDataset> Subsets { get; }
-
-	IWorkspace Workspace { get; }
-
-	//IPropertySet PropertySet { get; }
-
-	bool CanCopy();
-
-	//IDataset Copy(string copyName, IWorkspace copyWorkspace);
-
-	bool CanDelete();
-
-	void Delete();
-
-	bool CanRename();
-
-	void Rename(string Name);
-}
