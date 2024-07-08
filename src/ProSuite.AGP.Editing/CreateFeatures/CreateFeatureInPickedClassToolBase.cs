85b952dbea078cc1cbeaf714e8a081a59e6d88dc
Paola Dürlewanger <p.duerlewanger@dirageosystems.ch>
2024-07-08 16:28:04 +0200

    Fix Sketch Geometry (Edit operation failed: a polygon part has no defined zs) in CreateFeatureInPickedClassToolBase

--- src/ProSuite.AGP.Editing/CreateFeatures/CreateFeatureInPickedClassToolBase.cs
+++ src/ProSuite.AGP.Editing/CreateFeatures/CreateFeatureInPickedClassToolBase.cs
@@ -113,7 +113,7 @@ public abstract class CreateFeatureInPickedClassToolBase : ToolBase
 		{
 			Inspector inspector = new Inspector();
 			await inspector.LoadAsync(layer, selectedOid);
-			ProSuite.Commons.AGP.Core.Spatial.GeometryUtils.Simplify(geometry);
+			geometry=ProSuite.Commons.AGP.Core.Spatial.GeometryUtils.Simplify(geometry);
 			inspector.Shape = geometry;
 
 			Attribute subtype = inspector.SubtypeAttribute;
