3f444ebeff40d3597755ca02b56cc57d6546a134
Emanuel Mahler <e.mahler@dirageosystems.ch>
2024-08-23 07:53:30 +0200

    Add IApplicableAttributes interface

---
+++ src/ProSuite.GIS.Geodatabase/IApplicableAttributes.cs
@@ -0,0 +1,21 @@
+using ESRI.ArcGIS.Geodatabase;
+using ProSuite.Commons.Essentials.CodeAnnotations;
+
+namespace ProSuite.GIS.Geodatabase
+{
+	public interface IApplicableAttributes
+	{
+		bool HasNonApplicableAttributes([NotNull] IObjectClass objectClass);
+
+		bool IsApplicable([NotNull] IObjectClass objectClass, int fieldIndex, int? subtype);
+
+		bool IsNonApplicableForAnySubtype([NotNull] IObjectClass objectClass, int fieldIndex);
+
+		object GetNonApplicableValue([NotNull] IObjectClass objectClass, int fieldIndex);
+
+		bool IsNonApplicableValue([NotNull] IObjectClass objectClass, int fieldIndex,
+		                          object value);
+
+		void ClearCache();
+	}
+}
