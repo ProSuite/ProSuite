3b82380223ab7d9f5d1a0db1cb32e6c8a8f9ed65
Markus Bedel <m.bedel@esri.ch>
2024-09-24 18:42:03 +0200

    add support for mobile geodatabases cont.

--- src/ProSuite.Commons.AGP/Gdb/GdbWorkspaceIdentity.cs
+++ src/ProSuite.Commons.AGP/Gdb/GdbWorkspaceIdentity.cs
@@ -29,7 +29,6 @@ namespace ProSuite.Commons.AGP.Gdb
 			switch (connector)
 			{
 				case DatabaseConnectionProperties connectionProperties:
-
 					ConnectionString = connectionString;
 					WorkspaceFactory = WorkspaceFactory.SDE;
 					break;
@@ -41,6 +40,11 @@ namespace ProSuite.Commons.AGP.Gdb
 					ConnectionString = fileSystemConnection.Path.ToString();
 					WorkspaceFactory = WorkspaceFactory.Shapefile;
 					break;
+				case MobileGeodatabaseConnectionPath mobileGeodatabaseConnectionPath:
+					ConnectionString = mobileGeodatabaseConnectionPath.Path.ToString();
+					WorkspaceFactory = WorkspaceFactory.SQLite;
+					break;
+
 				default:
 					throw new NotImplementedException(
 						$"connector {connector.GetType()} is not implemented");
