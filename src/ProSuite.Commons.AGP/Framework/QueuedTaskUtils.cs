a9205a0e40651b8f21d27a77688141e4fa8f93e2
Emanuel Mahler <e.mahler@dirageosystems.ch>
2024-10-31 23:57:41 +0100

    Await async lambdas, log potential exception

--- src/ProSuite.Commons.AGP/Framework/QueuedTaskUtils.cs
+++ src/ProSuite.Commons.AGP/Framework/QueuedTaskUtils.cs
@@ -2,6 +2,7 @@ using System;
 using System.Threading.Tasks;
 using ArcGIS.Desktop.Framework.Threading.Tasks;
 using ProSuite.Commons.Essentials.CodeAnnotations;
+using ProSuite.Commons.Logging;
 
 namespace ProSuite.Commons.AGP.Framework
 {
@@ -15,48 +16,84 @@ namespace ProSuite.Commons.AGP.Framework
 	/// 3 progressor types: none, Progressor, CancelableProgressor</remarks>
 	public static class QueuedTaskUtils
 	{
+		private static readonly IMsg _msg = Msg.ForCurrentClass();
+
 		public static Task Run(
 			[NotNull] Action action, Progressor progressor = null,
 			TaskCreationOptions options = TaskCreationOptions.None)
 		{
-			if (progressor is null)
-				return QueuedTask.Run(action, options);
-			if (progressor is CancelableProgressor cancelableProgressor)
-				return QueuedTask.Run(action, cancelableProgressor, options);
-			return QueuedTask.Run(action, progressor, options);
+			try
+			{
+				if (progressor is null)
+					return QueuedTask.Run(action, options);
+				if (progressor is CancelableProgressor cancelableProgressor)
+					return QueuedTask.Run(action, cancelableProgressor, options);
+				return QueuedTask.Run(action, progressor, options);
+			}
+			catch (Exception e)
+			{
+				// Log in case there is a subsequent application termination, e.g. because
+				// some async lambda is not awaited resulting in an unobserved exception.
+				_msg.Debug("Exception in Queued Task.", e);
+				throw;
+			}
 		}
 
 		public static Task Run(
-			[NotNull] Func<Task> action, Progressor progressor = null,
+			[NotNull] Func<Task> function, Progressor progressor = null,
 			TaskCreationOptions options = TaskCreationOptions.None)
 		{
-			if (progressor is null)
-				return QueuedTask.Run(action, options);
-			if (progressor is CancelableProgressor cancelableProgressor)
-				return QueuedTask.Run(action, cancelableProgressor, options);
-			return QueuedTask.Run(action, progressor, options);
+			try
+			{
+				if (progressor is null)
+					return QueuedTask.Run(function, options);
+				if (progressor is CancelableProgressor cancelableProgressor)
+					return QueuedTask.Run(function, cancelableProgressor, options);
+				return QueuedTask.Run(function, progressor, options);
+			}
+			catch (Exception e)
+			{
+				_msg.Debug("Exception in Queued Task.", e);
+				throw;
+			}
 		}
 
 		public static Task<T> Run<T>(
 			[NotNull] Func<T> function, Progressor progressor = null,
 			TaskCreationOptions options = TaskCreationOptions.None)
 		{
-			if (progressor is null)
-				return QueuedTask.Run(function, options);
-			if (progressor is CancelableProgressor cancelableProgressor)
-				return QueuedTask.Run(function, cancelableProgressor, options);
-			return QueuedTask.Run(function, progressor, options);
+			try
+			{
+				if (progressor is null)
+					return QueuedTask.Run(function, options);
+				if (progressor is CancelableProgressor cancelableProgressor)
+					return QueuedTask.Run(function, cancelableProgressor, options);
+				return QueuedTask.Run(function, progressor, options);
+			}
+			catch (Exception e)
+			{
+				_msg.Debug("Exception in Queued Task.", e);
+				throw;
+			}
 		}
 
 		public static Task<T> Run<T>(
 			[NotNull] Func<Task<T>> function, Progressor progressor = null,
 			TaskCreationOptions options = TaskCreationOptions.None)
 		{
-			if (progressor is null)
-				return QueuedTask.Run(function, options);
-			if (progressor is CancelableProgressor cancelableProgressor)
-				return QueuedTask.Run(function, cancelableProgressor, options);
-			return QueuedTask.Run(function, progressor, options);
+			try
+			{
+				if (progressor is null)
+					return QueuedTask.Run(function, options);
+				if (progressor is CancelableProgressor cancelableProgressor)
+					return QueuedTask.Run(function, cancelableProgressor, options);
+				return QueuedTask.Run(function, progressor, options);
+			}
+			catch (Exception e)
+			{
+				_msg.Debug("Exception in Queued Task.", e);
+				throw;
+			}
 		}
 	}
 }
