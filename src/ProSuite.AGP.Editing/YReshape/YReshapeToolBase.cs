77734c537d58307fe3830df26efd8f551c10da73
linusrg <l.rueegg@dirageosystems.ch>
2024-11-04 17:57:59 +0100

    add support for options

--- src/ProSuite.AGP.Editing/YReshape/YReshapeToolBase.cs
+++ src/ProSuite.AGP.Editing/YReshape/YReshapeToolBase.cs
@@ -1,13 +1,16 @@
 using System;
+using System.Threading.Tasks;
 using ProSuite.AGP.Editing.AdvancedReshape;
+using ProSuite.AGP.Editing.Cracker;
 using ProSuite.AGP.Editing.Properties;
 using ProSuite.AGP.Editing.Selection;
+using ProSuite.Commons;
 using ProSuite.Commons.AGP.Core.GeometryProcessing.AdvancedReshape;
 using ProSuite.Commons.Essentials.CodeAnnotations;
 
 namespace ProSuite.AGP.Editing.YReshape
 {
-	public class YReshapeToolBase : AdvancedReshapeToolBase
+	public abstract class YReshapeToolBase : AdvancedReshapeToolBase
 	{
 		//protected string OptionsFileName => "YReshapeToolOptions.xml";
 		// TODO: adapt Options from AdvancedReshape as soon as implemented
@@ -26,11 +29,34 @@ namespace ProSuite.AGP.Editing.YReshape
 
 		}
 
-		protected override SelectionSettings GetSelectionSettings()
-		{
-			throw new NotImplementedException();
+		protected string OptionsFileName => "YReshapeToolOptions.xml";
+
+		[CanBeNull]
+		protected virtual string CentralConfigDir => null;
+
+		protected virtual string LocalConfigDir =>
+			EnvironmentUtils.ConfigurationDirectoryProvider.GetDirectory(AppDataFolder.Roaming);
+
+		protected override void OnUpdateCore() {
+			Enabled = MicroserviceClient != null;
+
+			if (MicroserviceClient == null)
+				DisabledTooltip = ToolUtils.GetDisabledReasonNoGeometryMicroservice();
 		}
 
-		protected override IAdvancedReshapeService MicroserviceClient { get; }
+		protected override void OnToolActivatingCore() {
+			//InitializeOptions();
+		   //TODO: implement Options in advanced reshape
+			_feedback = new YReshapeFeedback();
+		}
+
+		//Make sure this is always true (settings in AdvancedReshape not implemented yet, so no effect atm)
+		protected async Task<bool> UpdateOpenJawReplacedEndpointAsync() {
+		
+			return await Task.FromResult(true);
+		}
 	}
+
+
 }
+
