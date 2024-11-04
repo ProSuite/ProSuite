d8b48129af1edfc81008a4482b6570117543b02a
linusrg <l.rueegg@dirageosystems.ch>
2024-11-04 09:38:58 +0100

    Add YReshape options and feedback

--- src/ProSuite.AGP.Editing/YReshape/YReshapeToolBase.cs
+++ src/ProSuite.AGP.Editing/YReshape/YReshapeToolBase.cs
@@ -1,15 +1,47 @@
+using System;
 using ProSuite.AGP.Editing.AdvancedReshape;
+using ProSuite.AGP.Editing.Properties;
 using ProSuite.AGP.Editing.Selection;
+using ProSuite.Commons;
 using ProSuite.Commons.AGP.Core.GeometryProcessing.AdvancedReshape;
+using ProSuite.Commons.Essentials.CodeAnnotations;
+using ProSuite.Commons.ManagedOptions;
 
-namespace ProSuite.AGP.Editing.YReshape;
-
-public class YReshapeToolBase : AdvancedReshapeToolBase
+namespace ProSuite.AGP.Editing.YReshape
 {
-	protected override SelectionSettings GetSelectionSettings()
+	public class YReshapeToolBase : AdvancedReshapeToolBase
 	{
-		throw new System.NotImplementedException();
-	}
+		private YReshapeToolOptions _yreshapeToolOptions;
+		private OverridableSettingsProvider<YReshapeToolOptions> _settingsProvider;
 
-	protected override IAdvancedReshapeService MicroserviceClient { get; }
+		protected string OptionsFileName => "YReshapeToolOptions.xml";
+
+		[CanBeNull]
+		protected virtual string CentralConfigDir => null;
+
+		protected virtual string LocalConfigDir =>
+			EnvironmentUtils.ConfigurationDirectoryProvider.GetDirectory(AppDataFolder.Roaming);
+
+		protected override SelectionSettings GetSelectionSettings()
+		{
+			throw new NotImplementedException();
+		}
+
+		protected override IAdvancedReshapeService MicroserviceClient { get; }
+
+		protected YReshapeToolBase()
+		{
+			SelectionCursor = ToolUtils.GetCursor(Resources.YReshapeToolCursor);
+			SelectionCursorShift = ToolUtils.GetCursor(Resources.YReshapeToolCursorShift);
+		}
+
+		// TopGIS Code //
+
+		protected override void OptionsInitialized()
+		{
+			AdvancedReshapeOptions.OpenJawReshapePolicy = OpenJawReshapePolicy.Allow;
+
+			AdvancedReshapeOptions.ProtectOpenJawSettings();
+		}
+	}
 }
