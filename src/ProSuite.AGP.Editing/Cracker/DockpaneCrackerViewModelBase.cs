bd84dfb0af826f7834e1b42c461b9923a5e10dd6
linusrg <l.rueegg@dirageosystems.ch>
2024-12-17 17:51:03 +0100

    Add RevertToDefaults Button in Cracker

--- src/ProSuite.AGP.Editing/Cracker/DockpaneCrackerViewModelBase.cs
+++ src/ProSuite.AGP.Editing/Cracker/DockpaneCrackerViewModelBase.cs
@@ -1,4 +1,6 @@
 using ProSuite.Commons.AGP.Framework;
+using System.Windows.Input;
+using ArcGIS.Desktop.Framework;
 
 namespace ProSuite.AGP.Editing.Cracker
 {
@@ -6,7 +8,10 @@ namespace ProSuite.AGP.Editing.Cracker
 	{
 		protected abstract string DockPaneDamlID { get; }
 
-		protected DockpaneCrackerViewModelBase() : base(new DockpaneCracker()) { }
+		protected DockpaneCrackerViewModelBase() : base(new DockpaneCracker())
+		{
+			RevertToDefaultsCommand = new RelayCommand(RevertToDefaults);
+		}
 
 		/// Text shown near the top of the DockPane.
 		private string _heading = "Cracker Options";
@@ -35,5 +40,12 @@ namespace ProSuite.AGP.Editing.Cracker
 		public TargetFeatureSelectionViewModel TargetFeatureSelectionVM { get; private set; }
 
 		public double SpinnerValue { get; set; }
+
+		// Add a command to trigger RevertToDefaults
+		public ICommand RevertToDefaultsCommand { get; }
+	
+		private void RevertToDefaults() {
+			Options?.RevertToDefaults();
+		}
 	}
 }
