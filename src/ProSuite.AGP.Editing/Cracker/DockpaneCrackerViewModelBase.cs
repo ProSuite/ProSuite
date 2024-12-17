3fed6f76811e0f6e8846f1eea5c9d397b89c5ccb
linusrg <l.rueegg@dirageosystems.ch>
2024-12-17 19:29:19 +0100

    Cleanup

--- src/ProSuite.AGP.Editing/Cracker/DockpaneCrackerViewModelBase.cs
+++ src/ProSuite.AGP.Editing/Cracker/DockpaneCrackerViewModelBase.cs
@@ -1,30 +1,34 @@
-using ProSuite.Commons.AGP.Framework;
+using System.ComponentModel;
 using System.Windows.Input;
 using ArcGIS.Desktop.Framework;
-using System.ComponentModel;
+using ProSuite.Commons.AGP.Framework;
 
 namespace ProSuite.AGP.Editing.Cracker
 {
 	public abstract class DockpaneCrackerViewModelBase : DockPaneViewModelBase
 	{
-		protected DockpaneCrackerViewModelBase() : base(new DockpaneCracker()) {
+		protected DockpaneCrackerViewModelBase() : base(new DockpaneCracker())
+		{
 			RevertToDefaultsCommand = new RelayCommand(RevertToDefaults);
 		}
-		#region RestoreDefaultsButton
-		public TargetFeatureSelectionViewModel TargetFeatureSelectionVM { get; private set; }
 
+		#region RestoreDefaultsButton
+
+		public TargetFeatureSelectionViewModel TargetFeatureSelectionVM { get; private set; }
 
 		public ICommand RevertToDefaultsCommand { get; }
 
 		public bool IsButtonEnabled => _options?.CentralOptions != null;
 
-		private void RevertToDefaults() {
+		private void RevertToDefaults()
+		{
 			Options?.RevertToDefaults();
 		}
+
 		#endregion
 
 		protected abstract string DockPaneDamlID { get; }
-		
+
 		/// Text shown near the top of the DockPane.
 		private string _heading = "Cracker Options";
 
@@ -48,14 +52,12 @@ namespace ProSuite.AGP.Editing.Cracker
 						_options.CentralizableTargetFeatureSelection);
 			}
 		}
-		
 
 		public event PropertyChangedEventHandler PropertyChanged;
-		protected virtual void OnPropertyChanged(string propertyName) {
+
+		protected virtual void OnPropertyChanged(string propertyName)
+		{
 			PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
 		}
-
-
-
 	}
 }
