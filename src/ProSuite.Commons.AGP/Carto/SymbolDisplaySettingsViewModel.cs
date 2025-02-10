21f7bf20a0f2744ad644535ec883ed205873a054
Urs-Jakob Ruetschi <u.rueetschi@esri.ch>
2025-02-10 14:27:17 +0100

    Extract ScaleConverter and ScaleValidation

--- src/ProSuite.Commons.AGP/Carto/SymbolDisplaySettingsViewModel.cs
+++ src/ProSuite.Commons.AGP/Carto/SymbolDisplaySettingsViewModel.cs
@@ -1,9 +1,6 @@
 using System;
 using System.ComponentModel;
-using System.Globalization;
 using System.Runtime.CompilerServices;
-using System.Windows.Controls;
-using System.Windows.Data;
 
 namespace ProSuite.Commons.AGP.Carto;
 
@@ -85,87 +82,5 @@ public class SymbolDisplaySettingsViewModel : INotifyPropertyChanged
 	private void OnPropertyChanged([CallerMemberName] string name = null)
 	{
 		PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
-	}
-}
-
-[ValueConversion(typeof(double), typeof(string))]
-public class ScaleConverter : IValueConverter // TODO move to Commons.UI/WPF?
-{
-	// Exceptions in Convert and ConvertBack crash the app!
-	// Use validation so that ConvertBack is only called for valid input:
-	// <TextBox>
-	//   <TextBox.Text>
-	//     <Binding Path="ScaleDenomDoubleProperty"
-	//              UpdateSourceTrigger="Default"> <!--or PropertyChanged-->
-	//       <Binding.ValidationRules>
-	//         <namespace:ScaleValidation/>
-	//       </Binding.ValidationRules>
-	//       <Binding.Converter>
-	//         <namespace:ScaleConverter/>
-	//       </Binding.Converter>
-	//     </Binding>
-	//   </TextBox.Text>
-	// </TextBox>
-	// https://learn.microsoft.com/en-us/dotnet/desktop/wpf/data/?view=netdesktop-9.0#associating-validation-rules-with-a-binding
-	// https://stackoverflow.com/questions/6123880/how-to-handle-exception-in-value-converter-so-that-custom-error-message-can-be-d
-
-	public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
-	{
-		if (value is null) return string.Empty;
-		var denom = System.Convert.ToDouble(value);
-		if (denom <= 0 || ! double.IsFinite(denom)) return "None";
-		return string.Create(culture, $"1:{value}");
-	}
-
-	public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
-	{
-		return Parse(value as string, culture);
-	}
-
-	public static double Parse(string text, CultureInfo culture)
-	{
-		const double none = 0.0;
-
-		text = text.Trim();
-		if (string.IsNullOrEmpty(text)) return none;
-
-		if (string.Equals(text, "None", StringComparison.OrdinalIgnoreCase))
-		{
-			return none;
-		}
-
-		int index = text.IndexOf(':');
-
-		if (index < 0)
-		{
-			return double.Parse(text.Trim(), culture);
-		}
-
-		string numerText = text.Substring(0, index).Trim();
-
-		string denomText = text.Substring(index + 1).Trim();
-
-		double numer = double.Parse(numerText, culture);
-		double denom = double.Parse(denomText, culture);
-
-		denom /= numer;
-
-		return double.IsNaN(denom) ? none : denom;
-	}
-}
-
-public class ScaleValidation : ValidationRule // TODO move to Commons.UI/WPF?
-{
-	public override ValidationResult Validate(object value, CultureInfo cultureInfo)
-	{
-		try
-		{
-			ScaleConverter.Parse(value as string, cultureInfo);
-			return new ValidationResult(true, null);
-		}
-		catch (Exception ex)
-		{
-			return new ValidationResult(false, ex.Message);
-		}
 	}
 }
