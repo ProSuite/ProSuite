fdfc93377e91c27677d8f584aa1d760a19e4c71c
Paola Dürlewanger <p.duerlewanger@dirageosystems.ch>
2025-02-24 09:11:57 +0100

    Clean up

--- src/ProSuite.QA.Tests.Definitions/Transformers/Filters/TrSpatiallyFilteredDefinition.cs
+++ src/ProSuite.QA.Tests.Definitions/Transformers/Filters/TrSpatiallyFilteredDefinition.cs
@@ -28,41 +28,5 @@ namespace ProSuite.QA.Tests.Transformers.Filters
 		[TestParameter(_defaultSearchOption)]
 		[DocTr(nameof(DocTrStrings.TrSpatiallyFiltered_FilteringSearchOption))]
 		public SearchOption FilteringSearchOption { get; set; }
-
-
-		//protected override FilteredFeatureClass GetTransformedCore(string name)
-		//{
-		//	if (_resultingClass == null)
-		//	{
-		//		string filteredTableName = ((ITableTransformer)this).TransformerName;
-
-		//		// Un-transformed, uncached identical schema as the _featureClassToFilter
-		//		// If the evaluation of the filter criterion is slow, re-consider caching.
-		//		// But an efficient cache could also be implemented locally, e.g. by
-		//		// remembering the OIDs that were filtered out previously.
-		//		_resultingClass = new FilteredFeatureClass(
-		//			_featureClassToFilter, filteredTableName,
-		//			createBackingDataset: gdbTable =>
-		//				CreateFilteredDataset((FilteredFeatureClass)gdbTable));
-
-		//		SpatiallyFilteredBackingDataset filterBackingData =
-		//			(SpatiallyFilteredBackingDataset)_resultingClass.BackingData;
-
-		//		IFeatureClassFilter filterIntersecting = new AoFeatureClassFilter();
-
-		//		// TODO: Is this the way to do this?
-		//		int intersectingTableIndex = 1;
-		//		ConfigureQueryFilter(intersectingTableIndex, filterIntersecting);
-
-		//		filterIntersecting.SpatialRelationship = esriSpatialRelEnum.esriSpatialRelIntersects;
-
-		//		filterBackingData.IntersectingFeatureFilter = filterIntersecting;
-		//	}
-
-		//	return _resultingClass;
-		//}
-
-		//protected abstract SpatiallyFilteredBackingDataset CreateFilteredDataset(
-		//	[NotNull] FilteredFeatureClass resultClass);
 	}
 }
