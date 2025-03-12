6b0ead379a8b777230e9f51f4bd7e12d1d1f9039
Paola Dürlewanger <p.duerlewanger@dirageosystems.ch>
2025-03-12 16:10:55 -0700

    Reshape Along Options: Move to CentralizableSettingViewModel

--- src/ProSuite.AGP.Editing/ChangeAlong/DockPaneReshapeAlongViewModelBase.cs
+++ src/ProSuite.AGP.Editing/ChangeAlong/DockPaneReshapeAlongViewModelBase.cs
@@ -42,6 +42,59 @@ namespace ProSuite.AGP.Editing.ChangeAlong
 			set { SetProperty(ref _heading, value, () => Heading); }
 		}
 
+		public CentralizableSettingViewModel<bool> ExcludeLinesOutsideSource =>
+			new CentralizableSettingViewModel<bool>(Options.CentralizableExcludeLinesOutsideSource);
+
+		public CentralizableSettingViewModel<double> ExcludeLinesTolerance =>
+			new CentralizableSettingViewModel<double>(Options.CentralizableExcludeLinesTolerance,
+			                                          Options
+				                                          .CentralizableExcludeLinesOutsideSource);
+
+		public CentralizableSettingViewModel<bool> ExcludeLinesDisplay =>
+			new CentralizableSettingViewModel<bool>(Options.CentralizableExcludeLinesDisplay,
+			                                        Options.CentralizableExcludeLinesOutsideSource);
+
+		public CentralizableSettingViewModel<bool> ExcludeLinesShowOnlyRemove =>
+			new CentralizableSettingViewModel<bool>(
+				Options.CentralizableExcludeLinesShowOnlyRemove);
+
+		public CentralizableSettingViewModel<bool> ExcludeLinesOverlaps =>
+			new CentralizableSettingViewModel<bool>(Options.CentralizableExcludeLinesOverlaps);
+
+		public CentralizableSettingViewModel<bool> DisplayExcludeCutLines =>
+			new CentralizableSettingViewModel<bool>(Options.CentralizableDisplayExcludeCutLines);
+
+		public CentralizableSettingViewModel<bool> DisplayRecalculateCutLines =>
+			new CentralizableSettingViewModel<bool>(Options.CentralizableDisplayRecalculateCutLines,
+			                                        Options.CentralizableDisplayExcludeCutLines);
+
+		public CentralizableSettingViewModel<bool> BufferTarget =>
+			new CentralizableSettingViewModel<bool>(Options.CentralizableBufferTarget);
+
+		public CentralizableSettingViewModel<double> BufferTolerance =>
+			new CentralizableSettingViewModel<double>(Options.CentralizableBufferTolerance,
+			                                          Options.CentralizableBufferTarget);
+
+		public CentralizableSettingViewModel<bool> EnforceMinimumBufferSegmentLength =>
+			new CentralizableSettingViewModel<bool>(
+				Options.CentralizableEnforceMinimumBufferSegmentLength,
+				Options.CentralizableBufferTarget);
+
+		public CentralizableSettingViewModel<double> MinBufferSegmentLength =>
+			new CentralizableSettingViewModel<double>(Options.CentralizableMinBufferSegmentLength,
+			                                          Options
+				                                          .CentralizableEnforceMinimumBufferSegmentLength);
+
+		public CentralizableSettingViewModel<bool> InsertVertices =>
+			new CentralizableSettingViewModel<bool>(Options.CentralizableInsertVertices);
+
+		public CentralizableSettingViewModel<bool> MinimalToleranceApply =>
+			new CentralizableSettingViewModel<bool>(Options.CentralizableMinimalToleranceApply);
+
+		public CentralizableSettingViewModel<double> MinimalTolerance =>
+			new CentralizableSettingViewModel<double>(Options.CentralizableMinimalTolerance,
+			                                          Options.CentralizableMinimalToleranceApply);
+
 		public ReshapeAlongToolOptions Options
 		{
 			get => _options;
