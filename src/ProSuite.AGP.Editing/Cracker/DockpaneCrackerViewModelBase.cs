2bd40d9e28633c0113b55c1258863ea2cfc405e4
Paola Dürlewanger <p.duerlewanger@dirageosystems.ch>
2025-03-13 10:36:57 -0700

    Cracker Tool Options: Move to CentralizableSettingViewModel

--- src/ProSuite.AGP.Editing/Cracker/DockpaneCrackerViewModelBase.cs
+++ src/ProSuite.AGP.Editing/Cracker/DockpaneCrackerViewModelBase.cs
@@ -42,6 +42,23 @@ namespace ProSuite.AGP.Editing.Cracker
 			set { SetProperty(ref _heading, value, () => Heading); }
 		}
 
+		public CentralizableSettingViewModel<bool> SnapToTargetVertices=>
+			new CentralizableSettingViewModel<bool>(Options.CentralizableSnapToTargetVertices);
+
+		public CentralizableSettingViewModel<double> SnapTolerance =>
+			new CentralizableSettingViewModel<double>(Options.CentralizableSnapTolerance,
+			                                          Options.CentralizableSnapToTargetVertices);
+
+		public CentralizableSettingViewModel<bool> RespectMinimumSegmentLength =>
+			new CentralizableSettingViewModel<bool>(Options.CentralizableRespectMinimumSegmentLength);
+
+		public CentralizableSettingViewModel<double> MinimumSegmentLength =>
+			new CentralizableSettingViewModel<double>(Options.CentralizableMinimumSegmentLength,
+			                                          Options.CentralizableRespectMinimumSegmentLength);
+
+		public CentralizableSettingViewModel<bool> UseSourceZs =>
+			new CentralizableSettingViewModel<bool>(Options.CentralizableUseSourceZs);
+
 		public CrackerToolOptions Options
 		{
 			get => _options;
