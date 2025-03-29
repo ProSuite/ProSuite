314e6811f166457925f9d861036086d2cf3bb4c4
Emanuel Mahler <e.mahler@dirageosystems.ch>
2025-03-29 14:57:11 +0100

    Start support for BigInteger
    They start appearing starting with 11.0.

--- src/ProSuite.QA.Container/TestUtils.cs
+++ src/ProSuite.QA.Container/TestUtils.cs
@@ -1298,6 +1298,11 @@ namespace ProSuite.QA.Container
 
 				case esriFieldType.esriFieldTypeInteger:
 					return typeof(int);
+#if !NET48
+				// TODO: BigInteger support everywhere!
+				case esriFieldType.esriFieldTypeBigInteger:
+					return typeof(long);
+#endif
 
 				case esriFieldType.esriFieldTypeSingle:
 					return typeof(float);
