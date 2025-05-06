01bdfd4124eff378c7c129ddb997708858abfcdf
Urs-Jakob Ruetschi <u.rueetschi@esri.ch>
2025-05-06 21:03:07 +0200

    Extract ModelUtils into its own file (was in ModelExtensions)

--- src/ProSuite.DomainModel.AO/DataModel/ModelExtensions.cs
+++ src/ProSuite.DomainModel.AO/DataModel/ModelExtensions.cs
@@ -2,7 +2,6 @@ using System;
 using ESRI.ArcGIS.Geodatabase;
 using ProSuite.Commons;
 using ProSuite.Commons.AO.Geodatabase;
-using ProSuite.Commons.DomainModels;
 using ProSuite.Commons.Essentials.Assertions;
 using ProSuite.Commons.Essentials.CodeAnnotations;
 using ProSuite.Commons.Logging;
@@ -126,32 +125,6 @@ namespace ProSuite.DomainModel.AO.DataModel
 			return true;
 		}
 
-		private static bool DetermineMasterDatabaseWorkspaceAccessibility(this Model model)
-		{
-			if (model.UserConnectionProvider == null)
-			{
-				return false;
-			}
-
-			try
-			{
-				// try to open the workspace
-				model.UserConnectionProvider.OpenWorkspace();
-
-				return true;
-			}
-			catch (Exception ex)
-			{
-				_msg.DebugFormat("Error opening master database for model {0}: {1}",
-				                 model.Name, ex.Message);
-
-				//_lastMasterDatabaseAccessError = ex.Message;
-				model.CachedMasterDatabaseNoAccessReason = ex.Message;
-
-				return false;
-			}
-		}
-
 		[NotNull]
 		public static IWorkspaceContext AssertMasterDatabaseWorkspaceContextAccessible(
 			this Model model)
@@ -215,80 +188,37 @@ namespace ProSuite.DomainModel.AO.DataModel
 
 		#endregion
 
+		private static bool DetermineMasterDatabaseWorkspaceAccessibility(this Model model)
+		{
+			if (model.UserConnectionProvider == null)
+			{
+				return false;
+			}
+
+			try
+			{
+				// try to open the workspace
+				model.UserConnectionProvider.OpenWorkspace();
+
+				return true;
+			}
+			catch (Exception ex)
+			{
+				_msg.DebugFormat("Error opening master database for model {0}: {1}",
+				                 model.Name, ex.Message);
+
+				//_lastMasterDatabaseAccessError = ex.Message;
+				model.CachedMasterDatabaseNoAccessReason = ex.Message;
+
+				return false;
+			}
+		}
+
 		private static IModelMasterDatabase GetMasterDatabase(Model model)
 		{
 			return model as IModelMasterDatabase ??
 			       throw new InvalidOperationException(
 				       $"Model is not {nameof(IModelMasterDatabase)}");
-		}
-	}
-
-	public static class ModelUtils
-	{
-		private static readonly IMsg _msg = Msg.ForCurrentClass();
-
-		public static string QualifyModelElementName(
-			Model model, string modelElementName)
-		{
-			if (model is null)
-				throw new ArgumentNullException(nameof(model));
-
-			IWorkspace workspace = model.GetMasterDatabaseWorkspace();
-
-			if (workspace == null)
-			{
-				return modelElementName;
-			}
-
-			return DatasetUtils.QualifyTableName(workspace,
-			                                     model.DefaultDatabaseName,
-			                                     model.DefaultDatabaseSchemaOwner,
-			                                     modelElementName);
-		}
-
-		[NotNull]
-		public static string TranslateToModelElementName(
-			[NotNull] Model model, [NotNull] string masterDatabaseDatasetName)
-		{
-			Assert.ArgumentNotNull(model, nameof(model));
-			Assert.ArgumentNotNullOrEmpty(masterDatabaseDatasetName,
-			                              nameof(masterDatabaseDatasetName));
-
-			// the master database context does not support any prefix mappings etc.
-
-			// translate query class name (if it is one) to table name
-			string gdbDatasetName = ModelElementUtils.GetBaseTableName(
-				masterDatabaseDatasetName, model.GetMasterDatabaseWorkspaceContext());
-
-			return model.ElementNamesAreQualified
-				       ? gdbDatasetName // expected to be qualified also
-				       : ModelElementNameUtils.GetUnqualifiedName(gdbDatasetName);
-		}
-
-		public static IWorkspaceContext CreateDefaultMasterDatabaseWorkspaceContext(Model model)
-		{
-			Assert.ArgumentNotNull(model, nameof(model));
-
-			_msg.Debug("Opening default master database workspace context...");
-
-			IFeatureWorkspace featureWorkspace = model.UserConnectionProvider.OpenWorkspace();
-
-			var result = new MasterDatabaseWorkspaceContext(featureWorkspace, model);
-
-			if (model.AutoEnableSchemaCache && ! model.DisableAutomaticSchemaCaching)
-			{
-				// The model schema cache can be turned OFF by environment variable.
-				bool noModelSchemaCache =
-					EnvironmentUtils.GetBooleanEnvironmentVariableValue(
-						Model.EnvironmentVariableNoModelSchemaCache);
-
-				if (! noModelSchemaCache)
-				{
-					WorkspaceUtils.EnableSchemaCache(result.Workspace);
-				}
-			}
-
-			return result;
 		}
 	}
 }
