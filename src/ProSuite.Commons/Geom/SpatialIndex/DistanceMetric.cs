68ffc400e7bbe92a20951c202bffc10e9bc76e7f
paluchs <patrick.lu@bluewin.ch>
2025-06-13 15:45:33 +0200

    Add a source for reference to DistanceMetric class.

--- src/ProSuite.Commons/Geom/SpatialIndex/DistanceMetric.cs
+++ src/ProSuite.Commons/Geom/SpatialIndex/DistanceMetric.cs
@@ -1,5 +1,7 @@
 namespace ProSuite.Commons.Geom.SpatialIndex
 {
+	// Three typical distance metrices:
+	// https://chris3606.github.io/GoRogue/articles/grid_components/measuring-distance.html
 	public enum DistanceMetric
 	{
 		EuclideanDistance = 0,
