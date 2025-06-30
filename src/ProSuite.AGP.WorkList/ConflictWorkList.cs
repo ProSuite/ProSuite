f6ed1fc51a3791d29c8cd41591749be1813ace62
Daniel Roth <d.roth@esri.ch>
2025-06-30 14:32:26 +0200

    Add ConflictWorkList

---
+++ src/ProSuite.AGP.WorkList/ConflictWorkList.cs
@@ -0,0 +1,14 @@
+using ArcGIS.Core.Geometry;
+using ProSuite.AGP.WorkList.Contracts;
+using ProSuite.AGP.WorkList.Domain;
+using ProSuite.Commons.Essentials.CodeAnnotations;
+
+namespace ProSuite.AGP.WorkList;
+
+public class ConflictWorkList : DbStatusWorkList
+{
+	public ConflictWorkList([NotNull] IWorkItemRepository repository,
+	                        [NotNull] Geometry areaOfInterest, [NotNull] string name,
+	                        [NotNull] string displayName) : base(
+		repository, areaOfInterest, name, displayName) { }
+}
