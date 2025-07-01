211c8de4e75df5a100b2ba9cd6b253a3c50f1ea2
Emanuel Mahler <e.mahler@dirageosystems.ch>
2025-07-01 07:34:32 +0200

    Fix build: Use async methods, get rid of .Wait()

--- src/ProSuite.AGP.Editing/MergeFeatures/MergeFeaturesToolBase.cs
+++ src/ProSuite.AGP.Editing/MergeFeatures/MergeFeaturesToolBase.cs
@@ -133,17 +133,17 @@ namespace ProSuite.AGP.Editing.MergeFeatures
 			}
 		}
 
-		protected override async Task ToggleSelectionSketchGeometryType(
+		protected override async Task ToggleSelectionSketchGeometryTypeAsync(
 			SketchGeometryType toggleSketchType,
 			SelectionCursors selectionCursors = null)
 		{
 			if (await IsInSelectionPhaseAsync())
 			{
-				await base.ToggleSelectionSketchGeometryType(toggleSketchType, selectionCursors);
+				await base.ToggleSelectionSketchGeometryTypeAsync(toggleSketchType, selectionCursors);
 			}
 			else
 			{
-				await base.ToggleSelectionSketchGeometryType(toggleSketchType, _secondPhaseCursors);
+				await base.ToggleSelectionSketchGeometryTypeAsync(toggleSketchType, _secondPhaseCursors);
 			}
 		}
 
@@ -233,7 +233,10 @@ namespace ProSuite.AGP.Editing.MergeFeatures
 		private async Task StartSecondPhaseAsync()
 		{
 			await QueuedTask.Run(() => { SetupSketch(); });
-			await QueuedTask.Run(() => { ResetSelectionSketchType(_secondPhaseCursors).Wait(); });
+			await QueuedTask.Run(async () =>
+			{
+				await ResetSelectionSketchTypeAsync(_secondPhaseCursors);
+			});
 		}
 
 		protected override void LogUsingCurrentSelection()
