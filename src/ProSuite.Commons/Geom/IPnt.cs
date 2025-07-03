7724855feec08481e067d306b042bcdae6c25ce0
paluchs <patrick.lu@bluewin.ch>
2025-07-03 11:28:29 +0200

    Add documentation to ICoordinates and related interfaces and objects.

--- src/ProSuite.Commons/Geom/IPnt.cs
+++ src/ProSuite.Commons/Geom/IPnt.cs
@@ -1,6 +1,18 @@
 namespace ProSuite.Commons.Geom
 {
-	public interface IPnt : ICoordinates, IGmtry, IBoundedXY
+	/// <summary>
+	/// More specific interface for a point in space than <see cref="ICoordinates"/>. Extends both <see cref="ICoordinates"/> and <see cref="IGmtry"/>.
+	/// </summary>
+	/// <remarks>
+	/// Grown over time.
+	/// <para>
+	/// <strong>Future Considerations:</strong><br></br>
+	/// We may want to reconsider the design of this interface and its position in the hierarchy.
+	/// Probably we don't want to expose the indexer here, since it is an implementation detail.
+	/// Moreover, <c>Clone()</c> might need to be pushed to <see cref="IGmtry"/> or a more general interface.
+	/// </para>
+	/// </remarks>
+	public interface IPnt : ICoordinates, IGmtry
 	{
 		double this[int index] { get; set; }
 
