6565107cb3cb0aa8fa8b4855f21a42ac8d81b0f6
Daniel Roth <d.roth@esri.ch>
2025-07-07 10:12:15 +0200

    Add own RelayCommand without command parameter
    To not use Pro RelayCommand and therefor usable in own
    standalone test exe.

--- src/ProSuite.Commons.UI/WPF/RelayCommand.cs
+++ src/ProSuite.Commons.UI/WPF/RelayCommand.cs
@@ -1,17 +1,128 @@
-using System;
-using System.Reflection;
-using System.Windows;
-using System.Windows.Input;
-using System.Windows.Threading;
 using ProSuite.Commons.Essentials.Assertions;
 using ProSuite.Commons.Essentials.CodeAnnotations;
 using ProSuite.Commons.Logging;
+using System;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Threading;
 
 namespace ProSuite.Commons.UI.WPF
 {
+	public class RelayCommand : ICommand
+	{
+		private static readonly IMsg _msg = Msg.ForCurrentClass();
+
+		private readonly Action _execute;
+		private readonly Func<bool> _canExecute;
+
+		private readonly Func<Task> _executeTask;
+		private bool _previousCanExecute;
+
+		public RelayCommand(Action execute, Func<bool> canExecute)
+		{
+			_execute = execute;
+			_canExecute = canExecute;
+		}
+
+		public RelayCommand(Func<Task> execute, Func<bool> canExecute)
+		{
+			_executeTask = execute;
+			_canExecute = canExecute;
+		}
+
+		public void RaiseCanExecuteChanged(bool knownChanged = false,
+		                                   object parameter = null)
+		{
+			if (_canExecute == null)
+			{
+				return;
+			}
+
+			if (! knownChanged)
+			{
+				// This will test the Can method and trigger this method with knownChanged == true.
+				CanExecute(parameter);
+				return;
+			}
+
+			// NOTE (WinForms): Application.Current is null in ArcMap. Supposedly the non-modal
+			// dialog has it's own message pump which ensures the correct state of the UI.
+
+			// NOTE (WPF): When setting properties on objects that implement INotifyPropertyChanged
+			// from a background thread, WPF automatically invokes the setter on the UI thread
+			// which is necessary to avoid InvalidOperationExceptions ('The calling thread cannot
+			// access this object because a different thread owns it').
+			// However, for raising events this is not the case! The command and the event handler
+			// were created on the UI thread and hence must be accessed on the UI thread:
+			Application.Current?.Dispatcher.BeginInvoke(
+				new Action(delegate
+				{
+					// This invokes the CommandManager.RequerySuggested event which fires the
+					// CanExecuteChanged event below.
+					CommandManager.InvalidateRequerySuggested();
+				}),
+				DispatcherPriority.ApplicationIdle);
+		}
+
+		#region ICommand Members
+
+		public bool CanExecute(object parameter)
+		{
+			try
+			{
+				bool result = _canExecute?.Invoke() ?? true;
+
+				if (result != _previousCanExecute)
+				{
+					_previousCanExecute = result;
+
+					RaiseCanExecuteChanged(true);
+				}
+
+				return result;
+			}
+			catch (Exception e)
+			{
+				_msg.Debug("Error in relay command's CanExecute method", e);
+
+				throw;
+			}
+		}
+		public event EventHandler CanExecuteChanged
+		{
+			add => CommandManager.RequerySuggested += value;
+			remove => CommandManager.RequerySuggested -= value;
+		}
+
+		public async void Execute(object parameter)
+		{
+			try
+			{
+				if (_executeTask != null)
+				{
+					await _executeTask();
+					return;
+				}
+
+				_execute();
+			}
+			catch (Exception e)
+			{
+				// Throwing from here can crash the (WinForms host) application.
+				// Just for diagnostic purposes, log the exception.
+				_msg.Debug("Error in relay command's Execute method", e);
+
+				throw;
+			}
+		}
+
+		#endregion
+	}
+
 	public class RelayCommand<T> : ICommand
 	{
-		private static readonly IMsg _msg = new Msg(MethodBase.GetCurrentMethod().DeclaringType);
+		private static readonly IMsg _msg = Msg.ForCurrentClass();
 
 		private readonly Action<T> _execute;
 		private readonly Predicate<T> _canExecute;
