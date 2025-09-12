cb1c4cb6e6e67f4e668351356ee39189b84e781a
Emanuel Mahler <e.mahler@dirageosystems.ch>
2025-09-12 14:21:40 +0200

    Separate instantiation from loading
    To avoid loading from null path in base class before it is initialized
    in the sub class.

--- src/ProSuite.AGP.WorkList.Contracts/IWorkItemStateRepository.cs
+++ src/ProSuite.AGP.WorkList.Contracts/IWorkItemStateRepository.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using ArcGIS.Core.Geometry;
+using ProSuite.Commons.Essentials.CodeAnnotations;
 
 namespace ProSuite.AGP.WorkList.Contracts
 {
@@ -10,17 +11,30 @@ namespace ProSuite.AGP.WorkList.Contracts
 	/// </summary>
 	public interface IWorkItemStateRepository
 	{
+		/// <summary>
+		/// Loads all states from the persisted state (work list file).
+		/// </summary>
+		void LoadAllStates();
+
 		void Refresh(IWorkItem item);
 
 		void UpdateState(IWorkItem item);
 
-		void Commit(IList<ISourceClass> sourceClasses,
+		/// <summary>
+		/// Writes all states to the persisted state in the <see cref="WorkListDefinitionFilePath"/>
+		/// </summary>
+		/// <param name="sourceClasses"></param>
+		/// <param name="extent"></param>
+		void Commit([NotNull] IList<ISourceClass> sourceClasses,
 		            Envelope extent);
 
 		int? CurrentIndex { get; set; }
 
 		void Rename(string name);
 
+		/// <summary>
+		/// The full path of the persistent storage.
+		/// </summary>
 		string WorkListDefinitionFilePath { get; set; }
 	}
 }
