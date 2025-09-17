967aa23ff26f4d1720c6a492f0c7ed7d1b516790
Daniel Roth <d.roth@esri.ch>
2025-09-17 14:24:12 +0200

    Fix creation of work list

--- src/ProSuite.AGP.WorkList/IWorkListEnvironmentFactory.cs
+++ src/ProSuite.AGP.WorkList/IWorkListEnvironmentFactory.cs
@@ -1,18 +1,7 @@
-using System;
-using ProSuite.AGP.WorkList.Contracts;
-
 namespace ProSuite.AGP.WorkList;
 
 public interface IWorkListEnvironmentFactory
 {
-	void WithPath(Func<string, IWorkEnvironment> createEnvironment);
-
-	void WithItemStore(Func<IWorkListItemDatastore, IWorkEnvironment> createEnvironment);
-
-	IWorkListEnvironmentFactory RegisterEnvironment<T>() where T : IWorkList;
-
 	// todo: (daro): make generic instead of strings?
 	IWorkEnvironment CreateWorkEnvironment(string path, string typeName);
-
-	void AddStore<T>(IWorkListItemDatastore store) where T : IWorkList;
 }
