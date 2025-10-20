531b2399b82350197c4aeaa0b3bdcee34dfa5886
Emanuel Mahler <e.mahler@dirageosystems.ch>
2025-10-20 09:23:54 +0200

    Add command to show dialogs

---
+++ src/ProSuite.Commons.AGP/Framework/ShowDialogCommandBase.cs
@@ -0,0 +1,77 @@
+using System;
+using System.Drawing;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ProSuite.Commons.Essentials.Assertions;
+using ProSuite.Commons.Essentials.CodeAnnotations;
+using ProSuite.Commons.UI.Env;
+using ProSuite.Commons.UI.WinForms;
+
+namespace ProSuite.Commons.AGP.Framework;
+
+public abstract class ShowDialogCommandBase : ButtonCommandBase
+{
+	private bool _clicked;
+
+	protected override Task<bool> OnClickAsyncCore()
+	{
+		if (_clicked)
+		{
+			return Task.FromResult(false);
+		}
+
+		_clicked = true;
+		try
+		{
+			ProcessClickAsync();
+		}
+		finally
+		{
+			_clicked = false;
+		}
+
+		return Task.FromResult(true);
+	}
+
+	protected abstract Task ProcessClickAsync();
+
+	protected static Point GetScreenLocation(CursorState cursorState)
+	{
+		return cursorState != null
+			       ? new Point(cursorState.X, cursorState.Y)
+			       : Cursor.Position;
+	}
+
+	protected static Point GetCorrectedMenuPosition([NotNull] Control control,
+	                                                CursorState cursorState)
+	{
+		Assert.ArgumentNotNull(control, nameof(control));
+
+		Point screenLocation = GetScreenLocation(cursorState);
+
+		return GetCorrectedLocation(control, screenLocation);
+	}
+
+	protected static Point GetCorrectedLocation([NotNull] Control control,
+	                                            Point clickLocation)
+	{
+		Assert.ArgumentNotNull(control, nameof(control));
+
+		return WinFormUtils.GetCorrectedPopupLocation(control, clickLocation);
+	}
+
+	protected static async Task ShowDialogAsync([NotNull] Action<Point> showDialog)
+	{
+		Assert.ArgumentNotNull(showDialog, nameof(showDialog));
+
+		CursorState cursorState = await UIEnvironment.ReleaseCursorAsync();
+		try
+		{
+			showDialog(new Point(cursorState.X, cursorState.Y));
+		}
+		finally
+		{
+			await UIEnvironment.RestoreCursorAsync(cursorState);
+		}
+	}
+}
