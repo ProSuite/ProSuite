1c1212a74dbe188033a11bccc2dcb8ea004ba1a2
linusrg <l.rueegg@dirageosystems.ch>
2025-10-31 10:23:56 +0100

    Add CloseWindowCommand for unified Picker closing
    - MouseLeftButtonUp closes the Picker, instead of right click
    - Ricght click selects an item, without closing the picker (improvement for tools listening to MouseButtonUp AND  using the picker. E.g. SelectConnectedLines)
    - Refactored OnWindowDeactivated to delegate window closing to CloseWindowCommand.

--- src/ProSuite.Commons.AGP/PickerUI/PickerWindow.xaml.cs
+++ src/ProSuite.Commons.AGP/PickerUI/PickerWindow.xaml.cs
@@ -1,5 +1,8 @@
 using System;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+using ArcGIS.Desktop.Framework.Controls;
 using ProSuite.Commons.AGP.Picker;
 
 namespace ProSuite.Commons.AGP.PickerUI
@@ -21,6 +24,18 @@ namespace ProSuite.Commons.AGP.PickerUI
 		public void Dispose()
 		{
 			_viewModel.Dispose();
+		}
+		private void ListBoxItem_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+		{
+			// optional: ensure left button
+			if (e.ChangedButton != MouseButton.Left)
+			{
+				return;
+			}
+
+			// sender should be the ListBoxItem; selection binding already updates SelectedItem
+			Close();
+			e.Handled = true;
 		}
 	}
 }
