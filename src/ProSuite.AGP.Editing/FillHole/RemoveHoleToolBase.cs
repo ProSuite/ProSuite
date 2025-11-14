c2589a2ff4c1d17e0117a742dbe58e1b97a4fe4b
Paola Dürlewanger <p.duerlewanger@dirageosystems.ch>
2025-11-14 14:59:04 +0100

    Update handling, feedback and log message for supoption 'Calculate preview only....' in FillHole and RemoveHole

--- src/ProSuite.AGP.Editing/FillHole/RemoveHoleToolBase.cs
+++ src/ProSuite.AGP.Editing/FillHole/RemoveHoleToolBase.cs
@@ -213,6 +213,23 @@ namespace ProSuite.AGP.Editing.FillHole
 		{
 			Assert.NotNull(_holes);
 
+			MapView activeMapView = MapView.Active;
+
+			List<Feature> selectedFeatures;
+
+			if (! GeometryUtils.Contains(_calculationExtent, sketch))
+			{
+				// The extent has changed since the holes were calculated -> recalculate
+				selectedFeatures =
+					MapUtils.GetFeatures(
+						        selection, true, activeMapView.Map.SpatialReference)
+					        .Where(f => GeometryUtils.Intersects(
+						               _calculationExtent, f.GetShape().Extent))
+					        .ToList();
+
+				CalculateHoles(selectedFeatures, null, CancellationToken.None);
+			}
+
 			IList<Holes> featuresWithHoles = SelectHoles(_holes, sketch);
 
 			_msg.DebugFormat("Selected {0} out of {1} hole features to remove holes",
@@ -223,9 +240,7 @@ namespace ProSuite.AGP.Editing.FillHole
 				return false;
 			}
 
-			MapView activeMapView = MapView.Active;
-
-			var selectedFeatures = MapUtils.GetFeatures(
+			selectedFeatures = MapUtils.GetFeatures(
 				selection, true, activeMapView.Map.SpatialReference).ToList();
 
 			var updates = new Dictionary<Feature, Geometry>();
@@ -300,8 +315,9 @@ namespace ProSuite.AGP.Editing.FillHole
 						: $"Found {holeCount} holes{{0}}. ";
 
 				holeCountMsg = string.Format(holeCountMsg,
+				                             _removeHoleToolOptions.ShowPreview &&
 				                             _removeHoleToolOptions.LimitPreviewToExtent
-					                             ? " in current extent (shown in green)"
+					                             ? " in current extent (shown as a green rectangle)"
 					                             : string.Empty);
 
 				string clickHoleMsg =
