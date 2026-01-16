ae2b6f7234c6edd6f451a00f98a02f3a222d7077
Emanuel Mahler <e.mahler@dirageosystems.ch>
2026-01-16 15:05:59 +0100

    Pull Files up to ILasDatasetDef, add OpenLasDataset method

--- src/ProSuite.Commons/GeoDb/ILasDatasetDef.cs
+++ src/ProSuite.Commons/GeoDb/ILasDatasetDef.cs
@@ -1,7 +1,17 @@
+using System.Collections.Generic;
+
 namespace ProSuite.Commons.GeoDb
 {
 	public interface ILasDatasetDef : IDatasetDef
 	{
+		/// <summary>
+		/// The file path of the LAS dataset if it has been created.
+		/// </summary>
 		string FilePath { get; }
+
+		/// <summary>
+		/// The list of LAS/LAZ files referenced in the dataset.
+		/// </summary>
+		IList<string> LasFiles { get; }
 	}
 }
