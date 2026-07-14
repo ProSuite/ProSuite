6c254583462659dc0e648e7bfd510db0ea55c9bf
Emanuel Mahler <e.mahler@dirageosystems.ch>
2026-07-14 23:39:42 +0200

    Make log message details window more beautiful

--- src/ProSuite.Commons.UI/Logging/LogEventItemDetailsForm.Designer.cs
+++ src/ProSuite.Commons.UI/Logging/LogEventItemDetailsForm.Designer.cs
@@ -30,98 +30,84 @@ namespace ProSuite.Commons.UI.Logging
         {
             this.textBoxDetails = new System.Windows.Forms.TextBox();
             this.buttonClose = new System.Windows.Forms.Button();
-            this.groupBoxDetails = new System.Windows.Forms.GroupBox();
             this.buttonCopy = new System.Windows.Forms.Button();
             this.statusStrip = new System.Windows.Forms.StatusStrip();
             this.toolStripStatusLabel = new System.Windows.Forms.ToolStripStatusLabel();
-            this.groupBoxDetails.SuspendLayout();
             this.statusStrip.SuspendLayout();
             this.SuspendLayout();
-            // 
+            //
             // textBoxDetails
-            // 
+            //
             this.textBoxDetails.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
                                                                                 | System.Windows.Forms.AnchorStyles.Left)
                                                                                | System.Windows.Forms.AnchorStyles.Right)));
-            this.textBoxDetails.Font = new System.Drawing.Font("Courier New", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBoxDetails.Location = new System.Drawing.Point(6, 19);
+            this.textBoxDetails.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxDetails.Location = new System.Drawing.Point(12, 12);
             this.textBoxDetails.Multiline = true;
             this.textBoxDetails.Name = "textBoxDetails";
             this.textBoxDetails.ReadOnly = true;
             this.textBoxDetails.ScrollBars = System.Windows.Forms.ScrollBars.Both;
-            this.textBoxDetails.Size = new System.Drawing.Size(590, 383);
-            this.textBoxDetails.TabIndex = 0;
-            // 
+            this.textBoxDetails.Size = new System.Drawing.Size(636, 416);
+            this.textBoxDetails.TabIndex = 2;
+            //
             // buttonClose
-            // 
+            //
             this.buttonClose.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
             this.buttonClose.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-            this.buttonClose.Location = new System.Drawing.Point(539, 426);
+            this.buttonClose.Location = new System.Drawing.Point(564, 440);
             this.buttonClose.Name = "buttonClose";
-            this.buttonClose.Size = new System.Drawing.Size(75, 23);
-            this.buttonClose.TabIndex = 1;
+            this.buttonClose.Size = new System.Drawing.Size(84, 27);
+            this.buttonClose.TabIndex = 0;
             this.buttonClose.Text = "Close";
             this.buttonClose.UseVisualStyleBackColor = true;
-            // 
-            // groupBoxDetails
-            // 
-            this.groupBoxDetails.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
-                                                                                 | System.Windows.Forms.AnchorStyles.Left)
-                                                                                | System.Windows.Forms.AnchorStyles.Right)));
-            this.groupBoxDetails.Controls.Add(this.textBoxDetails);
-            this.groupBoxDetails.Location = new System.Drawing.Point(12, 12);
-            this.groupBoxDetails.Name = "groupBoxDetails";
-            this.groupBoxDetails.Size = new System.Drawing.Size(602, 408);
-            this.groupBoxDetails.TabIndex = 2;
-            this.groupBoxDetails.TabStop = false;
-            this.groupBoxDetails.Text = "Details";
-            // 
+            //
             // buttonCopy
-            // 
+            //
             this.buttonCopy.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.buttonCopy.Location = new System.Drawing.Point(462, 426);
+            this.buttonCopy.Location = new System.Drawing.Point(472, 440);
             this.buttonCopy.Name = "buttonCopy";
-            this.buttonCopy.Size = new System.Drawing.Size(71, 23);
-            this.buttonCopy.TabIndex = 3;
+            this.buttonCopy.Size = new System.Drawing.Size(84, 27);
+            this.buttonCopy.TabIndex = 1;
             this.buttonCopy.Text = "Copy";
             this.buttonCopy.UseVisualStyleBackColor = true;
             this.buttonCopy.Click += new System.EventHandler(this.buttonCopy_Click);
-            // 
+            //
             // statusStrip
-            // 
+            //
             this.statusStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
                                                                                          this.toolStripStatusLabel});
-            this.statusStrip.Location = new System.Drawing.Point(0, 456);
+            this.statusStrip.Location = new System.Drawing.Point(0, 479);
             this.statusStrip.Name = "statusStrip";
-            this.statusStrip.Size = new System.Drawing.Size(626, 22);
-            this.statusStrip.TabIndex = 4;
-            // 
+            this.statusStrip.Size = new System.Drawing.Size(660, 22);
+            this.statusStrip.SizingGrip = false;
+            this.statusStrip.TabIndex = 3;
+            this.statusStrip.TabStop = false;
+            //
             // toolStripStatusLabel
-            // 
+            //
             this.toolStripStatusLabel.Name = "toolStripStatusLabel";
             this.toolStripStatusLabel.Size = new System.Drawing.Size(0, 17);
-            // 
+            //
             // LogEventItemDetailsForm
-            // 
+            //
             this.AcceptButton = this.buttonClose;
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.Font = new System.Drawing.Font("Segoe UI", 9F);
             this.CancelButton = this.buttonClose;
-            this.ClientSize = new System.Drawing.Size(626, 478);
-            this.Controls.Add(this.statusStrip);
+            this.ClientSize = new System.Drawing.Size(660, 501);
+            this.Controls.Add(this.textBoxDetails);
             this.Controls.Add(this.buttonCopy);
-            this.Controls.Add(this.groupBoxDetails);
             this.Controls.Add(this.buttonClose);
+            this.Controls.Add(this.statusStrip);
             this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(420, 320);
             this.Name = "LogEventItemDetailsForm";
             this.ShowIcon = false;
             this.ShowInTaskbar = false;
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Log message details";
             this.Load += new System.EventHandler(this.LogEventItemDetails_Load);
-            this.groupBoxDetails.ResumeLayout(false);
-            this.groupBoxDetails.PerformLayout();
             this.statusStrip.ResumeLayout(false);
             this.statusStrip.PerformLayout();
             this.ResumeLayout(false);
@@ -133,7 +119,6 @@ namespace ProSuite.Commons.UI.Logging
 
         private System.Windows.Forms.TextBox textBoxDetails;
         private System.Windows.Forms.Button buttonClose;
-        private System.Windows.Forms.GroupBox groupBoxDetails;
         private System.Windows.Forms.Button buttonCopy;
         private System.Windows.Forms.StatusStrip statusStrip;
         private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel;
